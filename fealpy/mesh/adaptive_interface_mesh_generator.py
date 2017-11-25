a9204ac39a78856effebd73cc9ab6feaf81b972a
Huayi Wei <weihuayi@xtu.edu.cn>
2017-11-26 06:28:29 +0800

    update

--- fealpy/mesh/adaptive_interface_mesh_generator.py
+++ fealpy/mesh/adaptive_interface_mesh_generator.py
@@ -16,22 +16,22 @@ class AdaptiveMarkerBase():
     def refine_marker(self, treemesh):
         idx = treemesh.leaf_cell_index()
         pmesh = treemesh.to_pmesh()
-        isMarkedCell = self.refine_mark(pmesh)
+        isMarkedCell = self.refine_mark(pmesh, treemesh)
         return idx[isMarkedCell]
 
     def coarsen_marker(self, treemesh):
         idx = treemesh.leaf_cell_index()
         pmesh = treemesh.to_pmesh()
-        isMarkedCell = self.coarsen_mark(pmesh)
+        isMarkedCell = self.coarsen_mark(pmesh, treemesh)
         return idx[isMarkedCell]
 
-    def interface_cell_flag(self):
+    def interface_cell_flag(self, pmesh):
         pass
 
-    def refine_mark(self, pmesh):
+    def refine_mark(self, pmesh, treemesh):
         pass
 
-    def coarsen_mark(self, pmesh):
+    def coarsen_mark(self, pmesh, treemesh):
         pass
 
 
@@ -53,7 +53,7 @@ class AdaptiveMarker2d(AdaptiveMarkerBase):
 
         return isInterfaceCell 
 
-    def refine_mark(self, pmesh):
+    def refine_mark(self, pmesh, treemesh=None):
         phi = self.phi
         c2p = pmesh.ds.cell_to_point()
 
@@ -230,8 +230,7 @@ class QuadtreeInterfaceMesh2d():
             idx1 = np.arange(initLocation[i], initLocation[i] + location[i, 0] + 1)
             newCell2[idx0] = cell[idx1]
             idx0 = np.arange(newCellLocation2[i] + location[i, 0] + 3, newCellLocation2[i] + NV2[i]) 
-            idx1 = np.arange(initLocation[i] + location[i, 1] + 1, initLocation[i] +
-                    NV0[i])
+            idx1 = np.arange(initLocation[i] + location[i, 1] + 1, initLocation[i] + NV0[i])
             newCell2[idx0] = cell[idx1]
 
         np.repeat(isInterfaceCell, NV)
@@ -252,10 +251,53 @@ class QuadtreeInterfaceMesh2d():
 class AdaptiveMarker3d(AdaptiveMarkerBase):
     def __init__(self, phi, maxh=0.01, maxa=5):
         super(AdaptiveMarker3d, self).__init__(phi, maxh, maxa)
-    def interface_cell_flag(self):
-        pass
-    def coarsen_mark(self, pmesh):
-        pass
+
+    def interface_cell_flag(self, pmesh):
+        isInterfaceFace = self.interface_face_flag(pmesh)
+        face2cell = pmesh.ds.face2cell
+        NC = pmesh.number_of_cells()
+        isInterfaceCell = np.zeros(NC, dtype=np.bool)
+        isInterfaceCell[face2cell[isInterfaceFace, 0:2]] = True
+        return isInterfaceCell 
+
+    def interface_face_flag(self, pmesh):
+        phi = self.phi
+        f2p = pmesh.ds.face_to_point()
+        phiValue = phi(pmesh.point)
+        phiSign = msign(phiValue)
+        NFV = pmesh.ds.number_of_vertices_of_faces()
+
+        eta1 = np.abs(f2p*phiSign)
+        eta2 = f2p*np.abs(phiSign)
+
+        isInterfaceFace = (eta1 < eta2) | ((eta1 == eta2) & ((NV - eta2) > 2))
+
+        return isInterfaceFace 
+
+
+    def refine_mark(self, treemesh):
+
+        N = pmesh.number_of_points()
+        NF = pmesh.number_of_faces()
+        NC = pmesh.number_of_cells()
+
+        isInterfaceCell = self.interface_cell_flag(self, pmesh)
+        c2p = pmesh.ds.cell_to_point()
+        isInterfacePoint= np.asarray(isInterfaceCell@c2p).reshape(-1)
+
+        isMarkedCell = np.zeros(NC, dtype=np.bool)
+
+        # Case 1
+        isLeafCell = treemesh.is_leaf_cell()
+        point = treemesh.point
+        cell = treemesh.ds.cell
+
+        h = np.sqrt(np.sum((point[cell[isLeafCell, 0]] - point[cell[isLeafCell, 6]])**2, axis=1))
+        maxh = self.maxh
+        isMarkedCell = isMarkedCell | ((h > maxh) & isInterfaceCell) 
+
+        return isMarkedCell 
+
     def coarsen_mark(self, pmesh):
         pass
 
