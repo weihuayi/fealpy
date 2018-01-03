14c2e7d577e720a82817998239e575bff3095b0d
xuming0629 <920972751@qq.com>
2018-01-03 20:23:21 +0800

    add test,PoissonFEMRate code

---
+++ example/PoissonFEMRate.py
@@ -0,0 +1,49 @@
+import sys
+
+import numpy as np  
+import matplotlib.pyplot as plt
+from fealpy.mesh.simple_mesh_generator import rectangledomainmesh, triangle
+from fealpy.model.poisson_model_2d import CosCosData
+from fealpy.femmodel.PoissonFEMModel import PoissonFEMModel
+
+from fealpy.tools.show import showmultirate
+
+
+m = int(sys.argv[1])
+p = int(sys.argv[2])
+n = int(sys.argv[3])
+
+if m == 1:
+    model = CosCosData()
+
+box = [0, 1, 0, 1]
+mesh = rectangledomainmesh(box, nx=n, ny=n, meshtype='tri')
+fem = PoissonFEMModel(mesh, model, p)
+maxit = 4
+
+errorType = ['$|| u_I - u_h ||_{l_2}$',
+             '$|| u - u_h||_{S,0}$',
+             '$||\\nabla_S u - \\nabla_S u_h||_{S, 0}$'
+             ]
+Ndof = np.zeros((maxit,), dtype=np.int)
+errorMatrix = np.zeros((len(errorType), maxit), dtype=np.float)
+for i in range(maxit):
+    fem.solve()
+    Ndof[i] = len(fem.uh)
+    errorMatrix[0, i] = fem.l2_error()
+    errorMatrix[1, i] = fem.L2_error()
+    errorMatrix[2, i] = fem.H1_error()
+    if i < maxit - 1:
+        mesh.uniform_refine()
+        fem.reinit(mesh)
+
+print('Ndof:', Ndof)
+print('error:', errorMatrix)
+fig = plt.figure()
+fig.set_facecolor('white')
+axes = fig.gca()
+optionlist = ['k-*', 'b-o', 'r--^', 'g->', 'm-8', 'c-D','y-x', 'y-+', 'y-h', 'y-p']
+showmultirate(axes, 0, Ndof, errorMatrix[:3, :], optionlist[:3], errorType[:3])
+axes.legend(loc=3, prop={'size': 30})
+plt.show()
+
--- fealpy/femmodel/PoissonFEMModel.py
+++ fealpy/femmodel/PoissonFEMModel.py
@@ -1,18 +1,146 @@
 import numpy as np
 from scipy.sparse import coo_matrix, csc_matrix, csr_matrix, spdiags, eye
 
-class PoissonFEMModel:
-    def __init__(self, model, V, dtype=np.float):
-        self.V = V
-        self.model = model  
-        self.lqf = lqf
-        self.rqf = rqf
+from fealpy.functionspace.function import FiniteElementFunction
+from fealpy.functionspace.tools import function_space
 
+from ..quadrature  import TriangleQuadrature
+from ..solver import solve
+from ..boundarycondition import DirichletBC
+
+class PoissonFEMModel(object):
+    def __init__(self, mesh,  model, p=1, dtype=np.float):
+        self.V = function_sapce(mesh,'Lagrange', p, dtype=dtype)
+        self.mesh = mesh 
+        self.model = model
+        self.uh = self.V.FiniteElementFunction()
+        self.uI = self.V.interpolation(model.solution)
+
+        self.dtype = dtype
+
+    def reinit(self, mesh, p=None):
+        if p is None:
+            p = self.V.p
+        self.function_space(mesh,femtype,p,dtype=self.dtype)
+        self.uh = self.V.FiniteElementFunction()
+        self.area = self.V.mesh.area()
+
+    
     def get_left_matrix(self):
-        return laplace(self.V, self.lqf, a=None)
+        V = self.V
+        mesh = self.mesh
+        NC = mesh.number_of_cells()
+        qf = TriangleQuadrature(4)
+        nQuad = qf.get_number_of_quad_points()
+        gdof = V.number_of_global_dofs()
+        ldof = V.number_of_local_dofs()
+        cell2dof = V.cell_to_dof()
+        area = self.area
+        A = coo_matrix((gdof, gdof), dtype=self.dtype)
+        for i in range(ldof):
+            for j in range(i,ldof):
+                val = np.zeros((NC,), dtype=self.dtype)
+                for q in range(nQuad):
+                    bc, w = qf.get_gauss_point_and_weight(q)
+                    gphi = grad_basis_einsum(bc)
+                    val += np.sum(gphi[:,i,:]*gphi[:,j,:], axis=1)*w
+                A += coo_matrix((val*area, (cell2dof[:,i], cell2dof[:,j])), shape=(gdof, gdof))
+                if j != i:
+                    A += coo_matrix((val*area, (cell2dof[:,j], cell2dof[:,i])), shape=(gdof, gdof))
+        return A.tocsr()
+
 
     def get_right_vector(self):
-        return source(self.pde.source, self.V, self.rqf)
+        V = self.V
+        mesh = V.mesh
+        model = self.model
+        NC = mesh.number_of_cells()
+        qf = TriangleQuadrature(4)
+        nQuad = qf.get_number_of_quad_points()
+        gdof = V.number_of_global_dofs()
+        ldof = V.number_of_local_dofs()
+        cell2dof = V.cell_to_dof()
+        bb = np.zeros((NC, ldof), dtype=self.dtype)
+        area = mesh.area()
+        for i in range(nQuad):
+            bc, w = qf.get_gauss_point_and_weight(i)
+            p = mesh.bc_to_point(bc)
+            fval = model.source(p)
+            phi = V.basis_einsum(bc)
+            for j in range(ldof):
+                bb[:, j] += fval*phi[j]*w_k
+        
+        bb *= area.reshape(-1, 1)
+        cell2dof = V.cell_to_dof()
+        b = np.zeros((gdof,), dtype=self.dtype)
+        np.add.at(b, cell2dof.flatten(), bb.flatten())
+        return b
 
-    def get_neuman_vector(self):
-        pass
+
+    def solve(self):
+        uh = self.uh
+        bc = DirichletBC(V, model.dirichlet, model.is_boundary)
+        solve(self, uh, dirichlet=bc, solver='direct')
+
+    
+    def is_boundary_dof(self,p):
+        isBdDof = np.zeros(p.shape[0], dtype=np.bool)
+        isBdDof[0] = True
+        return isBdDof
+
+    def l2_error(self):
+        uh = self.uh.copy()
+        uI = self.uI
+        uh += uI[0]
+        return np.sqrt(np.sum((uh - uI)**2)/len(uI))  
+    
+
+
+    def L2_error(self, order=4):
+        V = uh.V
+        mesh = V.mesh
+        model = self.model
+        NC = mesh.number_of_cells()
+        qf = TriangleQuadrature(order)
+        nQuad = qf.get_number_of_quad_points()
+        
+        gdof = V.number_of_global_dofs()
+        ldof = V.number_of_local_dofs()
+        
+        e = np.zeros((NC,), dtype=dtype)
+        for i in range(nQuad):
+            bc, w = qf.get_gauss_point_and_weight(i)
+            uhval = uh.value(bc)
+            p = mesh.bc_to_point(bc)
+            uval = model.source(p)
+            if len(uval.shape) == 1:
+                e += w_k*(uhval - uval)*(uhval - uval)
+            else:
+                e += w*((uhval - uval)*(uhval - uval)).sum(axis=1)
+        e *= mesh.area()
+        return np.sqrt(e.sum()) 
+
+    
+    def H1_error(self, order=4):
+        V = uh.V
+        mesh = V.mesh
+        model = self.model
+        
+        NC = mesh.number_of_cells()
+        qf = TriangleQuadrature(order)
+        nQuad = qf.get_number_of_quad_points()
+        gdof = V.number_of_global_dofs()
+        ldof = V.number_of_local_dofs()
+        e = np.zeros((NC,), dtype=dtype)
+        for i in range(nQuad):
+            bc, w = qf.get_gauss_point_and_weight(i)
+            gval = uh.grad_value(bc)
+            p = mesh.bc_to_point(bc)
+            val = model.gradient(p)
+            e += w*((gval - val)*(gval - val)).sum(axis=1)
+        e *= mesh.area()
+        return np.sqrt(e.sum())
+
+	
+
+
