507d9fb592168225705feaf2047aaba56505eca0
Huayi Wei <weihuayi@xtu.edu.cn>
2018-02-06 13:30:58 +0800

    update

---
+++ fealpy/tools/colors.py
@@ -0,0 +1,10 @@
+import matplotlib.cm as cm 
+import matplotlib.colors as colors
+
+def val_to_color(val, cmap='jet'):
+    vmax = val.max()
+    vmin = val.min()
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+    mapper = cm.ScalarMappable(norm=norm, cmap=cmap)
+    color = mapper.to_rgba(val)
+    return color
