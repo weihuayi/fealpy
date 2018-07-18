19c8faec0fdb005f1666f1b825922d7b4e305837
Huayi Wei <weihuayi@xtu.edu.cn>
2018-07-18 08:55:23 +0800

    add copyright file and setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='fealpy',
+      version='0.1',
+      description='FEALPy: Finite Element Analysis Library in Python',
+      url='http://github.com/weihuayi/fealpy',
+      author='Huayi Wei',
+      author_email='weihuayi@xtu.edu.cn',
+      license='GNU',
+      packages=['fealpy'],
+      install_requires=[
+          'pyamg', 'meshpy'
+      ],
+      zip_safe=False)
