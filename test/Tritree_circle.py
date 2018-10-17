33db97eb13e32f23bf8f9b21a28c5b922449186c
Huayi Wei <weihuayi@xtu.edu.cn>
2018-10-17 11:54:11 +0800

    update

--- test/Tritree_circle.py
+++ test/Tritree_circle.py
@@ -7,17 +7,23 @@ from fealpy.mesh.TriangleMesh import TriangleMesh
 from fealpy.mesh.level_set_function import dcircle
 from fealpy.mesh.Tritree import Tritree
 
-def get_idx(tritree, phi):
-    node = tritree.entity('node')
-    cell = tritree.entity('cell')
-    idx = tritree.leaf_cell_index()
-    value = phi(node)
-    valueSign = np.sign(value)
-    valueSign[np.abs(value)<1e-12] = 0
-    flag = (np.abs(np.sum(valueSign[cell[idx, :]], axis=1)) < 3)
-    idx = idx[flag]
-    return idx
+class AdaptiveMarker():
+    def __init__(self, phi):
+        self.phi = phi 
 
+    def refine_marker(self, tmesh):
+        node = tmesh.entity('node')
+        cell = tmesh.entity('cell')
+        idx = tmesh.leaf_cell_index()
+        value = self.phi(node)
+        valueSign = np.sign(value)
+        valueSign[np.abs(value)<1e-12] = 0
+        flag = (np.abs(np.sum(valueSign[cell[idx, :]], axis=1)) < 3)
+        idx = idx[flag]
+        return idx
+
+    def coarsen_marker(self, qtmesh):
+        pass
 node = np.array([
     (0, 0), 
     (1, 0), 
@@ -30,27 +36,28 @@ cell = np.array([
 cxy = (0.5, 0.5)
 r = 0.3
 phi = lambda p: dcircle(p, cxy, r)
+marker = AdaptiveMarker(phi)
+
 circle = Circle(cxy, r, edgecolor='g', fill=False, linewidth=2)
 
-tmesh = TriangleMesh(node, cell)
-tmesh.uniform_refine()
+mesh = TriangleMesh(node, cell)
+mesh.uniform_refine()
 
-node = tmesh.entity('node')
-cell = tmesh.entity('cell')
-tritree = Tritree(node, cell)
+node = mesh.entity('node')
+cell = mesh.entity('cell')
+tmesh = Tritree(node, cell)
 
-for i in range(5):
-    idx = get_idx(tritree, phi)
-    tritree.refine(idx)
+for i in range(2):
+    tmesh.refine(marker)
 
-idx = tritree.leaf_cell_index()
-mesh = TriangleMesh(tritree.node, tritree.ds.cell[idx, :])
+idx = tmesh.leaf_cell_index()
+mesh = TriangleMesh(tmesh.node, tmesh.ds.cell[idx, :])
 fig = plt.figure()
 axes = fig.gca()
 mesh.add_plot(axes)
-#tritree.find_node(axes, showindex=True)
-#tritree.find_cell(axes, showindex=True)
-#tritree.find_edge(axes, showindex=True) 
+#tmesh.find_node(axes, showindex=True)
+mesh.find_cell(axes, showindex=True)
+#tmesh.find_edge(axes, showindex=True) 
 plt.show()
 
 
