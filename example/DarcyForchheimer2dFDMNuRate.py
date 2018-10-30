d15c0bfe132132afa338beab19f58ffc4954e3fd
liao0415 <15516518273@163.com>
2018-10-30 11:04:08 +0800

    updated

--- example/DarcyForchheimer2dFDMNuRate.py
+++ example/DarcyForchheimer2dFDMNuRate.py
@@ -1,6 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-from fealpy.pde.darcy_forchheimer_2d import CoscosData1
-from fealpy.fdm.DarcyForchheimerFDMNuModel import DarcyForchheimerFDMModel
+from fealpy.pde.darcy_forchheimer_2d import PolyData
+from fealpy.fdm.NuDarcyForchheimerFDMModel import NuDarcyForchheimerFDMModel
 from fealpy.tools.show import showmultirate
+
+box = [0,1,0,1]
+hx = np.array([0.12,0.34,0.22,0.32])
+hy = np.array([0.25,0.13,0.33,0.29])
+#hy = np.array([0.16,0.23,0.32,0.11,0.18])
+#hx = np.array([0.25,0.25,0.25,0.25])
+#hy = np.array([0.25,0.25,0.25,0.25])
+#hx = np.array([0.2,0.2,0.2,0.2,0.2])
+hx = hx/4
+hy = hy/4
+hx = hx.repeat(4)
+hy = hy.repeat(4)
+
+pde = PolyData(box)
+maxit = 1
+Ndof = np.zeros((maxit,), dtype=np.int)
+errorType = ['$|| u_I - u_h||_0$','$||p_I - p_h||_0$',\
+        '$||Dp_I -  Dp_h||_0$','$||Dp1_I - Dp1_h||_0$']
+errpL2 = np.zeros((maxit,), dtype=np.float)
+err = np.zeros((2,maxit),dtype=np.float)
+error = np.zeros((4,maxit),dtype=np.float)
+count = np.zeros((maxit,), dtype=np.int)
+for i in range(maxit):
+    t1 = time.time()
+    mesh = pde.init_mesh(hx,hy)
+    fdm = NuDarcyForchheimerFDMModel(pde,mesh)
+    NE = mesh.number_of_edges()
+    NC = mesh.number_of_cells()
+    Ndof[i] = NE + NC
+    t2 = time.time()
+    count[i],r= fdm.solve()
+    Stime = time.time() - t2
+    ue,pe = fdm.get_max_error()
+    err[0,i] = ue
+    err[1,i] = pe
+    ueL2,peL2 = fdm.get_L2_error()
+    DpeL2 = fdm.get_DpL2_error()
+    Dp1eL2 = fdm.get_Dp1L2_error()
+ #   print('I',I)
+#    print('ep',ep)
+#    print('psemi',psemi)
+    error[0,i] = ueL2
+    error[1,i] = peL2
+    error[2,i] = DpeL2
+    error[3,i] = Dp1eL2
+#    showsolution(plt, mesh, pde, uh, ph)
+    x = np.arange(count[i])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax1.set_title('rp')
+    ax1.scatter(x,r[0,:count[i]],c='r',marker = '.')
+    ax2 = fig.add_subplot(122)
+    ax2.set_title('ru')
+    ax2.scatter(x,r[1,:count[i]],c='b',marker = '.')
+
+    if i < maxit - 1:
+        hx = hx/2
+        hx = hx.repeat(2)
+        hy = hy/2
+        hy = hy.repeat(2)
+    tottime = time.time() -t1
+    print('total time:',tottime)
+    print('Solve time:',Stime)
+print('err',err)
+print('error',error)
+print('iter',count)
+#mesh.add_plot(plt,cellcolor='w')
+#showmultirate(plt,0,Ndof,err,errorType)
+showmultirate(plt,0,Ndof,error,errorType)
+plt.show()
+
--- fealpy/pde/darcy_forchheimer_2d.py
+++ fealpy/pde/darcy_forchheimer_2d.py
@@ -1,22 +1,23 @@
 import numpy as np
 
+from ..mesh.litest_quadmesh import StructureQuadMesh1
 from ..mesh.StructureQuadMesh import StructureQuadMesh
 from ..mesh.Mesh2d import Mesh2d
 
-class CoscosData1:
+class PolyData:
     def __init__(self, box):
         self.box = box
         self.mu = 2
         self.k = 1
         self.rho = 1
-        self.beta = 30
+        self.beta = 5
         self.tol = 1e-6
         self.flat = 1
 
 
-    def init_mesh(self, nx, ny):
+    def init_mesh(self, hx, hy):
         box = self.box
-        mesh = StructureQuadMesh(box, nx, ny)
+        mesh = StructureQuadMesh1(box, hx, hy)
         return mesh
 
 
@@ -105,4 +106,244 @@ class CoscosData1:
         """ Dirichlet boundary condition
         """
         val = np.zeros(p.shape[0],1)
+        return val
+
+class ExponentData:
+    def __init__(self, box):
+        self.box = box
+        self.mu = 2
+        self.k = 1
+        self.rho = 1
+        self.beta = 5
+        self.tol = 1e-6
+
+
+    def init_mesh(self, nx, ny):
+        box = self.box
+        mesh = StructureQuadMesh(box, nx, ny)
+        return mesh
+
+
+    def source1(self, p):
+        """ The right hand side of DarcyForchheimer equation
+        """
+        x = p[..., 0]
+        y = p[..., 1]
+        rhs = x-y
+        return rhs
+
+    def velocity_x(self,p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = -y*x*(1-x)
+        return val
+
+    def velocity_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = x*y*(1-y)
+        return val
+
+    def pressure(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = np.exp(x*(1-x)*y*(1-y)) - 1
+        return val
+    def source2(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        m = -(mu/k + rho*beta*np.sqrt(x**2*y**2*((1-x)**2 + (1-y)**2)))
+        val = m*x*y*(1-x) + (1-2*x)*y*(1-y)*np.exp(x*(1-x)*y*(1-y))
+        return val
+
+    def source3(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        m = mu/k + rho*beta*np.sqrt(x**2*y**2*((1-x)**2 + (1-y)**2))
+        val = m*x*y*(1-y) + x*(1-x)*(1-2*y)*np.exp(x*(1-x)*y*(1-y))
+        return val
+    def grad_pressure_x(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = (1-2*x)*y*(1-y)*np.exp(x*(1-x)*y*(1-y))
+        return val
+
+    def grad_pressure_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = x*(1-x)*(1-2*y)*np.exp(x*(1-x)*y*(1-y))
+        return val
+    def dirichlet(self, p):
+        """ Dirichlet boundary condition
+        """
+        val = np.zeros(p.shape[0],1)
+        return val
+
+class SinsinData:
+    def __init__(self, box):
+        self.box = box
+        self.mu = 2
+        self.k = 1
+        self.rho = 1
+        self.beta = 5
+
+
+    def init_mesh(self, nx, ny):
+        box = self.box
+        mesh = StructureQuadMesh(box, nx, ny)
+        return mesh
+
+
+    def source1(self, p):
+        """ The right hand side of DarcyForchheimer equation
+        """
+        x = p.shape[0]
+        rhs = np.zeros(x,)
+        return rhs
+
+    def velocity_x(self,p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = np.sin(x)*np.cos(y)
+        return val
+    def velocity_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = -np.cos(x)*np.sin(y)
+        return val
+
+    def pressure(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        val = np.sin(pi*x)*np.sin(pi*y)
+        return val
+    def source2(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        t0 = np.sin(x)
+        t1 = np.cos(x)
+        t2 = np.sin(y)
+        t3 = np.cos(y)
+        m = mu/k + rho*beta*np.sqrt(t0**2*t3**2 + t1**2*t2**2)
+        val = m*t0*t3 + pi*np.cos(pi*x)*np.sin(pi*y)
+        return val
+
+    def source3(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        t0 = np.sin(x)
+        t1 = np.cos(x)
+        t2 = np.sin(y)
+        t3 = np.cos(y)
+        m = mu/k + rho*beta*np.sqrt(t0**2*t3**2 + t1**2*t2**2)
+        val = -m*t1*t2 + pi*np.sin(pi*x)*np.cos(pi*y)
+        return val
+    def grad_pressure_x(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        val= pi*np.cos(pi*x)*np.sin(pi*y)
+        return val
+
+    def grad_pressure_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        val = pi*np.sin(pi*x)*np.cos(pi*y)
+        return val
+
+class ArctanData:
+    def __init__(self, box):
+        self.box = box
+        self.mu = 2
+        self.k = 1
+        self.rho = 1
+        self.beta = 5
+
+
+    def init_mesh(self, nx, ny):
+        box = self.box
+        mesh = StructureQuadMesh(box, nx, ny)
+        return mesh
+
+
+    def source1(self, p):
+        """ The right hand side of DarcyForchheimer equation
+        """
+        rhs = np.zeros(p.shape[0],)
+
+        return rhs
+
+    def velocity_x(self,p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = -y
+        return val
+
+    def velocity_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = x
+        return val
+
+    def pressure(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+
+        val = 2/np.pi*np.arctan(10*(x+y-1))
+        return val
+    def source2(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        m = mu/k + rho*beta*np.sqrt(x**2+y**2)
+        val = -m*y + 20/(np.pi*(1+(10*(x+y-1))**2))
+        return val
+
+    def source3(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        mu = self.mu
+        k = self.k
+        rho = self.rho
+        beta = self.beta
+        m = mu/k + rho*beta*np.sqrt(x**2 + y**2)
+        val = m*x + 20/(np.pi*(1+(10*(x+y-1))**2))
+        return val
+    def grad_pressure_x(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = 20/(np.pi*(1+(10*(x+y-1))**2))
+        return val
+
+    def grad_pressure_y(self, p):
+        x = p[..., 0]
+        y = p[..., 1]
+        val = 20/(np.pi*(1+(10*(x+y-1))**2))
         return val
