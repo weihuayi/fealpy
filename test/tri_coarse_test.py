25f5215db4d65d4a34ff9a865628f00e1ca4cb2a
liujiangang0518 <2438577904@qq.com>
2018-12-31 11:02:57 +0800

    update

--- test/tri_coarse_test.py
+++ test/tri_coarse_test.py
@@ -14,19 +14,22 @@ cell = np.array([
         [1, 2, 0],
         [3, 0, 2]], dtype=np.int)
 
-idx = np.array([1, 3, 5, 7])
+
 mesh = TriangleMesh(node, cell)
-mesh.uniform_refine(4)
+mesh.uniform_refine(1)
 node = mesh.entity('node')
 cell = mesh.entity('cell')
 tmesh = Tritree(node, cell)
-for i in range(3):
+isLeafCell = tmesh.is_leaf_cell()
+flag = (np.sum(cell == 0, axis=1) == 5) & isLeafCell
+idx, = np.where(flag)
+for i in range(2):
+    tmesh.refine(idx)
     tmesh.coarsen(idx)
-
 fig = plt.figure()
 axes = fig.gca()
 tmesh.add_plot(axes)
-tmesh.find_node(axes, showindex=True)
-tmesh.find_edge(axes, showindex=True)
-tmesh.find_cell(axes, showindex=True)
+#tmesh.find_node(axes, showindex=True)
+#tmesh.find_edge(axes, showindex=True)
+#tmesh.find_cell(axes, showindex=True)
 plt.show()
