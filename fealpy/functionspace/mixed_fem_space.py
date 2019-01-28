e760e0cf7a7f37426eb3a061712d544e8c07bbc4
Huayi Wei <weihuayi@xtu.edu.cn>
2019-01-29 06:09:33 +0800

    linear elasticity can work well!

--- fealpy/functionspace/mixed_fem_space.py
+++ fealpy/functionspace/mixed_fem_space.py
@@ -11,7 +11,7 @@ class HuZhangFiniteElementSpace():
         self.mesh = mesh
         self.p = p
         self.dof = self.space.dof
-        self.dim = self.space.dim
+        self.dim = self.space.GD
         self.init_orth_matrices()
         self.init_cell_to_dof()
 
@@ -78,7 +78,6 @@ class HuZhangFiniteElementSpace():
             gdof += (tdim-1)*edof*NE # 边内部连续自由度的个数 
             E = mesh.number_of_edges_of_cells() # 单元边的个数
             gdof += NC*E*edof # 边内部不连续自由度的个数 
-            #TODO: 找到错误了，对于三维边上连续的自由度个数为 2 个！
 
         if p > 2:
             fdof = (p+1)*(p+2)//2 - 3*p # 面内部自由度的个数
@@ -151,8 +150,9 @@ class HuZhangFiniteElementSpace():
             if gdim == 2:
                 cell2dof[:, idx, :] = base1 + tdim*(c2d[:, idx] - base0) + np.arange(tdim)
             elif gdim == 3:
-                # 0, 1, 2号局部自由度对应切向不连续的张量自由度, 留到后面重新编号
-                cell2dof[:, idx, 3:]= base1 + (tdim - 3)*(c2d[:, idx] - base0) + np.arange(tdim - 3)
+                # 1, 2, 3 号局部自由度对应切向不连续的张量自由度, 留到后面重新编号
+                # TODO: check it is right
+                cell2dof[:, idx.reshape(-1, 1), np.array([0, 4, 5])]= base1 + (tdim - 3)*(c2d[:, idx] - base0) + np.arange(tdim - 3)
 
         fdof = (p+1)*(p+2)//2 - 3*p # 边内部自由度
         if gdim == 3:
@@ -174,8 +174,9 @@ class HuZhangFiniteElementSpace():
         if gdim == 3:
             base1 += NC*len(idx)
             idx, = np.nonzero(dofFlags[2])
+            print(idx)
             if len(idx) > 0:
-                cell2dof[:, idx, 0:3] = base1 + np.arange(NC*len(idx)*3).reshape(NC, len(idx), 3)
+                cell2dof[:, idx.reshape(-1, 1), np.array([1, 2, 3])] = base1 + np.arange(NC*len(idx)*3).reshape(NC, len(idx), 3)
 
         self.cell2dof = cell2dof.reshape(NC, -1)
 
