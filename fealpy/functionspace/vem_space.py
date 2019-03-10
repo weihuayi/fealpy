026aefc5574ac589df29dd7118f26ae7826ce3c4
Huayi Wei <weihuayi@xtu.edu.cn>
2019-03-10 18:57:03 +0800

    upddate

--- fealpy/functionspace/vem_space.py
+++ fealpy/functionspace/vem_space.py
@@ -241,14 +241,6 @@ class ScaledMonomialSpace2d():
         f = Function(self, dim=dim, array=array)
         return f
 
-   # def function(self, dim=None):
-   #     return Function(self, dim=dim)
-   #
-#    def array(self, dim=None):
-#        ldof = self.number_of_local_dofs()
-#        NC = self.mesh.number_of_cells()
-#        return np.zeros(NC*ldof, dtype=np.float)
-
     def array(self, dim=None):
         gdof = self.number_of_global_dofs()
         if dim is None:
