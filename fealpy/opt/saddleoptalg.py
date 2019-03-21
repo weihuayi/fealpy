f114e1be3c4e19481228b8b45cb79011eadbd761
liujiangang0518 <2438577904@qq.com>
2019-03-21 16:00:36 +0800

    update

--- fealpy/opt/saddleoptalg.py
+++ fealpy/opt/saddleoptalg.py
@@ -22,7 +22,7 @@ class SteepestDescentAlg:
         print("Initial energe: %12.11g, gnorm :%12.11g"%(self.f, gnorm))
 
         if maxit is None:
-            maxit = options['MaxIters']
+            maxit = options['MaxFunEvals']
 
         for i in range(maxit):
             self.x[:, 0] += alpha*self.g[:, 0]
@@ -46,7 +46,7 @@ class SteepestDescentAlg:
                 )
                 break
 
-        self.fun.output('', queue=queue, stop=True)
+        self.fun.output('tag', queue=queue, stop=True)
 
         return self.x, self.f, self.g
 
