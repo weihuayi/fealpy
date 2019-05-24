e8eaa8df92af6ad94aa21c15ef02392700fc8167
Huayi Wei <weihuayi@xtu.edu.cn>
2019-05-24 10:12:43 +0800

    udpate

--- test/test_quadtreeforest.py
+++ test/test_quadtreeforest.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+# 
 import numpy as np
 import matplotlib.pyplot as plt
 from fealpy.mesh import QuadtreeMesh
 
-
+b = 5
 node = np.array([
     (0, 0),
     (1, 0),
@@ -12,9 +14,13 @@ node = np.array([
 cell = np.array([(0, 1, 2, 3)], dtype=np.int)
 
 mesh = QuadtreeMesh(node, cell)
-mesh.uniform_refine(5)
+mesh.uniform_refine(b)
+mesh.node *= 2**b
 fig = plt.figure()
 axes = fig.gca()
-mesh.add_plot(axes)
+mesh.add_plot(axes, showaxis=True, cellcolor='lightgray', edgecolor='gray',
+        linewidths=1)
+plt.xticks(np.arange(2**b+1))
+plt.yticks(np.arange(2**b+1))
 plt.show()
 
