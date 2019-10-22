64dad641851141312f4bcddf9d9e26d0c8e9ec37
Huayi Wei <weihuayi@xtu.edu.cn>
2019-10-22 08:30:39 +0800

    udpate

--- fealpy/pde/parabolic_model_2d.py
+++ fealpy/pde/parabolic_model_2d.py
@@ -76,3 +76,95 @@ class SinCosExpData:
         eps = 1e-14
         return (p[:, 1] < eps) | (p[:, 1] > 1.0 - eps)
 
+
+class SpaceMeasureDiracSourceData:
+
+    def __init__(self):
+        pass
+
+    def domain(self):
+        points = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
+        facets = [(0, 1), (1, 2), (2, 3), (3, 0)]
+        return points, facets
+
+    def init_mesh(self, n=1, meshtype='tri'):
+        node = np.array([
+            (-1, -1),
+            (1, -1),
+            (1, 1),
+            (-1, 1)], dtype=np.float)
+
+        if meshtype is 'tri':
+            cell = np.array([(1, 2, 0), (3, 0, 2)], dtype=np.int)
+            mesh = TriangleMesh(node, cell)
+            mesh.uniform_refine(n)
+            return mesh
+        elif meshtype is 'quadtree':
+            cell = np.array([(0, 1, 2, 3)], dtype=np.int)
+            mesh = Quadtree(node, cell)
+            mesh.uniform_refine(n)
+            return mesh
+        elif meshtype is 'tritree':
+            cell = np.array([(1, 2, 0), (3, 0, 2)], dtype=np.int)
+            mesh = Tritree(node, cell)
+            mesh.uniform_refine(n)
+            return mesh
+        else:
+            raise ValueError("I don't know the meshtype %s".format(meshtype))
+
+    def init_value(self, p):
+        return self.solution(p, 0.0)
+
+    def solution(self, p, t):
+        """ The exact solution
+        """
+        pi = np.pi
+        cos = np.cos
+        sin = np.sin
+
+        gt = 1 - np.exp(-500*(t - 0.5)^2)
+        p0 = np.array([cos(2*pi*t), sin(2*pi*t)], dtype=p.dtype)
+        r = np.sqrt(np.sum((p - p0)**2, axis=-1))
+        val = -0.5*np.log(r)*gt/pi
+        return val
+
+    def gradient(self, p, t):
+        pi = np.pi
+        cos = np.cos
+        sin = np.sin
+
+        pi = np.pi
+        cos = np.cos
+        sin = np.sin
+
+        gt = 1 - np.exp(-500*(t - 0.5)^2)
+        p0 = np.array([cos(2*pi*t), sin(2*pi*t)], dtype=p.dtype)
+        val = x - p0
+        r = -1/(2*pi*np.sum(g**2, axis=-1))*gt
+        val *= r[..., np.newaxis]
+        return val
+
+    def source(self, p, t):
+        """ The right hand side of Possion equation
+        INPUT:
+            p: array object, N*2
+        """
+        return 0.0
+
+    def dirichlet(self, p, t):
+        """ Dilichlet boundary condition
+        """
+        return 0.0
+
+    def neuman(self, p, t):
+        """ Neuman boundary condition
+        """
+        return 0.0
+
+    def is_dirichlet_boundary(self, p):
+        eps = 1e-14 
+        return (p[:, 0] < eps) | (p[:, 0] > 1.0 - eps) 
+
+    def is_neuman_boundary(self, p):
+        eps = 1e-14
+        return (p[:, 1] < eps) | (p[:, 1] > 1.0 - eps)
