a1b1390810117777f0f632702c41fbdbc933305a
Huayi Wei <weihuayi@xtu.edu.cn>
2019-12-16 17:09:18 +0800

    update

--- fealpy/pde/surface_parabolic_model_3d.py
+++ fealpy/pde/surface_parabolic_model_3d.py
@@ -1,36 +1,60 @@
 import numpy as np
+from ..timeintegratoralg.timeline_new import UniformTimeLine
+from ..timeintegratoralg.timeline_new import ChebyshevTimeLine
 
-class SinSinSinExpData:
+
+class SinSinSinExpDataSphere():
     def __init__(self):
-        self.diffusionCoefficient = 1/16
+        from fealpy.geometry import SphereSurface
+        self.surface = SphereSurface()
 
-    def init_value(self,p):
-        return self.solution(p, 0.0)
+    def domain(self):
+        return self.surface
+
+    def time_mesh(self, t0, t1, NT, timeline='uniform'):
+        if timeline is 'uniform':
+            return UniformTimeLine(t0, t1, NT)
+        elif timeline is 'chebyshev':
+            return ChebyshevTimeLine(t0, t1, NT)
+
+    def init_mesh(self, n=4):
+        mesh = self.surface.init_mesh()
+        mesh.uniform_refine(n, surface=self.surface)
+        return mesh
 
     def solution(self, p, t):
-        x = p[...,: , 0]
-        y = p[...,: , 1]
-        z = p[...,: , 2]
+        """ The exact solution
+        """
+        x = p[..., 0]
+        y = p[..., 1]
+        z = p[..., 2]
         pi = np.pi
-        sin = np.sin
-        cos = np.cos
-        u = sin(pi*x)*sin(pi*y)*sin(pi*z)*np.exp(t)
-        return u 
+        u = np.sin(pi*x)*np.sin(pi*y)*np.sin(pi*z)*np.exp(-t)
+        return u
 
-    def diffusion_coefficient(self):
-        return self.diffusionCoefficient
-
-    def source(self, p, t):        
-        x = p[...,: , 0]
-        y = p[...,: , 1]
-        z = p[...,: , 2]
+    def source(self, p, t):
+        """ The right hand side of parabolic equation
+        INPUT:
+            p: array object, N*3
+        """
+        x = p[..., 0]
+        y = p[..., 1]
+        z = p[..., 2]
         pi = np.pi
-        sin = np.sin
         cos = np.cos
-        K = self.diffusionCoefficient
-        rhs = (1+K*pi**2)*sin(pi*x)*sin(pi*y)*sin(pi*z)*np.exp(t)
+        sin = np.sin
+        r = x**2 + y**2 + z**2
+        t1 = (-1 + 2*pi**2)*sin(pi*x)*sin(pi*y)*sin(pi*z)*np.exp(-t)
+        t2 = 2*pi*np.exp(-t)*(
+                sin(pi*x)*sin(pi*y)*cos(pi*z)*z +
+                sin(pi*x)*sin(pi*z)*cos(pi*y)*y +
+                sin(pi*y)*cos(pi*x)*sin(pi*z)*x)/r
+        t3 = 2*pi**2*np.exp(-t)*(
+                sin(pi*x)*cos(pi*z)*cos(pi*y)*y*z +
+                sin(pi*y)*cos(pi*z)*cos(pi*x)*x*z +
+                cos(pi*x)*sin(pi*z)*cos(pi*y)*x*y)/r
+
+        rhs = t1 + t2 + t3
         return rhs
-
-
 
 
--- test/test_TimeIntegratorAlg.py
+++ test/test_TimeIntegratorAlg.py
@@ -56,8 +56,59 @@ class ParabolicFEMModel():
         return A, b
 
 class SurfaceParabolicFEMModel():
-    def __init__(self):
-        pass
+    def __init__(self, pde, mesh, p=1, q=6, p0=None):
+        from fealpy.functionspace import SurfaceLagrangeFiniteElementSpace
+        from fealpy.boundarycondition import DirichletBC
+        self.space = SurfaceLagrangeFiniteElementSpace(mesh, pde.surface, p=p, p0=p0, q=q)
+        self.mesh = self.space.mesh
+        self.surface = pde.surface
+        self.pde = pde
+
+        self.ftype = self.mesh.ftype
+        self.itype = self.mesh.itype
+
+        self.M = self.space.mass_matrix()
+        self.A = self.space.stiff_matrix()
+
+    def init_solution(self, timeline):
+        NL = timeline.number_of_time_levels()
+        gdof = self.space.number_of_global_dofs()
+        uh = np.zeros((gdof, NL), dtype=self.mesh.ftype)
+        return uh
+
+    def interpolation(self, timeline):
+        NL = timeline.number_of_time_levels()
+        gdof = self.space.number_of_global_dofs()
+        ps = self.space.interpolation_points()
+        uI = np.zeros((gdof, NL), dtype=self.mesh.ftype)
+        times = timeline.all_time_levels()
+        for i, t in enumerate(times):
+            uI[:, i] = self.pde.solution(ps, t)
+        return uI
+
+    def get_current_left_matrix(self, timeline):
+        dt = timeline.current_time_step_length()
+        return self.M + 0.5*dt*self.A
+
+    def get_current_right_vector(self, uh, timeline):
+        dt = timeline.current_time_step_length()
+        t0 = timeline.current_time_level()
+        t1 = timeline.next_time_level()
+        f0 = lambda x: self.pde.source(x, t0) + self.pde.source(x, t1)
+        #f0 = lambda x: self.pde.source(x, t1)
+        F = self.space.source_vector(f0)
+        return self.M@uh - 0.5*dt*(self.A@uh - F)
+
+    def apply_boundary_condition(self, A, b, timeline):
+        t1 = timeline.next_time_level()
+        bc = DirichletBC(self.space, lambda x:self.pde.solution(x, t1), self.is_boundary_dof)
+        A, b = bc.apply(A, b)
+        return A, b
+
+    def is_boundary_dof(self, p):
+        isBdDof = np.zeros(p.shape[0], dtype=np.bool)
+        isBdDof[0] = True
+        return isBdDof
 
 class TimeIntegratorAlgTest():
     def __init__(self):
@@ -86,9 +137,30 @@ class TimeIntegratorAlgTest():
         print(error[:-1]/error[1:])
         print(error)
 
+    def test_SurfaceParabolicFEMModel_time(self, maxit=4):
+        from fealpy.pde.surface_parabolic_model_3d import SinSinSinExpDataSphere
+        pde = SinSinSinExpDataSphere()
+        mesh = pde.init_mesh(n=5)
+        timeline = pde.time_mesh(0, 1, 2)
+        error = np.zeros(maxit, dtype=mesh.ftype)
+        for i in range(maxit):
+            print(i)
+            dmodel = SurfaceParabolicFEMModel(pde, mesh)
+            uh = dmodel.init_solution(timeline)
+            uI = dmodel.interpolation(timeline)
+            uh[:, 0] = uI[:, 0]
+            timeline.time_integration(uh, dmodel, self.solver.divide)
+            error[i] = np.max(np.abs(uh - uI))
+            timeline.uniform_refine()
+            mesh.uniform_refine(surface=pde.surface)
+
+        print(error[:-1]/error[1:])
+        print(error)
+
 
 
 test = TimeIntegratorAlgTest()
+test.test_SurfaceParabolicFEMModel_time()
 test.test_ParabolicFEMModel_time()
 
  
