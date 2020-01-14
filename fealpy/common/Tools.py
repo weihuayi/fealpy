e2570f3869635fc9ddeebf2f6fda33353802ae32
Huayi Wei <weihuayi@xtu.edu.cn>
2020-01-14 17:42:40 +0800

    udpate

--- fealpy/common/Tools.py
+++ fealpy/common/Tools.py
@@ -7,3 +7,19 @@ def ranges(nv, start = 0):
     id_arr[shifts[:-1]] = -np.asarray(nv[:-1])+1
     id_arr[0] = start
     return id_arr.cumsum()
+
+def hash2map(dec, ha):
+    n = ha.shape[1]
+    b = np.floor(dec.reshape(-1, 1)/2**np.arange(n))%2
+    m = np.zeros_like(b)
+    v = np.zeros_like(dec)
+    idx, jdx = np.nonzero(b)
+    for i in range(len(dec)):
+        if dec[i]:
+            flag = (idx==i)
+            pos, _ = np.nonzero(ha[:, jdx[flag]])
+            mdx = np.argmin(np.sum(np.abs(ha[pos] - b[i]), axis=-1))
+            m[i] = ha[pos[mdx]]
+            v[i] = pos[mdx]+1
+
+    return m, v
