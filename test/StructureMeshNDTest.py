86d93a97b7a8bacf913575292b3c7dcd8e0deffe
Huayi Wei <weihuayi@xtu.edu.cn>
2020-04-20 11:41:52 +0800

    udpate

--- test/StructureMeshNDTest.py
+++ test/StructureMeshNDTest.py
@@ -22,15 +22,28 @@ box = [0, 2*np.pi, 0, 2*np.pi]
 N = 4
 mesh = StructureMeshND(box, N)
 node = mesh.node
-k = mesh.ps_coefficients(flat=True)
-print('k:\n', k)
-print('node:\n', node)
+
+F0 = f(node)
+F1 = np.fft.fft2(F0)
+F2 = np.fft.ifft2(F1).real
+
+print('F0:\n', F0)
+print('F2:\n', F2)
+
+#k2 = mesh.ps_coefficients_square()
+#U = F/k2
+#U[N, N] = 0
+#U = np.fft.ifft2(U)
+#print('U:\n', U)
+#print('k2:\n', k2)
+
+
+
 
 qmesh = StructureQuadMesh(box, N, N)
 mi = qmesh.multi_index()
-
 fig = plt.figure()
 axes = fig.gca()
 qmesh.add_plot(axes)
-qmesh.find_node(axes, showindex=True, multiindex=k)
+qmesh.find_node(axes, showindex=True)
 plt.show()
