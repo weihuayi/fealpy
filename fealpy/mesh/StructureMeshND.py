c8f47166e953b6cc2443dca3b00a12a6c1c8074d
Huayi Wei <weihuayi@xtu.edu.cn>
2020-04-21 12:00:37 +0800

    udpate

--- fealpy/mesh/StructureMeshND.py
+++ fealpy/mesh/StructureMeshND.py
@@ -12,33 +12,35 @@ class StructureMeshND:
         self.itype = np.int32
 
     @property
-    def node(self, flat=False):
+    def node(self):
         N = self.N
         GD = self.GD
         box = self.box
-        node = np.mgrid[
+        node = np.ogrid[
                 tuple(
-                    slice(start, stop, complex(0, N+1))
+                    slice(start, stop-(stop - start)/N, complex(0, N))
                     for start, stop in zip(box[0::2], box[1::2])
                     )
                 ]
 
-        if flat:
-            return node.reshape(-1).reshape(((N+1)*(N+1), GD), order='F')
-        else:
-            return node
+        return node
 
-    def ps_coefficients(self, flat=False):
+    def freq(self):
         N = self.N
         GD = self.GD
-        box = self.box
-        n = N//2
-        k = np.mgrid[tuple(slice(-n, n+1) for i in range(GD))]
-        if flat:
-            return k.reshape(-1).reshape(((N+1)*(N+1), GD), order='F')
-        else:
-            return k
+        xi = np.fft.fftfreq(N)*N
+        xi = np.meshgrid(*(GD*(xi,)), sparse=True)
+        return xi
 
-    def ps_coefficients_square(self, flat=False):
-        k = self.ps_coefficients(flat=flat)
-        return np.sum(k**2, axis=0)
+    def linear_equation(self, f, cfun=lambda x: 1 + np.sum(x**2, axis=0)):
+        N = self.N
+        GD = self.GD
+        xi = self.freq()
+        F = f(self.node)[
+        F = np.fft.fft(F)
+        U = F/cfun(xi)
+        print('U1:', U1)
+        U1 = np.fft.ifft(U1).real
+
+
+
--- test/StructureMeshNDTest.py
+++ test/StructureMeshNDTest.py
@@ -8,43 +8,50 @@ from fealpy.mesh import StructureQuadMesh
 
 def u(p):
     x = p[0]
-    y = p[1]
-    pi = np.pi
-    return np.sin(pi*x)*np.sin(pi*y)
+    return np.sin(x) 
 
 def f(p):
     x = p[0]
-    y = p[1]
-    pi = np.pi
-    return (2*pi**2+1)*np.sin(pi*x)*np.sin(pi*y)
+    return 2*np.sin(x) 
 
-box = [0, 2*np.pi, 0, 2*np.pi]
-N = 4
+box = [0, 2*np.pi]
+N = 10 
 mesh = StructureMeshND(box, N)
 node = mesh.node
 
+xi = np.fft.fftfreq(N)*N
+print('xi:\n', xi)
+
 F0 = f(node)
-F1 = np.fft.fft2(F0)
-F2 = np.fft.ifft2(F1).real
+print('F0:', F0)
 
-print('F0:\n', F0)
-print('F2:\n', F2)
-
-u = u(node)
-k2 = mesh.ps_coefficients_square()
-U = F/(k2+1)
-U = np.fft.ifft2(U).real
-print('u:\n', u)
-print('U:\n', U)
-print('k2:\n', k2+1)
+F1 = np.fft.fft(F0)
+print('F1:', F1)
 
 
+U1 = F1/(1+xi**2)
+print('U1:', U1)
+U1 = np.fft.ifft(U1).real
+print('U1:', U1)
+
+U0 = u(node)
+print('U0:', U0)
+
+error = np.sqrt(np.sum((U0 - U1)**2)/N)
+print(error)
 
 
-qmesh = StructureQuadMesh(box, N, N)
-mi = qmesh.multi_index()
-fig = plt.figure()
-axes = fig.gca()
-qmesh.add_plot(axes)
-qmesh.find_node(axes, showindex=True)
-plt.show()
+
+if False:
+    k = mesh.ps_coefficients()
+    print('k:', k)
+
+    k2 = mesh.ps_coefficients_square()
+    print('k2:', k2)
+    qmesh = StructureQuadMesh(box, N, N)
+    mi = qmesh.multi_index()
+    fig = plt.figure()
+    axes = fig.gca()
+    qmesh.add_plot(axes)
+    qmesh.find_node(axes, showindex=True)
+    plt.show()
