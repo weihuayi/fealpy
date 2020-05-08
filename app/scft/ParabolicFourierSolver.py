0cb4e84efe495a43888cc3be7ed9339c587b4743
Huayi Wei <weihuayi@xtu.edu.cn>
2020-05-08 11:42:35 +0800

    update

--- app/scft/ParabolicFourierSolver.py
+++ app/scft/ParabolicFourierSolver.py
@@ -1,26 +1,30 @@
 
 import numpy as np
 
-
-class ParabolicFourierModel():
-    def __init__(self, space, timeline, q0, w):
+class ParabolicFourierSolver():
+    def __init__(self, space, timeline):
         self.space = space 
         self.k, self.k2 = self.space.reciprocal_lattice(return_square=True)
-
         self.timeline = timeline 
-
         dt = self.timeline.current_time_step_length()
-        self.q0 = q0 # initial value
-        self.w = w # field
-
-        # 
-        self.E0 = np.exp(-dt/2*w)
         self.E1 = np.exp(-dt*self.k2)
-        # half time step size   
-        self.E2 = np.exp(-dt/4*w)
         self.E3 = np.exp(-dt/2*self.k2)
 
-    def init_solution(self):
+    def initialize(self, q, w):
+        """
+        Parameters
+        ----------
+
+        q : 
+        w :
+
+        Note
+        ----
+        """
+
+        self.E0 = np.exp(-dt/2*w)
+        self.E2 = np.exp(-dt/4*w)
+
         NL = self.timeline.number_of_time_levels()
 
         E0 = self.E0
@@ -28,9 +32,6 @@ class ParabolicFourierModel():
 
         E2 = self.E2
         E3 = self.E3 
-
-        q = self.space.function(dim=NL)
-        q[0] = self.q0 
 
         for i in range(1, 4):
             q0 = q[i-1]
@@ -52,7 +53,6 @@ class ParabolicFourierModel():
             q1 *= E2
             q[i] *= -1/3
             q[i] += 4*q1/3
-        return q
 
     def solve(self, q): 
         NL = self.timeline.number_of_time_levels()
@@ -71,25 +71,3 @@ class ParabolicFourierModel():
             q1 = np.fft.fftn(q0)
             q1 /= 25/12 + dt*k2
             q[i] = np.fft.ifftn(q1).real
-            
-
-
-
-if __name__ == "__main__":
-
-    box = np.array([
-        [2*np.pi, 0],
-        [0, 2*np.pi]])
-
-    T = [0, 1]
-    NS = 4
-    NT = 10 
-    w = 0
-    q0 = 1
-
-    model = ParabolicFourierModel(box, T, NS, NT, q0, w)
-    q = model.init_solution()
-    model.solve(q)
-    print(q)
-
-
--- app/scft/SCFTA1BA2CLinearModel.py
+++ app/scft/SCFTA1BA2CLinearModel.py
@@ -2,6 +2,7 @@
 import numpy as np
 from fealpy.functionspace import FourierSpace
 from fealpy.timeintegratoralg.timeline_new import UniformTimeLine
+from ParabolicFourierSolver import ParabolicFourieeSolver
 
 def model_options(
         nspecies = 3,
@@ -18,7 +19,10 @@ def model_options(
         chiBC = 0.25,
         dim = 2,
         NS = 16,
-        maxdt = 0.01):
+        maxdt = 0.01,
+        bA = 1,
+        bB = 1,
+        bC = 1):
         # the parameter for scft model
         options = {
                 'nspecies': nspecies,
@@ -35,7 +39,10 @@ def model_options(
                 'chiBC': chiBC,
                 'dim': dim,
                 'NS' : NS,
-                'maxdt' : = maxdt
+                'maxdt' : = maxdt,
+                'bA': bA,
+                'bB': bB,
+                'bC': bC
                 }
         return options
 
@@ -55,11 +62,111 @@ class SCFTA1BA2CLinearModel():
         fC  = options['fC']
         maxdt = options['maxdt']
 
-        self.timeline0 = UniformTimeLine(0, fA1, int(fA1//maxdt))
-        self.timeline1 = UniformTimeLine(0, fB, int(fB//maxdt))
-        self.timeline2 = UniformTimeLine(0, fA2, int(fA2//maxdt))
-        self.timeline3 = UniformTimeLine(0, fC, int(fC//maxdt))
+        self.timelines = []
+        self.timelines.append(UniformTimeLine(0, fA1, int(fA1//maxdt)))
+        self.timelines.append(UniformTimeLine(0, fB, int(fB//maxdt)))
+        self.timelines.append(UniformTimeLine(0, fA2, int(fA2//maxdt)))
+        self.timelines.append(UniformTimeLine(0, fC, int(fC//maxdt)))
 
-        self.pdesolver0 = ParabolicFourierModel(self.space, self.timeline0) 
+        self.pdesolvers = []
+        for i in range(4):
+            self.pdesolvers.append(
+                    ParabolicFourierSolver(self.space, self.timelines[i])
+                    )
+
+        TNL = 0 # total number of time levels
+        for i in range(4):
+            TNL += self.timelines[i].number_of_time_levels()
+        TNL -= options['nblock'] - 1
+
+        self.qf = self.space.function(dim=TNL) # forward  propagator 
+        self.qb = self.space.function(dim=TNL) # backward propagator
+
+        self.qf[0] = 1
+        self.qb[0] = 1
+
+        self.rho = self.space.function(dim=options['nspecies'])
+        self.grad = self.space.function(dim=options['nspecies']+1)
+        self.Q = np.zeros(options['nblend'], dtype=np.float)
+        self.w = self.space.function(dim=options['nspecies']+1)
+
+    def __call__(self, w, returngrad=True):
+        """
+        目标函数，给定外场，计算哈密尔顿量及其梯度
+        """
+
+        # solver the forward and backward equation
+        self.compute_propagator(w)
+        # compute single chain partition function Q
+        self.compute_single_Q()
+        # compute density
+        self.compute_density()
+        # compute energy function and its gradient
+        self.compute_energe()
 
 
+    def compute_energe(self, w):
+        chiABN = options['chiAB']*options['ndeg']
+        chiBCN = options['chiBC']*options['ndeg']
+        chiACN = options['chiAC']*options['ndeg']
+        rho = self.rho
+
+        E = chiABN*rho[0]*rho[1] 
+        E += chiBCN*rho[1]*rho[2]
+        E += chiACN*rho[0]*rho[2]
+        E -= w[1]*rho[0]
+        E -= w[2]*rho[1]
+        E -= w[3]*rho[2]
+        E -= w[0]*(1 - rho.sum(axis=0))
+        E = np.fft.fftn(E)
+        self.H = np.real(E.flat[0])
+        self.H -= np.log(self.Q[0])
+
+
+    def compute_propagator(self, w):
+
+        qf = self.qf
+        qb = self.qb
+
+        start = 0
+        F = [w[1], w[2], w[1], w[3]]
+        for i in range(options['nblock']):
+            NL = self.timelines[i].number_of_time_levels()
+            self.pdesolvers[i].initialize(self.qf[start:start + NL], F[i])
+            start += NL - 1
+
+        start = 0
+        F = [w[3], w[1], w[2], w[1]]
+        for i in range(options['nblock']):
+            NL = self.timelines[i].number_of_time_levels()
+            self.pdesolvers[i].initialize(self.qb[start:start + NL], F[i])
+            start += NL - 1
+
+
+    def compute_single_Q(self):
+        q = self.qf[-1]
+        q = np.fft.fftn(q)
+        self.Q[0] = np.real(q.flat[0])
+
+
+    def compute_density(self):
+        q = self.qf*self.qb[-1::-1]
+
+        start = 0
+        rho = []
+        for i in range(options['nblock']):
+            NL = self.timelines[i].number_of_time_levels()
+            dt = self.timelines[i].current_time_step_length()
+            rho.append(self.integral_time(q[start:start+NL], dt))
+            start += NL - 1
+
+        self.rho[0] = rho[0] + rho[2]
+        self.rho[1] = rho[1]
+        self.rho[2] = rho[3]
+        self.rho /= self.Q[0]
+
+    def integral_time(self, q, dt):
+        f = -0.625*(q[0] + q[-1]) + 1/6*(q[1] + q[-2]) - 1/24*(q[2] + q[-3])
+        f += np.sum(q, axis=0)
+        f *= dt
+        return f
