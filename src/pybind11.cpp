3fe9d95a21a6f2e40301e66defe805ef3abb4bb4
Huayi Wei <weihuayi@xtu.edu.cn>
2020-05-13 08:19:25 +0800

    update

--- src/pybind11.cpp
+++ src/pybind11.cpp
@@ -30,22 +30,48 @@ FT sphere_function (Point_3 p) {
 
 
 namespace py = pybind11;
-// std::tuple <py::array_t<double>, py::array_t<int> > 
+std::tuple< py::array_t<double>, py::array_t<int> > 
 void generate_surface_mesh()
 {
-  Tr tr; 
-  C2t3 c2t3 (tr); 
+    typedef typename C2t3::Triangulation Tr;
+    typedef typename Tr::Finite_facets_iterator Finite_facets_iterator;
+    typedef typename Tr::Finite_vertices_iterator Finite_vertices_iterator;
+    typedef typename Tr::Vertex_handle Vertex_handle;
 
-  Surface_3 surface(sphere_function,             
-                    Sphere_3(CGAL::ORIGIN, 2.)); 
-
-  CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,  // angular bound
+    Tr tr; 
+    C2t3 c2t3 (tr); 
+    Surface_3 surface(sphere_function, Sphere_3(CGAL::ORIGIN, 2.)); 
+    CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,  // angular bound
                                                      0.1,  // radius bound
                                                      0.1); // distance bound
-  CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+    CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+    auto NN = c2t3.number_of_vertices();
+    auto NC = c2t3.number_of_facets();
 
-  std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
+    double * node = new double[3*NN];
+    int * cell = new double[3*NC];
 
+    std::map<Vertex_handle, int> V;
+    int inum = 0;
+    for(auto vit = tr.finite_vertices_begin();
+      vit != tr.finite_vertices_end(); ++vit)
+    {
+        V[vit] = inum++;
+    } 
+
+    for(auto fit = tr.finite_facets_begin(); 
+            fit != tr.finite_facets_end(); ++fit)
+    {
+        const auto face = fit->first;
+        const int& index = fit->second;
+        if (face->is_facet_on_surface(index)==true)
+        {
+            const int index1 = V[face->vertex(tr.vertex_triple_index(index, 0))];
+            const int index2 = V[face->vertex(tr.vertex_triple_index(index, 1))];
+            const int index3 = V[face->vertex(tr.vertex_triple_index(index, 2))];
+        }
+    }
+    std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
 }
 
 int add(int i, int j)
