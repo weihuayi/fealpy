f0c91fc428a4d861af07f410792854b2fa9bd2cc
Li Ao <1822326109@qq.com>
2020-07-23 17:27:58 +0800

    update

--- example/OCP_example.py
+++ example/OCP_example.py
@@ -58,7 +58,7 @@ class PDE():
         x = p[..., 0]
         y = p[..., 1]
         pi = np.pi
-        val = np.zeros(point.shape, dtype=np.float64)
+        val = np.zeros(p.shape, dtype=np.float64)
         val[..., 0] = pi*np.cos(pi*x)*np.sin(pi*y)*np.exp(2*t)
         val[..., 1] = pi*np.sin(pi*x)*np.cos(pi*y)*np.exp(2*t)
         return val # val.shape == x.shape
@@ -69,7 +69,7 @@ class PDE():
         x = p[..., 0]
         y = p[..., 1]
         pi = np.pi
-        val = np.zeros(point.shape, dtype=np.float64)
+        val = np.zeros(p.shape, dtype=np.float64)
         val[..., 0] = pi*np.cos(pi*x)*np.sin(pi*y)*(np.exp(2*t)+1)/2
         val[..., 1] = pi*np.sin(pi*x)*np.cos(pi*y)*(np.exp(2*t)+1)/2
         return val # val.shape == x.shape
@@ -168,13 +168,17 @@ class Model():
 
 
         self.A = self.uspace.stiff_matrix() # RT 质量矩阵
-        self.D = self.uspace.div_matrix() # TODO: 确定符号
+        self.D = self.uspace.div_matrix() # TODO: 确定符号, 符号为正
         data = self.cellmeasure*np.ones(NC, )
         self.M = spdiags(data, 0, NC, NC)
 
 #        self.M = self.pspace.cell_mass_matrix() # 每个单元上是 1 x 1 的矩阵i
 
     def get_state_current_right_vector(self, sp, nt):
+        '''
+        sp: (NE, 1), 前 n-1 层的累加
+        nt: 表示 n-1 时间层,要求的是 n 个时间层的方程
+        '''
         dt = self.dt
         f1 = dt*sp
         NC = self.mesh.number_of_cells()
@@ -188,7 +192,7 @@ class Model():
         bcs, ws = qf.get_quadrature_points_and_weights()
         ps = mesh.bc_to_point(bcs, etype='cell')
         phi = self.pspace.basis(bcs)
-        val = pde.source(ps, nt*dt)
+        val = self.pde.source(ps, nt*dt)
         bb1 = np.einsum('i, ij, jk, j->jk', ws, val, phi, self.cellmeasure)
         bb2 = self.M@u
         bb = bb1 + bb2
@@ -198,16 +202,20 @@ class Model():
         b = np.zeros(shape, dtype=phi.dtype)
         np.add.at(b, cell2dof, bb)
 
-        f3 = dt*b + self.yh[:, nt]
+        f3 = dt*b + self.M@self.yh[:, nt]
 
         return np.r_[np.r_[f1, f2], f3]
 
     def get_costate_current_right_vector(self, sq, nt):
+        '''
+        sq: (NE, 1) n 到 N 时间层的累加
+        nt: 正序的 n-1 个时间层的值, 则逆序所对应的时间层为 NL - nt -1
+        NL: 时间方向剖分的总的层数
+        '''
         dt = self.dt
         NL = timeline.number_of_time_levels()
         f1 = -dt*sq
         NC = self.mesh.number_of_cells()
-#        print('tn', NL - nt-1)
         tpd = self.uspace.function()
         edge2dof = self.uspace.dof.edge_to_dof()
         en = self.mesh.edge_unit_normal()
@@ -229,17 +237,18 @@ class Model():
         return np.r_[np.r_[f1, f2], f3]
 
     def state_one_step_solve(self, t, sp):
-
+        dt = self.dt
         F = self.get_state_current_right_vector(sp, t)
-        A = bmat([[self.A, None, None],[None, self.A, self.D], \
+        A = bmat([[self.A, (dt-1)*self.A, None],[None, self.A, self.D], \
                 [-self.dt*self.D.T, None, self.M]], format='csr')
         PU = spsolve(A, F)
         return PU
 
     def costate_one_step_solve(self, t, sq):
+        dt = self.dt
         F = self.get_costate_current_right_vector(sq, t)
-        A = bmat([[self.A, None, None],[None, self.A, -self.D], \
-                [self.dt*self.D.T, None, -self.M]], format='csr')
+        A = bmat([[self.A, (dt-1)*self.A, None],[None, self.A, -self.D], \
+                [self.dt*self.D.T, None, self.M]], format='csr')
         QZ = spsolve(A, F)
         return QZ
 
@@ -251,13 +260,11 @@ class Model():
         sp = np.zeros(NE, dtype=mesh.ftype)
         while not timeline.stop():
 #            self.state_solve()
-#            print('time', timeline.current)
             PU = self.state_one_step_solve(timeline.current, sp)
             self.tph[:, timeline.current+1] = PU[:NE]
             self.ph[:] = PU[NE:2*NE]
             self.yh[:, timeline.current+1] = PU[2*NE:]
             timeline.current += 1
-#            print('time', timeline.current)
             sp = sp + self.A@self.ph[:] 
         timeline.reset()
         return sp
@@ -270,16 +277,13 @@ class Model():
         qf = self.integrator
         bcs, ws = qf.get_quadrature_points_and_weights()
         ps = mesh.bc_to_point(bcs, etype='cell')
-#        print('ps', ps.shape)
         sq = np.zeros(NE, dtype=mesh.ftype)
         while not timeline.stop():
-#            self.state_solve()
             QZ = self.costate_one_step_solve(timeline.current, sq)
             self.qh[:] = QZ[NE:2*NE]
             self.zh[:, NL-timeline.current-2] = QZ[2*NE:]
             zh1 = self.pspace.function(array=self.zh[:, NL - timeline.current-2])
             val = zh1(ps)
-#            print('val', val.shape)
             e = np.einsum('i, ij, j->j', ws, val, self.cellmeasure)
             self.uh[:, NL-timeline.current-1] = max(0, np.sum(e)/np.sum(self.cellmeasure)) \
                             - self.zh[:, NL - timeline.current-2]
@@ -289,10 +293,9 @@ class Model():
             timeline.current += 1
             sq = sq + self.qh
         timeline.reset()
-        print('uh2', self.zh)
         return sq
 
-    def nonlinear_solve(self, maxit=4):
+    def nonlinear_solve(self, maxit=20):
 
         eu = 1
         k = 1
@@ -307,6 +310,8 @@ class Model():
         timeline.reset()
         while (eu > 1e-4) and (k < maxit):
             print('ui', uI)
+            uh1 = self.uh.copy()
+            print('uh1', uh1)
             sp = self.state_solve()
             sq = self.costate_solve()
             print('uh', self.uh)
@@ -314,6 +319,28 @@ class Model():
             k = k + 1
             print('eu', eu)
             print('k', k)
+
+            return sp, sq
+
+    def L2error(self):
+        dt = self.dt
+        NL = timeline.number_of_time_levels()
+        T = dt*(NL-1)
+
+#        def f(bc, t):
+#            xx = mesh.bc_to_point(bc)
+#            return (self.pde.p_solution(xx, t) - self.ph(xx))**2
+#        pL2error = self.uspace.integralalg.integral(lambda x: f(x, T))
+        pL2error = self.uspace.integralalg.L2_error(lambda x: \
+                self.pde.p_solution(x, T), self.ph)
+        print(pL2error)
+        tpL2error = self.uspace.integralalg.L2_error(lambda x: \
+                self.pde.tp_solution(x, T), \
+                self.uspace.function(array=self.tph[:, NL-1])) 
+        print(tpL2error)
+
+
+        return pL2error
 
 
         
@@ -323,6 +350,7 @@ space = RaviartThomasFiniteElementSpace2d(mesh, p=0)
 timeline = UniformTimeLine(0, 1, 2)
 MFEMModel = Model(pde, mesh, timeline)
 MFEMModel.nonlinear_solve()
+pL2error = MFEMModel.L2error()
 #state = StateModel(pde, mesh, timeline)
 
 
