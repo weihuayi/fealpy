64173b778740f89bfcef2abd09a0b637524fbec0
Huayi Wei <weihuayi@xtu.edu.cn>
2020-08-05 06:44:35 +0800

    update

---
+++ fealpy/mesh/vtk_extent.py
@@ -0,0 +1,90 @@
+"""
+
+Notes
+-----
+    FEALPy 的 VTK 的扩展模块
+
+Authors
+------
+Huayi Wei, weihuayi@xtu.edu.cn
+"""
+import numpy as np
+import vtk
+import vtk.util.numpy_support as vnp
+
+from .vtkCellTypes import *
+
+def vtk_cell_index(p, celltype):
+    """
+
+    Notes
+    -----
+        获取 vtk cell 的相对于 fealpy 网格 cell 的顶点编号规则，用于把 FEALPy 中
+        的 cell 顶点编号顺序转化为 vtk 的编号顺序。
+    """
+    if celltype == VTK_LAGRANGE_CURVE: 
+        return np.arange(p+1, dtype=np.int_)
+    elif celltype == VTK_LAGRANGE_TRIANGLE:
+        tri = vtk.vtkLagrangeTriangle()
+        ldof = (p + 1)*(p + 2)//2
+        tri.GetPointIds().SetNumberOfIds(ldof)
+        tri.GetPoints().SetNumberOfPoints(ldof)
+        tri.Initialize()
+        multiIndex = np.zeros((ldof, 3), dtype=np.int_)
+        for i in range(ldof):
+            tri.GetPointIds().SetId(i, i)
+            tri.ToBarycentricIndex(i, multiIndex[i])
+        s = np.sum(multiIndex[:, 1:], axis=-1)
+        index = s*(s+1)//2 + multiIndex[:, 2]
+        return index
+    elif celltype == VTK_LAGRANGE_TETRAHEDRON:
+        tet = vtk.vtkLagrangeTetra()
+        ldof = (p + 1)*(p + 2)*(p + 3)//6
+        tet.GetPointIds().SetNumberOfIds(ldof)
+        tet.GetPoints().SetNumberOfPoints(ldof)
+        tet.Initialize()
+        multiIndex = np.zeros((ldof, 4), dtype=np.int_)
+        for i in range(ldof):
+            tet.GetPointIds().SetId(i, i)
+            tet.ToBarycentricIndex(i, multiIndex[i])
+        s0 = np.sum(multiIndex[:, 1:], axis=-1)
+        s1 = np.sum(multiIndex[:, 2:], axis=-1)
+        index = s0*(s0+1)*(s0+2)//6 + s1*(s1+1)//2 + multiIndex[:, 3]
+
+def write_to_vtu(fname, node, NC, cellType, cell, nodedata=None, celldata=None):
+    """
+
+    Notes
+    -----
+    """
+    points = vtk.vtkPoints()
+    points.SetData(vnp.numpy_to_vtk(node))
+
+    cells = vtk.vtkCellArray()
+    cells.SetCells(NC, vnp.numpy_to_vtkIdTypeArray(cell))
+
+    mesh =vtk.vtkUnstructuredGrid() 
+    mesh.SetPoints(points)
+    mesh.SetCells(cellType, cells)
+
+    pdata = mesh.GetPointData()
+
+    if nodedata is not None:
+        for key, val in nodedata.items():
+            if val is not None:
+                d = vnp.numpy_to_vtk(val[:])
+                d.SetName(key)
+                pdata.AddArray(d)
+
+    if celldata is not None:
+        cdata = mesh.GetCellData()
+        for key, val in celldata.items():
+            if val is not None:
+                d = vnp.numpy_to_vtk(val[:])
+                d.SetName(key)
+                cdata.AddArray(d)
+    writer = vtk.vtkXMLUnstructuredGridWriter()
+    writer.SetFileName(fname)
+    writer.SetInputData(mesh)
+    writer.Write()
+
