fa31825137252e2c09f893d2fdd1b0bc171bc11b
Huayi Wei <weihuayi@xtu.edu.cn>
2020-08-14 19:39:44 +0800

    update

--- app/shalegas/parabolic_test.py
+++ app/shalegas/parabolic_test.py
@@ -1,52 +1,4 @@
 
-"""
-
-Notes
------
-
-    1. 混合物的摩尔浓度 c 的计算公式为
-        c = p/(ZRT), 
-        Z^3 - (1 - B)Z^2 + (A - 3B^2 -2B)Z - (AB - B^2 -B^3) = 0
-        A = a p/(R^2T^2)
-        B = b p/(RT)
-        其中:
-
-        a = 3
-        b = 1/3
-
-        p 是压力 
-        Z 是压缩系数 
-        R 是气体常数
-        T 是温度
-        M_i 是组分 i 的摩尔质量
-        
-        混合物的密度计算公式为:
-
-        rho = M_0 c_0 + M_1 c_1 + M_2 c_2 ...
-
-    2. c_i : 组分 i 的摩尔浓度
-       z_i : 组分 i 的摩尔分数
-
-       c_i = z_i c
-
-    3. 
-    甲烷: methane,  CH_4,   16.04 g/mol,  0.42262 g/cm^3
-    乙烷: Ethane, C_2H_6,   30.07 g/mol, 1.212 kg/m^3
-    丙烷: Propane,  C_3H_8,  44.096 g/mol,  1.83 kg/m^3 
-    (25度 100 kPa
-
-    4. 气体常数  R = 8.31446261815324 	J/K/mol
-
-    5. 6.02214076 x 10^{23}
-
-
-References
-----------
-[1] https://www.sciencedirect.com/science/article/abs/pii/S0378381217301851
-
-Authors
-    Huayi Wei, weihuayi@xtu.edu.cn
-"""
 import numpy as np
 from numpy.linalg import inv
 from scipy.sparse import csr_matrix, bmat, spdiags
@@ -104,7 +56,7 @@ class Model_1():
         mesh = mf.boxmesh2d(box, nx=n, ny=n, meshtype='tri')
         return mesh
 
-    def time_mesh(self, n=1):
+    def time_mesh(self, n=1000):
         timeline = UniformTimeLine(0, 1, n)
         return timeline
 
@@ -170,13 +122,18 @@ class Model_1():
         -----
         在区域左下角给一个速度边界条件 v\cdot n
         """
-        return -0.1 
+        x = p[..., 0]
+        y = p[..., 1]
+        flag = (x < 1) & (y < 1)
+        val = np.zeros(p.shape[0:-1], dtype=p.dtype)
+        val[flag] = -0.01
+        return val 
 
     @cartesian
     def is_velocity_bc(self, p):
         x = p[..., 0]
         y = p[..., 1]
-        flag = (x < 1) & (y < 1)
+        flag = (x < 49) | (y < 49)
         return flag
 
     @cartesian
@@ -195,46 +152,21 @@ class Model_1():
         flag = (x > 49) & (y > 49)
         return flag
 
-class ShaleGasSolver():
+class ParabolicMFEMSolver():
+
     def __init__(self, model):
         self.model = model
         self.mesh = model.space_mesh()
-        self.timeline =  model.time_mesh() 
-        self.uspace = RaviartThomasFiniteElementSpace2d(self.mesh, p=0)
-        self.cspace = ScaledMonomialSpace2d(self.mesh, p=1) # 线性间断有限元空间
+        self.timeline = model.time_mesh()
+        dt = self.timeline.current_time_step_length()
+        self.space = RaviartThomasFiniteElementSpace2d(self.mesh, p=0)
+        self.uh = self.space.function()
+        self.ph = model.init_pressure(self.space.smspace)
 
-        self.uh = self.uspace.function() # 速度
-        self.ph = model.init_pressure(self.uspace.smspace) # 初始压力
-
-        # 三个组分的摩尔密度, 三个组分一起计算 
-        self.ch = model.init_molar_density(self.cspace) 
-
-        # TODO：初始化三种物质的浓度
-        self.options = {
-                'viscosity': 1.0,    # 粘性系数
-                'permeability': 1.0, # 渗透率 
-                'temperature': 397, # 初始温度 K
-                'pressure': 50,   # 初始压力
-                'porosity': 0.2,  # 孔隙度
-                'injecttion_rate': 0.1,  # 注入速率
-                'compressibility': 0.001, #压缩率
-                'pmv': (1.0, 1.0, 1.0), # 偏摩尔体积
-                'rdir': '/home/why/result/test/',
-                'step': 1} 
-        self.CM = self.cspace.cell_mass_matrix() 
-        self.H = inv(self.CM)
-
-        c = self.options['viscosity']/self.options['permeability']
-        self.M = c*self.uspace.mass_matrix()
-        self.B = -self.uspace.div_matrix()
-
-        dt = self.timeline.dt
-        c = self.options['porosity']*self.options['compressibility']/dt
-        self.D = c*self.uspace.smspace.mass_matrix() 
-
-        # 压力边界条件
-        self.F0 = -self.uspace.set_neumann_bc(
-                model.pressure_bc, threshold=model.is_pressure_bc)
+        self.M = self.space.mass_matrix()
+        self.B = -self.space.div_matrix()
+        self.D = self.space.smspace.mass_matrix()/dt
+        self.F0 = -self.space.set_neumann_bc(model.pressure_bc, threshold=model.is_pressure_bc) 
 
         # vtk 文件输出
         node, cell, cellType, NC = self.mesh.to_vtk()
@@ -245,41 +177,24 @@ class ShaleGasSolver():
         self.cellType = cellType
 
     def one_step_solve(self):
-        """
 
-        Notes
-        -----
-            求解一个时间层的数值解
-        """
-        udof = self.uspace.number_of_global_dofs()
-        pdof = self.uspace.smspace.number_of_global_dofs()
-        gdof = udof + pdof
-
-        cdof = self.cspace.number_of_global_dofs()
-
-        timeline = self.timeline
-        phi = self.options['porosity'] # 孔隙度
-        dt = timeline.current_time_step_length()
-        nt = timeline.next_time_level()
-
-        # 1. 求解下一时间层的速度和压力
-        M = self.M
-        B = self.B
-        D = self.D
-        E = self.uspace.pressure_matrix(self.ch)
-
-        F1 = D@self.ph
-
-        AA = bmat([[M, B], [E, D]], format='csr')
+        AA = bmat([[self.M, self.B], [self.B.T, self.D]], format='csr')
+        F1 = self.D@self.ph
         FF = np.r_['0', self.F0, F1]
 
-        isBdDof = self.uspace.set_dirichlet_bc(self.uh, 
-                self.model.velocity_bc, threshold=self.model.is_velocity_bc)
-        x = np.r_['0', self.uh, self.ph] 
+        udof = self.space.number_of_global_dofs()
+        pdof = self.space.smspace.number_of_global_dofs()
+        gdof = udof + pdof
+
+        uh = self.space.function()
+        ph = self.space.smspace.function()
+        isBdDof = self.space.set_dirichlet_bc(uh, 
+                model.velocity_bc, threshold=model.is_velocity_bc)
+        x = np.r_['0', uh, ph] 
         isBdDof = np.r_['0', isBdDof, np.zeros(pdof, dtype=np.bool_)]
-        
+
         FF -= AA@x
-        bdIdx = np.zeros(gdof, dtype=np.int)
+        bdIdx = np.zeros(gdof, dtype=np.int_)
         bdIdx[isBdDof] = 1
         Tbd = spdiags(bdIdx, 0, gdof, gdof)
         T = spdiags(1-bdIdx, 0, gdof, gdof)
@@ -290,17 +205,7 @@ class ShaleGasSolver():
         self.uh[:] = x[:udof]
         self.ph[:] = x[udof:]
 
-        # 2. 求解下一层的浓度
-        nc = self.ch.shape[1] # 这里有 nc 个组分
-        for i in range(nc):
-            g = lambda x : self.model.concentration_bc(x, n=i)
-            F = self.uspace.convection_vector(nt, self.ch.index(i), self.uh,
-                    g=g) 
-            F = self.H@(F[:, :, None]/phi)
-            F *= dt
-            self.ch[:, i] += F.flat
-
-    def solve(self):
+    def solve(self, rdir='.'):
         """
 
         Notes
@@ -309,8 +214,6 @@ class ShaleGasSolver():
         计算所有的时间层。
         """
 
-        rdir = self.options['rdir']
-        step = self.options['step']
         timeline = self.timeline
         dt = timeline.current_time_step_length()
         timeline.reset() # 时间置零
@@ -321,10 +224,9 @@ class ShaleGasSolver():
         while not timeline.stop():
             self.one_step_solve()
             timeline.current += 1
-            if timeline.current%step == 0:
-                fname = rdir + '/test_'+ str(timeline.current).zfill(10) + '.vtu'
-                print(fname)
-                self.write_to_vtk(fname)
+            fname = rdir + '/test_'+ str(timeline.current).zfill(10) + '.vtu'
+            print(fname)
+            self.write_to_vtk(fname)
         timeline.reset()
 
     def write_to_vtk(self, fname):
@@ -350,31 +252,14 @@ class ShaleGasSolver():
         val.SetName('pressure')
         cdata.AddArray(val)
 
-        ch = self.ch
-        val = ch.value(ps)
-        if len(ch.shape) == 2:
-            for i in range(ch.shape[1]):
-                val0 = vnp.numpy_to_vtk(val[:, i])
-                val0.SetName('concentration' + str(i))
-                cdata.AddArray(val0)
-        else:
-            val = vnp.numpy_to_vtk(val)
-            val.SetName('concentration')
-            cdata.AddArray(val)
-            
-
         writer = vtk.vtkXMLUnstructuredGridWriter()
         writer.SetFileName(fname)
         writer.SetInputData(vmesh)
         writer.Write()
 
 
-
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
 
     model = Model_1()
-
-    solver = ShaleGasSolver(model)
+    solver = ParabolicMFEMSolver(model)
     solver.solve()
-
