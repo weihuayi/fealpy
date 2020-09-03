e006a0e685bd297ed0bed47a95c6d1178da16da9
Huayi Wei <weihuayi@xtu.edu.cn>
2020-09-03 20:28:24 +0800

    update

--- fealpy/solver/fast_solver.py
+++ fealpy/solver/fast_solver.py
@@ -16,6 +16,62 @@ class IterationCounter(object):
         if self._disp:
             print('iter %3i\trk = %s' % (self.niter, rk))
 
+class GaussSeidelSmoother():
+    def __init__(self, A, isDDof=None):
+        """
+
+        Notes
+        -----
+
+        """
+        if isDDof is not None:
+            # 处理 D 氏 自由度条件
+            gdof = len(isDDof)
+            bdIdx = np.zeros(gdof, dtype=np.int_)
+            bdIdx[isDDof] = 1 
+            Tbd = spdiags(bdIdx, 0, gdof, gdof)
+            T = spdiags(1-bdIdx, 0, gdof, gdof)
+            A = T@A@T + Tbd
+
+        self.L0 = tril(A).tocsr()
+        self.U0 = triu(A, k=1).tocsr()
+
+        self.U1 = self.L0.T.tocsr()
+        self.L1 = self.U0.T.tocsr()
+
+    def smooth(self, b, lower=True, maxit=100):
+        r = np.zeros_like(b)
+        if lower:
+            for i in range(maxit):
+                r[:] = spsolve_triangular(self.L0, b-self.U0@r, lower=lower)
+        else:
+            for i in range(maxit):
+                r[:] = spsolve_triangular(self.U1, b-self.L1@r, lower=lower)
+        return r
+
+class JacobiSmoother():
+    def __init__(self, A, isDDof=None):
+        if isDDof is not None:
+            # 处理 D 氏 自由度条件
+            gdof = len(isDDof)
+            bdIdx = np.zeros(gdof, dtype=np.int_)
+            bdIdx[isDDof] = 1 
+            Tbd = spdiags(bdIdx, 0, gdof, gdof)
+            T = spdiags(1-bdIdx, 0, gdof, gdof)
+            A = T@A@T + Tbd
+
+        self.D = A.diagonal() 
+        self.L = tril(A, k=-1).tocsr()
+        self.U = triu(A, k=1).tocsr()
+
+    def smooth(self, b, maxit=100):
+        r = b.copy()
+        for i in range(maxit):
+            r[:] = b - self.L@r - self.U@r
+            r /= self.D
+        return r
+
+
 class HighOrderLagrangeFEMFastSolver():
     def __init__(self, A, F, P, I, isBdDof):
         """
@@ -32,6 +88,21 @@ class HighOrderLagrangeFEMFastSolver():
         self.F = F # 右端 (gdof, ), 注意这里也没有处理 D 氏边界
         self.I = I # 插值矩阵 (gdof, NN), 把线性元的解插值到 p 次解
         self.isBdDof = isBdDof
+
+        # 获得磨光子
+        gdof = self.gdof
+        bdIdx = np.zeros(gdof, dtype=np.int_)
+        bdIdx[isBdDof] = 1 # 这里假定 A 的前 NN 个自由度是网格节点
+        Tbd = spdiags(bdIdx, 0, gdof, gdof)
+        T = spdiags(1-bdIdx, 0, gdof, gdof)
+        A = T@A@T + Tbd
+
+        self.L0 = tril(A).tocsr()
+        self.U0 = triu(A, k=1).tocsr()
+
+        self.U1 = self.L0.T.tocsr()
+        self.L1 = self.U0.T.tocsr()
+
 
         # 处理预条件子的边界条件
         NN = P.shape[0]
@@ -59,9 +130,22 @@ class HighOrderLagrangeFEMFastSolver():
         return r
 
     def preconditioner(self, b):
+        b = self.smooth(b, lower=True, m=3)
         b = self.I.T@b
-        r = self.ml.solve(b, tol=1e-8, accel='cg')       
-        return self.I@r
+        b = self.ml.solve(b, tol=1e-8, accel='cg')       
+        b = self.I@b
+        b = self.smooth(self.I@b, lower=False, m=3)
+        return b
+
+    def smooth(self, b, lower=True, m=3):
+        r = np.zeros_like(b)
+        if lower:
+            for i in range(m):
+                r[:] = spsolve_triangular(self.L0, b-self.U0@r, lower=lower)
+        else:
+            for i in range(m):
+                r[:] = spsolve_triangular(self.U1, b-self.L1@r, lower=lower)
+        return r
 
     @timer
     def solve(self, uh, F, tol=1e-8):
@@ -84,7 +168,7 @@ class HighOrderLagrangeFEMFastSolver():
         P = LinearOperator((gdof, gdof), matvec=self.preconditioner)
                 
         counter = IterationCounter()
-        uh[:], info = cg(A, F, tol=tol, callback=counter)
+        uh[:], info = cg(A, F, M=P, tol=tol, callback=counter)
         print("Convergence info:", info)
         print("Number of iteration of cg:", counter.niter)
 
