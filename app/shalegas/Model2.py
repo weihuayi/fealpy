17508d8ed19680abb53b6eeedb7a062c9550693a
Huayi Wei <weihuayi@xtu.edu.cn>
2020-09-06 11:20:11 +0800

    update

--- app/shalegas/Model2.py
+++ app/shalegas/Model2.py
@@ -89,6 +89,10 @@ https://www.coursehero.com/file/14152920/Rel-Perm/
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+from scipy.sparse import csr_matrix, coo_matrix, bmat, diags
+
 from fealpy.decorator import barycentric
 from fealpy.functionspace import LagrangeFiniteElementSpace
 from fealpy.functionspace import RaviartThomasFiniteElementSpace2d
@@ -102,17 +106,17 @@ class WaterFloodingModel():
         self.rock = {
             'permeability': 2, # 1 d = 9.869 233e-13 m^2 
             'porosity': 0.3, # None
-            'lame':(1.0e+8, 3.0e+8), # lambda and mu
+            'lame':(1.0e+8, 3.0e+8), # lambda and mu 拉梅常数
             'biot': 1.0,
             'initial pressure': 3, # MPa
             'initial stress': 60.66, # MPa 先不考虑应力的计算 
             'solid grain stiffness': 6.25 # MPa
             }
         self.water = {
-            'viscosity': 1, # cp
+            'viscosity': 1, # 1 cp = 1 mPa*s
             'compressibility': 1.0e-3, # MPa^{-1}
             'initial saturation': 0.0, 
-            'injection rate': 3.51e-6 # s^{-1}
+            'injection rate': 3.51e-6 # s^{-1}, 每秒多少体积
             }
         self.oil = {'viscosity': 2, # cp
             'compressibility': 2.0e-3, # MPa^{-1}
@@ -122,7 +126,7 @@ class WaterFloodingModel():
         self.bc = {'displacement': 0.0, 'flux': 0.0}
 
         # Water-oil relative permeability SWT
-        #      SW        krw       krow 
+        #      SW        krw       krow == kro? TODO: make it clear
         self.SWT = np.array([
             (0.200000, 0.000000, 0.510200),
             (0.235714, 0.000010, 0.439917),
@@ -140,6 +144,7 @@ class WaterFloodingModel():
             (0.664286, 0.290398, 0.002603),
             (0.700000, 0.390600, 0.000000)], dtype=np.float64)
 
+        # 二次拟合
         self.krw = np.poly1d(np.polyfit(self.SWT[:, 0], self.SWT[:, 1], 2))
         self.kro = np.poly1d(np.polyfit(self.SWT[:, 0], self.SWT[:, 2], 2))
 
@@ -156,17 +161,28 @@ class WaterFloodingModel():
         return timeline
 
     def relative_permeability_water(self, Sw):
+        """
+
+        Notes
+        ----
+        给定水的饱和度, 计算水的相对渗透率
+        """
         val = self.krw(Sw)
         val[val<0.0] = 0.0
         val[val>1.0] = 1.0
         return val
 
     def relative_permeability_oil(self, Sw):
+        """
+
+        Notes
+        ----
+        给定水的饱和度, 计算油的相对渗透率
+        """
         val = self.kro(Sw)
         val[val<0.0] = 0.0
         val[val>1.0] = 1.0
         return val
-
 
 
 
@@ -178,22 +194,63 @@ class WaterFloodingModelSolver():
         self.mesh = model.space_mesh()
         self.timeline = model.time_mesh()
 
-        self.vspace = RaviartThomasFiniteElementSpace2d(mesh, p=0) # 速度空间
+        self.vspace = RaviartThomasFiniteElementSpace2d(self.mesh, p=0) # 速度空间
         self.pspace = self.vspace.smspace # 压强空间
-        self.cspace = LagrangeFiniteElementSpace(mesh, p=1) # 位移和饱和度连续空间
+        self.cspace = LagrangeFiniteElementSpace(self.mesh, p=1) # 位移和饱和度连续空间
+
 
         self.v = self.vspace.function() # 速度函数
         self.p = self.pspace.function() # 压强函数
-        self.s = self.cspace.function() # 水的饱和度函数 默认为0, 初始水的饱和度为0
         self.u = self.cspace.function(dim=2) # 位移函数
+        self.s = self.cspace.function() # 水的饱和度函数 默认为0, 初始时刻区域内水的饱和度为0
         self.phi = self.pspace.function() # 孔隙度函数, 分片常数
 
+        NN = self.mesh.number_of_nodes()
+        self.fg = self.cspace.function()
+        self.fw = self.cspace.function()
+
+        # TODO: 注意这里假设用的是结构网格, 换其它的网格需要修改代码
+        self.fg[0] = self.model.water['injection rate'] # 注入
+        self.fw[-1] = -self.model.oil['production rate'] # 产出
+
         # 初值
-        self.p[:] = model.rock['initial pressure'] 
-        self.phi[:] = model.rock['porosity']
+        self.p[:] = model.rock['initial pressure']  # MPa
+        self.phi[:] = model.rock['porosity'] 
+
+        # 一些常数矩阵和向量
+        # (\nabla\cdot v, w) 速度散度矩阵
+        self.B = self.vspace.div_matrix()
+
+        # (\nabla\cdot u, w) 位移散度矩阵
+        cellmeasure = self.mesh.entity_measure('cell')
+        cellmeasure *= self.model.rock['biot']
+        gphi = self.mesh.grad_lambda() # (NC, TD+1, GD)
+        gphi *= cellmeasure[:, None, None]
+        c2d0 = self.pspace.cell_to_dof()
+        c2d1 = self.cspace.cell_to_dof()
+        gdof0 = self.pspace.number_of_global_dofs()
+        gdof1 = self.cspace.number_of_global_dofs()
+        I = np.broadcast_to(c2d0, shape=c2d1.shape)
+        J = c2d1 
+        self.U00 = csr_matrix(
+                (gphi[..., 0].flat, (I.flat, J.flat)), 
+                shape=(gdof0, gdof1)
+                )
+        self.U01 = csr_matrix(
+                (gphi[..., 1].flat, (I.flat, J.flat)),
+                shape=(gdof0, gdof1)
+                )
+        
 
     @barycentric
     def pressure_coefficient(self, bc):
+
+        """
+
+        Notes
+        -----
+        计算压强矩阵的系数
+        """
 
         b = self.model.rock['biot'] 
         Ks = self.model.rock['solid grain stiffness']
@@ -213,6 +270,12 @@ class WaterFloodingModelSolver():
 
     @barycentric
     def saturation_coefficient(self, bc):
+        """
+
+        Notes
+        -----
+        计算饱和度矩阵的系数
+        """
         b = self.model.rock['biot'] 
         Ks = self.model.rock['solid grain stiffness']
         cw = self.model.water['compressibility']
@@ -225,20 +288,30 @@ class WaterFloodingModelSolver():
         val /= Ks
         val += phi*cw
         val *= Sw
-
         return val
 
     def flux_coefficient(self, bc):
         """
         Notes
         -----
-        目前假设是常系数
+
+        计算通量的系数, 流体的相对渗透率除以粘性系数得到流动性系数 
+        krg/mu_g 是气体的流动性系数 
+        kro/mu_o 是油的流动性系数
+        krw/mu_w 是水的流动性系数
+
+        这里假设压强的单位是 MPa 
+
+        1 d = 9.869 233e-13 m^2 = 1000 md
+        1 cp = 1 mPa s = 1e-9 MPa.s
+
         """
 
-        muw = self.model.water['viscosity'] # 单位是 1 cp = mPa/s
-        muo = self.model.oil['viscosity'] 
+        muw = self.model.water['viscosity'] # 单位是 1 cp = 1 mPa.s
+        muo = self.model.oil['viscosity'] # 单位是 1 cp = 1 mPa.s 
 
-        k = self.model.rock['permeability']*9.869233-4 # 压强单位是 MPa 
+        # 岩石的绝对渗透率, 这里考虑量纲的一致性
+        k = self.model.rock['permeability']*9.869233e-4  
 
         Sw = self.s.value(bc) # 水的饱和度系数
 
@@ -252,26 +325,162 @@ class WaterFloodingModelSolver():
         return val
 
     def stabilization_coefficient(self, bc):
+        """
+        Notes
+        -----
+        稳定项系数, 这里暂时假设为常数
+        """
         alpha = 0.1
         beta = 0.2
         CR = 1.0
         return 0.01
 
-    def get_linear_elasticity_system(self):
+    def water_fractional_flow(self, bc):
+        """
+
+        Notes
+        -----
+        """
+
+        Sw = self.s.value(bc) # 水的饱和度系数
+        lamw = self.model.relative_permeability_water(Sw)
+        lamw /= muw
+        lamo = self.model.relative_permeability_oil(Sw)
+        lamo /= muo
+        val = lamw/(lamw + lamo)
+        return val
+
+    def get_total_system(self):
+        """
+        Notes
+        -----
+        构造整个系统
+
+        x = [v, p, s, u0, u1]
+
+        A = [[   V,  -B, None, None,  None]
+             [ B.T,   P, None, DU00,  DU01]
+             [  FV,  SP,    S, DU10,  DU10] 
+             [None, UP0, None,  U00,   U01]
+             [None, UP1, None,  U10,   U11]
+
+        F = [None, FP, FS, FU0, FU1]
+        """
+        pass
+
+    def get_velocity_system(self, q=2):
+        """
+        Notes
+        -----
+        计算速度方程对应的离散系统.
+        """
+        cellmeasure = self.vspace.cellmeasure
+        qf = self.mesh.integrator(q, etype='cell')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+        c = self.flux_coefficient(bcs)
+        phi = self.vspace.basis(bcs)
+        V = np.einsum('q, qc, qcin, qcjn, c->cij', ws, c, phi, phi,
+                cellmeasure, optimize=True)
+
+        c2d = self.vspace.cell_to_dof()
+        I = np.broadcast_to(c2d[:, :, None], shape=V.shape)
+        J = np.broadcast_to(c2d[:, None, :], shape=V.shape)
+
+        gdof = self.vspace.number_of_global_dofs()
+        V = csr_matrix(
+                (V.flat, (I.flat, J.flat)), 
+                shape=(gdof, gdof)
+
+        return [V, -B, None, None, None], np.zeros(gdof, dtype=np.float64) 
+
+    def get_pressure_system(self, q=2):
+        """
+        Notes
+        -----
+        计算压强方程对应的离散系统
+
+        这里组装矩阵时, 利用了压强是分片常数的特殊性 
+        """
+        dt = self.timeline.current_time_step_length()
+        cellmeasure = self.pspace.cellmeasure
+        qf = self.mesh.integrator(q, etype='cell')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+        c = self.pressure_coefficient(bcs) # (NQ, NC)
+        c = np.sum(c, axis=0)
+        c *= cellmeasure 
+        P = diags(val, 0)
+
+        FP = self.fg.value(bcs) + self.fw.value(bcs)
+        FP = np.sum(val, axis=0)
+        FP *= cellmeasure
+        FP *= dt
+
+        FP += P@self.p
+        FP += self.U00@self.u[:, 0]
+        FP += self.U01@self.u[:, 1]
+
+        return [dt*self.B.T, P, None, self.U00, self.U01], FP
+
+    def get_saturation_system(self, q=2):
+        """
+        Notes
+        ----
+        计算饱和度方程对应的离散系统
+
+        [  FV,  SP,    S, DU10,  DU10] 
+
+        """
+
+        dt = self.timeline.current_time_step_length()
+        cellmeasure = self.cspace.cellmeasure
+        qf = self.mesh.integrator(q, etype='cell')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+
+        gphi = self.mesh.grad_lambda() #(NC, TD+1, GD)
+        vphi = self.vspace.basis(bcs) # (NQ, NC, TD+1, GD)
+        c = self.water_fractional_flow(bcs) # (NQ, NC)
+
+        FV = np.einsum('q, qc, cin, qcjn, c->cij', ws, c, gphi, vphi, cellmeasure)
+
+        gdof0 = self.cspace.number_of_global_dofs()
+        gdof1 = self.vspace.number_of_global_dofs()
+        c2d0 = self.cspace.cell_to_dof()
+        c2d1 = self.vspace.cell_to_dof()
+        I = np.broadcast_to(c2d0[:, :, None], shape=FV.shape)
+        J = np.broadcast_to(c2d1[:, None, :], shape=FV.shape)
+
+        FV = csr_matrix(
+                (FV.flat, (I.flat, J.flat)),
+                shape=(gdof0, gdof1)
+                )
+
+
+
+
+
+    def get_dispacement_system(self):
+        """
+        Notes
+        -----
+        计算位移方程对应的离散系统, 即线弹性方程系统.
+        """
 
         lam, mu = self.model.rock['lame']
         A = self.cspace.linear_elasticity_matrix(lam, mu)
 
-    def get_right_vector(self):
-        pass
+
+
+
+
 
 
 if __name__ == '__main__':
 
     model = WaterFloodingModel()
-    s = np.random.rand(10)
-    print(s)
-    val = model.relative_permeability_water(s)
-    print(val)
-    val = model.relative_permeability_oil(s)
-    print(val)
+    solver = WaterFloodingModelSolver(model)
+
+    fig = plt.figure()
+    axes = fig.gca()
+    solver.mesh.add_plot(axes)
+    solver.mesh.find_node(axes, showindex=True)
+    plt.show()
