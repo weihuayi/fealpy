72faf9e011f1d5beec63ed3e2aac4c277772d32c
Huayi Wei <weihuayi@xtu.edu.cn>
2020-10-02 07:59:57 +0800

    update

--- fealpy/solver/__init__.py
+++ fealpy/solver/__init__.py
@@ -2,7 +2,11 @@
 from .solve import solve, active_set_solver
 from .amg import AMGSolver
 from .matlab_solver import MatlabSolver
-from .petsc_solver import PETScSolver
+
+try:
+    from .petsc_solver import PETScSolver
+except ImportError:
+    print('I do not find petsc and petsc4py installed on this system!, so you can not use it')
 
 from .fast_solver import HighOrderLagrangeFEMFastSolver
 from .fast_solver import SaddlePointFastSolver
