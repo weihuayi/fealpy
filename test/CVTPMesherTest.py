f6716e66ad0ffc3e57936ebab063577cbc40a821
wangdong2 <804778943@qq.com>
2021-02-15 15:10:52 +0800

    update

--- test/CVTPMesherTest.py
+++ test/CVTPMesherTest.py
@@ -201,6 +201,21 @@ class CVTPMesherTest:
             uniform_mesh.uniform_boundary_meshing(n=1)
             bnode = uniform_mesh.bnode
 
+        if domain == 'square2':
+            vertices = np.array([
+                ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0, 1.0),
+                ( 3.0, 0.0),( 4.0, 0.0),( 4.0, 1.0),( 3.0, 1.0)],dtype=np.float)
+            facets = np.array([
+                (0, 1),(1, 2),(2, 3),(3, 0),
+                (4, 5),(5, 6),(6, 7),(7, 4)], dtype=np.int)
+            subdomain = np.array([
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (2, 0),(2, 0),(2, 0),(2, 0)], dtype=np.int)
+            mesh = HalfEdgeMesh2d.from_edges(vertices, facets, subdomain)
+            uniform_mesh = CVTPMesher(mesh)
+            uniform_mesh.uniform_boundary_meshing(n=2)
+            bnode = uniform_mesh.bnode
+
         if add_cnode == True:
             cnode = uniform_mesh.cnode
             bnode = np.append(bnode, cnode,axis=0)
@@ -326,6 +341,55 @@ class CVTPMesherTest:
             mesh = HalfEdgeMesh2d.from_edges(vertices, facets, subdomain)
             uniform_mesh = CVTPMesher(mesh)
             uniform_mesh.uniform_meshing(refine=1)
+        elif domain == 'hole2':
+            """
+            vertices = np.array([
+                ( 0.0, 0.0),( 0.5, 1.0),( 1.0, 0.0),( 1.5, 0.0),
+                ( 2.0, 0.0),( 2.0, 0.5),( 2.0, 1.0),( 2.0, 1.5),
+                ( 2.0, 2.0),( 1.5, 2.0),( 1.0, 2.0),( 0.5, 2.0),
+                ( 0.0, 2.0),( 0.0, 1.5),( 0.0, 1.0),( 0.0, 0.5),
+                ( 0.4, 0.4),( 0.4, 0.7),( 0.7, 0.7),( 0.7, 0.4),
+                ( 1.2, 1.2),( 1.2, 1.5),( 1.5, 1.5),( 1.5, 1.2)],dtype=np.float)
+            facets = np.array([
+                ( 0, 1),( 1, 2),( 2, 3),( 3, 4),
+                ( 4, 5),( 5, 6),( 6, 7),( 7, 8),
+                ( 8, 9),( 9,10),(10,11),(11,12),
+                (12,13),(13,14),(14,15),(15, 0),
+                (16,17),(17,18),(18,19),(19,16),
+                (20,21),(21,22),(22,23),(23,20)], dtype=np.int)
+            subdomain = np.array([
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1,-1),(1,-1),(1,-1),(1,-1),
+                (1,-2),(1,-2),(1,-2),(1,-2)], dtype=np.int)
+            """
+            #""" 
+            vertices = np.array([
+                ( 0.0, 0.0),( 1.0, 0.0),( 2.0, 0.0),( 2.0, 1.0),
+                ( 2.0, 2.0),( 1.0, 2.0),( 0.0, 2.0),( 0.0, 1.0),
+                ( 0.4, 0.4),( 0.4, 0.7),( 0.7, 0.7),( 0.7, 0.4),
+                ( 1.2, 1.2),( 1.2, 1.5),( 1.5, 1.5),( 1.5, 1.2)],dtype=np.float)
+            facets = np.array([
+                ( 0, 1),( 1, 2),( 2, 3),( 3, 4),
+                ( 4, 5),( 5, 6),( 6, 7),( 7, 0),
+                ( 8, 9),( 9,10),(10,11),(11, 8),
+                (12,13),(13,14),(14,15),(15,12),], dtype=np.int)
+            subdomain = np.array([
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1, 0),(1, 0),(1, 0),(1, 0),
+                (1,-1),(1,-1),(1,-1),(1,-1),
+                (1,-2),(1,-2),(1,-2),(1,-2)], dtype=np.int)
+
+            #"""
+
+            mesh = HalfEdgeMesh2d.from_edges(vertices, facets, subdomain)
+            uniform_mesh = CVTPMesher(mesh)
+            #uniform_mesh.uniform_boundary_meshing(n=1)
+            uniform_mesh.uniform_meshing(refine=1)
+            #bnode = uniform_mesh.bnode
+
         mesh.print()
         vor, start = uniform_mesh.voronoi()
         if plot:
@@ -367,6 +431,7 @@ class CVTPMesherTest:
 test = CVTPMesherTest()
 #test.uniform_boundary_meshing_test()
 #test.uniform_meshing_test(domain='square')
+test.uniform_meshing_test(domain='square2',interior_nodes = False)
 #test.uniform_meshing_test(domain='LShape')
 #test.uniform_meshing_test(domain='circle')
 #test.uniform_meshing_test(domain = 'partition1')
@@ -374,10 +439,10 @@ test = CVTPMesherTest()
 #test.uniform_meshing_test(domain = 'hole1')
 #test.uniform_meshing_test(domain='hole2',interior_nodes=False)
 #test.Lloyd_test(domain='square')
-test.Lloyd_test(domain = 'LShape')
+#test.Lloyd_test(domain = 'LShape')
 #test.Lloyd_test(domain = 'circle')
 #test.Lloyd_test(domain='partition1')
 #test.Lloyd_test(domain='partition2')
-#test.Lloyd_test(domain='hole1')
+#test.Lloyd_test(domain='hole2')
 
 
