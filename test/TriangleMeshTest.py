62227d790c9d6e75e47d5f6c7764fb57c03b7c92
Wang Dong <804778943@qq.com>
2021-04-12 08:46:46 +0000

    Master

--- test/TriangleMeshTest.py
+++ test/TriangleMeshTest.py
@@ -19,8 +19,6 @@ class TriangleMeshTest():
         point = np.array([[0.2, 0.5], [0.5, 0.8], [1.6, 0.5], [1.2, 0.2]])
         segment = np.array([[0, 2], [1, 3]])
 
-        isCrossCell = mesh.find_crossed_cell(point, segment)
-        print(np.where(isCrossCell))
         fig = plt.figure()
         axes = fig.gca()
         for i in range(len(segment)):
@@ -28,8 +26,24 @@ class TriangleMeshTest():
             b = segment[i, 1]
             axes.plot(point[[a, b], 0], point[[a, b], 1], 'r')
         mesh.add_plot(axes)
-        mesh.find_cell(axes, index=isCrossCell)
+        #mesh.find_cell(axes)
         plt.show()
 
+    def uniform_refine_test(self):
+        node = np.array([[0,0],[1,0],[1,1],[0,1],[2,0],[2,1]], dtype = np.float)
+        cell = np.array([[0,1,2],[0,2,3],[1,4,5],[2,1,5]],dtype = np.int)
+        mesh = TriangleMesh(node, cell)
+        N,C = mesh.uniform_refine(n=2,returnim = True)
+
+        fig = plt.figure()
+        axes = fig.gca()
+        mesh.add_plot(axes)
+        mesh.find_cell(axes)
+        plt.show()
+
+        print(N)
+        print(C)
+
 test = TriangleMeshTest()
-test.lineTest()
+#test.lineTest()
+test.uniform_refine_test()
