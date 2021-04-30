2694f9e9ea9e38c9dbfc06cdefd47400dcdbcaca
Huayi Wei <weihuayi@xtu.edu.cn>
2021-04-30 16:36:33 +0800

    update

--- test/buildtest/PoissonFEMWithDirichletBC_test.py
+++ test/buildtest/PoissonFEMWithDirichletBC_test.py
@@ -14,42 +14,44 @@ from fealpy.boundarycondition import DirichletBC
 from scipy.sparse.linalg import spsolve
 
 
-degree = 1  
-dim = 2 
-nrefine = 4 
-maxit = 4 
+dim = int(sys.argv[1])
 
-if dim == 2:
-    from fealpy.pde.poisson_2d import CosCosData as PDE
-elif dim == 3:
-    from fealpy.pde.poisson_3d import CosCosCosData as PDE
+def test_poisson_fem(dim):
+    degree = 1  
+    nrefine = 4 
+    maxit = 4 
 
-pde = PDE()
-mesh = pde.init_mesh(n=nrefine)
-
-errorMatrix = np.zeros((2, maxit), dtype=np.float64)
-NDof = np.zeros(maxit, dtype=np.int64)
-
-for i in range(maxit):
-    space = LagrangeFiniteElementSpace(mesh, p=degree)
-    NDof[i] = space.number_of_global_dofs()
-    bc = DirichletBC(space, pde.dirichlet) 
-
-    uh = space.function()
     if dim == 2:
-        A = space.stiff_matrix()
+        from fealpy.pde.poisson_2d import CosCosData as PDE
     elif dim == 3:
-        A = space.parallel_stiff_matrix(q=p)
+        from fealpy.pde.poisson_3d import CosCosCosData as PDE
 
-    F = space.source_vector(pde.source)
+    pde = PDE()
+    mesh = pde.init_mesh(n=nrefine)
 
-    A, F = bc.apply(A, F, uh)
+    errorMatrix = np.zeros((2, maxit), dtype=np.float64)
+    NDof = np.zeros(maxit, dtype=np.int64)
 
-    uh[:] = spsolve(A, F).reshape(-1)
+    for i in range(maxit):
+        space = LagrangeFiniteElementSpace(mesh, p=degree)
+        NDof[i] = space.number_of_global_dofs()
+        bc = DirichletBC(space, pde.dirichlet) 
 
-    errorMatrix[0, i] = space.integralalg.L2_error(pde.solution, uh)
-    errorMatrix[1, i] = space.integralalg.L2_error(pde.gradient, uh.grad_value)
+        uh = space.function()
+        if dim == 2:
+            A = space.stiff_matrix()
+        elif dim == 3:
+            A = space.parallel_stiff_matrix(q=p)
 
-    if i < maxit-1:
-        mesh.uniform_refine()
+        F = space.source_vector(pde.source)
+
+        A, F = bc.apply(A, F, uh)
+
+        uh[:] = spsolve(A, F).reshape(-1)
+
+        errorMatrix[0, i] = space.integralalg.L2_error(pde.solution, uh)
+        errorMatrix[1, i] = space.integralalg.L2_error(pde.gradient, uh.grad_value)
+
+        if i < maxit-1:
+            mesh.uniform_refine()
 
