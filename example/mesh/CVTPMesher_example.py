6070b8c050f0b039d5d133fde87d8f37fedc18ca
Huayi Wei <weihuayi@xtu.edu.cn>
2021-07-23 16:04:47 +0800

    update

--- example/mesh/CVTPMesher_example.py
+++ example/mesh/CVTPMesher_example.py
@@ -36,11 +36,11 @@ nbrefine = args.nbrefine
 
 if domain == 'square':
     node = np.array([
-        ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0, 1.0)],dtype=np.float)
+        ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0, 1.0)],dtype=np.float64)
     edge = np.array([
-        (0, 1),(1, 2),(2, 3),(3, 0)], dtype=np.int)
+        (0, 1),(1, 2),(2, 3),(3, 0)], dtype=np.int_)
     subdomain = np.array([
-        (1, 0),(1, 0),(1, 0),(1, 0)], dtype=np.int)
+        (1, 0),(1, 0),(1, 0),(1, 0)], dtype=np.int_)
     mesh = HalfEdgeMesh2d.from_edges(node, edge, subdomain)
     fixed = None
     times = None
@@ -48,14 +48,14 @@ if domain == 'square':
 elif domain == 'Lshape':
     node = np.array([
         ( 0.0, 0.0),( 1.0, 0.0),( 1.0,  1.0),(0.0,  1.0),
-        (-1.0, 1.0),(-1.0, 0.0),(-1.0, -1.0),(0.0, -1.0)],dtype=np.float)
-    fixed = np.array([1, 1, 1, 0, 1, 0, 1, 1],dtype=np.bool)
+        (-1.0, 1.0),(-1.0, 0.0),(-1.0, -1.0),(0.0, -1.0)],dtype=np.float64)
+    fixed = np.array([1, 1, 1, 0, 1, 0, 1, 1],dtype=np.bool_)
     edge = np.array([
        (0, 1), (1, 2), (2, 3), (3, 4), 
-       (4, 5), (5, 6), (6, 7), (7, 0)],dtype=np.int)
+       (4, 5), (5, 6), (6, 7), (7, 0)],dtype=np.int_)
     subdomain = np.array([
        (1, 0), (1, 0), (1, 0), (1, 0),
-       (1, 0), (1, 0), (1, 0), (1, 0)],dtype=np.int)
+       (1, 0), (1, 0), (1, 0), (1, 0)],dtype=np.int_)
     mesh = HalfEdgeMesh2d.from_edges(node, edge, subdomain, fixed)
     times = None
 
@@ -63,14 +63,14 @@ elif domain =='circle':
     n = 20
     h = 2*np.pi/n
     theta = np.arange(0, 2*np.pi, h)
-    node = np.zeros((n, 2), dtype=np.float)
+    node = np.zeros((n, 2), dtype=np.float64)
     node[:, 0] = np.cos(theta)
     node[:, 1] = np.sin(theta)
-    fixed = np.zeros(n, dtype=np.bool)
-    edge = np.zeros((n,2), dtype=np.int)
+    fixed = np.zeros(n, dtype=np.bool_)
+    edge = np.zeros((n,2), dtype=np.int_)
     edge[:, 0] = range(0, n)
     edge[:-1, 1] = range(1, n)
-    subdomain = np.zeros((n, 2),dtype=np.int)
+    subdomain = np.zeros((n, 2),dtype=np.int_)
     subdomain[:, 0] = 1
     mesh = HalfEdgeMesh2d.from_edges(node, edge, subdomain)
     times = None
@@ -78,11 +78,11 @@ elif domain =='circle':
 elif domain =='triangle':
     # n>=3
     node = np.array([
-        ( 0.0, 0.0),( 2.0, 0.0),( 1.0, 1.0)],dtype=np.float)
+        ( 0.0, 0.0),( 2.0, 0.0),( 1.0, 1.0)],dtype=np.float_)
     edge = np.array([
-        (0, 1),(1, 2),(2, 0)], dtype=np.int)
+        (0, 1),(1, 2),(2, 0)], dtype=np.int_)
     subdomain = np.array([
-        (1, 0),(1, 0),(1, 0)], dtype=np.int)
+        (1, 0),(1, 0),(1, 0)], dtype=np.int_)
     mesh = HalfEdgeMesh2d.from_edges(node, edge, subdomain)
     times = None
     fixed = None
@@ -92,20 +92,20 @@ elif domain =='circle_h':
     h = 2*np.pi/n
     theta = np.arange(0, 2*np.pi, h)
     node1 = np.array([(-2.0,-2.0),(2.0,-2.0),(2.0,2.0),(-2.0,2.0)])
-    node2 = np.zeros((n, 2), dtype=np.float)
+    node2 = np.zeros((n, 2), dtype=np.float64)
     node2[:, 0] = np.cos(theta)
     node2[:, 1] = -np.sin(theta)
     node = np.vstack((node1,node2))
     n = len(node)
-    fixed = np.ones(n, dtype=np.bool)
+    fixed = np.ones(n, dtype=np.bool_)
     fixed[4:] = False
-    edge = np.zeros((n,2), dtype=np.int)
+    edge = np.zeros((n,2), dtype=np.int_)
     edge[:4, 0] = range(0, 4)
     edge[:3, 1] = range(1, 4)
     edge[4:, 0] = range(4, n)
     edge[4:-1, 1] = range(5, n)
     edge[-1,1] = 4
-    subdomain = np.zeros((n, 2),dtype=np.int)
+    subdomain = np.zeros((n, 2),dtype=np.int_)
     subdomain[:4, 0] = 1
     subdomain[4:,0] = 1
     subdomain[4:,1] = -1
@@ -117,13 +117,13 @@ elif domain =='circle_h':
 elif domain == 'square_h':    
     node = np.array([
         ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0, 1.0),
-        ( 0.4, 0.4),( 0.4, 0.8),( 0.8, 0.8),( 0.8, 0.4)],dtype=np.float)
+        ( 0.4, 0.4),( 0.4, 0.8),( 0.8, 0.8),( 0.8, 0.4)],dtype=np.float64)
     edge = np.array([
         (0, 1),(1, 2),( 2, 3),( 3, 0),
-        (4, 5),(5, 6),( 6, 7),( 7, 4)], dtype=np.int)
+        (4, 5),(5, 6),( 6, 7),( 7, 4)], dtype=np.int_)
     subdomain = np.array([
         (1, 0),(1, 0),(1, 0),(1, 0),
-        (1,-1),(1,-1),(1,-1),(1,-1)], dtype=np.int)
+        (1,-1),(1,-1),(1,-1),(1,-1)], dtype=np.int_)
 
     mesh = HalfEdgeMesh2d.from_edges(node, edge, subdomain)
     times = np.zeros(len(edge))
@@ -133,13 +133,14 @@ elif domain == 'square_h':
 
 elif domain == 'partition_tr':
     vertices = np.array([
-        ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0, 1.0),(0.5,0.5)],dtype=np.float)
+        ( 0.0, 0.0),( 1.0, 0.0),( 1.0, 1.0),( 0.0,
+            1.0),(0.5,0.5)],dtype=np.float64)
     facets = np.array([
         (0, 1),(1, 2),(2, 3),(3, 0),
-        (0, 4),(4, 3),(4, 1),(4, 2)], dtype=np.int)
+        (0, 4),(4, 3),(4, 1),(4, 2)], dtype=np.int_)
     subdomain = np.array([
         (1, 0),(2, 0),(3, 0),(4, 0),
-        (4, 1),(4, 3),(2, 1),(3, 2)], dtype=np.int)
+        (4, 1),(4, 3),(2, 1),(3, 2)], dtype=np.int_)
     mesh = HalfEdgeMesh2d.from_edges(vertices, facets, subdomain)
     times = None
     fixed = None
@@ -148,15 +149,15 @@ elif domain == 'partition_s':
     vertices = np.array([
         ( 0.0, 0.0),(0.5, 0.0),( 1.0, 0.0),(1.0, 0.5),
         ( 1.0, 1.0),(0.5, 1.0),( 0.0, 1.0),(0.0, 0.5),
-        ( 0.5, 0.5)],dtype=np.float)
+        ( 0.5, 0.5)],dtype=np.float64)
     facets = np.array([
         (0, 1),(1, 2),(2, 3),(3, 4),
         (4, 5),(5, 6),(6, 7),(7, 0),
-        (1, 8),(8, 7),(8, 3),(8, 5)], dtype=np.int)
+        (1, 8),(8, 7),(8, 3),(8, 5)], dtype=np.int_)
     subdomain = np.array([
         (1, 0),(2, 0),(2, 0),(3, 0),
         (3, 0),(4, 0),(4, 0),(1, 0),
-        (1, 2),(1, 4),(3, 2),(4, 3)], dtype=np.int)
+        (1, 2),(1, 4),(3, 2),(4, 3)], dtype=np.int_)
     mesh = HalfEdgeMesh2d.from_edges(vertices, facets, subdomain)
     times = None
     fixed = None
@@ -181,7 +182,7 @@ mesh.find_node(axes, node=vor.points, showindex=False)
 voronoi_plot_2d(vor, ax=axes,show_vertices = False)
 plt.show()
 
-pmesh = mesher.to_PolygonMesh(vor)
+pmesh = mesher.to_polygonMesh(vor)
 fig = plt.figure()
 axes= fig.gca()
 pmesh.add_plot(axes)
