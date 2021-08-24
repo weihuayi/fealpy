24d2d673f0c987c5e90ca9cadcde73d69e7a9ca6
tiantian0347 <tiantian0347@126.com>
2021-08-24 14:50:23 +0800

    update

--- app/Planet/fast_solver.py
+++ app/Planet/fast_solver.py
@@ -24,6 +24,7 @@ class PlanetFastSovler():
     def set_matrix(self, Ak):
         self.Ak = Ak
 
+    @timer
     def linear_operator_1(self, b):
         '''
         
@@ -42,9 +43,9 @@ class PlanetFastSovler():
 
         return r
 
-    def linear_operator_2(self, r):
-        ml = pyamg.ruge_stuben_solver(self.D)
-        b = ml.solve(r, tol=1e-12, accel='cg').reshape(-1)    
+    @timer
+    def linear_operator_2(self, b):
+        b = self.D@b
         return b
 
     @timer
@@ -54,15 +55,14 @@ class PlanetFastSovler():
 
         A = LinearOperator((rdof, rdof), matvec=self.linear_operator_1)
         a = F[:rdof]
-        b = np.zeros(gdof, dtype=np.float64)
-        b[:] = F[rdof:]
 
         if self.ctx.myid == 0:
             self.ctx.set_rhs(b)
         self.ctx.run(job=3)
 
-        a -= self.B@b
+        a -= self.B@F[rdof:]
 
         uh[:rdof].T.flat, info = cg(A, a, tol=1e-8)
 
-        uh[rdof:] = self.linear_operator_2(F[rdof:]-uh[:rdof]@self.B)
+        P = LinearOperator((gdof, gdof), matvec=self.linear_operator_2)
+        uh[rdof:].T.flat, info = cg(P, F[rdof:]-uh[:rdof]@self.B, tol=1e-8)
