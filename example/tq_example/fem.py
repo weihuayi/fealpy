8731f62148852a07d23fc787fd3ebd0f6f77cba8
Huayi Wei <weihuayi@xtu.edu.cn>
2022-01-07 08:12:10 +0800

    add mv

--- example/tq_example/fem.py
+++ example/tq_example/fem.py
@@ -3,13 +3,16 @@ import taichi as ti
 
 from fealpy.mesh import MeshFactory as MF
 from fealpy.ti import lagrange_cell_stiff_matrix_0
+from fealpy.ti import lagrange_cell_stiff_matrix_1
 
-ti.init(arch=ti.gpu)
+from timeit import default_timer as dtimer 
+
+ti.init(arch=ti.cpu)
 
 GD = 2
 
 domain = [0, 1, 0, 1]
-mesh = MF.boxmesh2d(domain, nx=10, ny=10, meshtype='tri')
+mesh = MF.boxmesh2d(domain, nx=1000, ny=1000, meshtype='tri')
 
 NN = mesh.number_of_nodes()
 NC = mesh.number_of_cells()
@@ -17,46 +20,25 @@ NC = mesh.number_of_cells()
 # 全局变量
 
 node = ti.field(ti.float64, (NN, 2))
-cell = ti.field(ti.int32, (NC, 3)) # 存储未知量
-S = ti.field(ti.float64, (NC, 3, 3))
+cell = ti.field(ti.int32, (NC, 3)) 
+cnode = ti.field(ti.float64, (NC, 3, 2))
+S0 = ti.field(ti.float64, (NC, 3, 3))
+S1 = ti.field(ti.float64, (NC, 3, 3))
 
 node.from_numpy(mesh.entity('node'))
 cell.from_numpy(mesh.entity('cell'))
+cnode.from_numpy(mesh.entity('node')[mesh.entity('cell')])
 
-lagrange_cell_stiff_matrix_0(node, cell, S)
+start = dtimer()
+lagrange_cell_stiff_matrix_0(node, cell, S0)
+end = dtimer()
+print('run 0 with time:', end - start)
 
-S1 = S.to_numpy()
+start = dtimer()
+lagrange_cell_stiff_matrix_1(cnode, S1)
+end = dtimer()
+print('run 1 with time:', end - start)
 
-print(S1)
+print(S0.to_numpy() - S1.to_numpy())
 
-"""
-# 计算每个单元上的重心坐标的梯度
-@ti.kernel
-def grad_lambda():
-    for c in range(NC):
-        x0 = node[cell[c, 0], 0]
-        y0 = node[cell[c, 0], 1]
 
-        x1 = node[cell[c, 1], 0]
-        y1 = node[cell[c, 1], 1]
-
-        x2 = node[cell[c, 2], 0]
-        y2 = node[cell[c, 2], 1]
-
-        l = (x1 - x0)*(y2 - y0) - (y1 - y0)*(x2 - x0) 
-        gphi[c, 0, 0] = (y1 - y2)/l
-        gphi[c, 0, 1] = (x2 - x1)/l 
-
-        gphi[c, 1, 0] = (y2 - y0)/l
-        gphi[c, 1, 1] = (x0 - x2)/l 
-
-        gphi[c, 2, 0] = (y0 - y1)/l
-        gphi[c, 2, 1] = (x1 - x0)/l 
-
-grad_lambda()
-
-gphi0 = mesh.grad_lambda()
-gphi1 = gphi.to_numpy()
-
-print(gphi0 - gphi1)
-"""
--- fealpy/ti/mv.py
+++ fealpy/ti/mv.py
@@ -33,5 +33,24 @@ def lagrange_cell_stiff_matrix_0(node : ti.template(), cell : ti.template(),
 
 @ti.kernel
 def lagrange_cell_stiff_matrix_1(cnode : ti.template(), S : ti.template()):
-    print(cnode.shape)
-    print(S.shape)
+    for c in range(cnode.shape[0]):
+        x0 = cnode[c, 0, 0] 
+        y0 = cnode[c, 0, 1]
+
+        x1 = cnode[c, 1, 0] 
+        y1 = cnode[c, 1, 1]
+
+        x2 = cnode[c, 2, 0] 
+        y2 = cnode[c, 2, 1]
+
+
+        l = (x1 - x0)*(y2 - y0) - (y1 - y0)*(x2 - x0) 
+        gphi = ti.Matrix([
+            [(y1 - y2)/l, (x2 - x1)/l], 
+            [(y2 - y0)/l, (x0 - x2)/l],
+            [(y0 - y1)/l, (x1 - x0)/l]])
+        l *= 0.5
+        for i in ti.static(range(3)):
+            for j in ti.static(range(3)):
+                S[c, i, j] = l*(gphi[i, 0]*gphi[j, 0] +
+                        gphi[i, 1]*gphi[j, 1])
