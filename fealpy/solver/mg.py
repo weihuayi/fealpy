9ade150978f648f4f26bd5d855d0a6ff87ce878a
Huayi Wei <weihuayi@xtu.edu.cn>
2022-04-26 21:08:31 +0800

    update

--- fealpy/solver/mg.py
+++ fealpy/solver/mg.py
@@ -1,6 +1,90 @@
+import numpy 
+
+class GaussSeidelSmoother():
+    def __init__(self, A):
+        """
+
+        Notes
+        -----
+
+        对称正定矩阵的 Gauss 光滑
+
+        """
+
+        self.L0 = tril(A).tocsr()
+        self.U0 = triu(A, k=1).tocsr()
+
+        self.U1 = self.L0.T.tocsr()
+        self.L1 = self.U0.T.tocsr()
+
+    def smooth(self, b, x0, lower=True, maxit=3):
+        if lower:
+            for i in range(maxit):
+                #x0[:] = spsolve_triangular(self.L0, b-self.U0@x0, lower=lower)
+                x0[:] = spsolve(self.L0, b-self.U0@x0, permc_spec="NATURAL")
+        else:
+            for i in range(maxit):
+                #x0[:] = spsolve_triangular(self.U1, b-self.L1@x0, lower=lower)
+                x0[:] = spsolve(self.U1, b-self.L1@x0, permc_spec="NATURAL")
+
+
+class JacobiSmoother():
+    def __init__(self, A, isDDof=None):
+        if isDDof is not None:
+            # 处理 D 氏 自由度条件
+            gdof = len(isDDof)
+            bdIdx = np.zeros(gdof, dtype=np.int_)
+            bdIdx[isDDof] = 1 
+            Tbd = spdiags(bdIdx, 0, gdof, gdof)
+            T = spdiags(1-bdIdx, 0, gdof, gdof)
+            A = T@A@T + Tbd
+
+        self.D = A.diagonal() 
+        self.L = tril(A, k=-1).tocsr()
+        self.U = triu(A, k=1).tocsr()
+
+    def smooth(self, b, maxit=100):
+        r = b.copy()
+        for i in range(maxit):
+            r[:] = b - self.L@r - self.U@r
+            r /= self.D
+        return r
 
 
 
 class MG():
-    def __init__(self, A, P, R):
+    def __init__(self, A, P, R=None, c=None, options=None):
+        pass
+
+
+    def options(
+            self,
+            method='mean',
+            maxrefine=3,
+            maxcoarsen=0,
+            theta=1.0,
+            maxsize=1e-2,
+            minsize=1e-12,
+            data=None,
+            HB=True,
+            imatrix=False,
+            disp=True
+            ):
+
+        options = {
+                'method': method,
+                'maxrefine': maxrefine,
+                'maxcoarsen': maxcoarsen,
+                'theta': theta,
+                'maxsize': maxsize,
+                'minsize': minsize,
+                'data': data,
+                'HB': HB,
+                'imatrix': imatrix,
+                'disp': disp
+            }
+        return options
+
+
+    def solve(self):
         pass
