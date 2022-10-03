8148b1ac096d6090801a33a7902361a9af6e36b2
Huayi Wei <weihuayi@xtu.edu.cn>
2022-10-03 20:58:33 +0800

    update

--- fealpy/pde/poisson_1d.py
+++ fealpy/pde/poisson_1d.py
@@ -4,10 +4,10 @@ from fealpy.decorator import cartesian, barycentric
 
 class CosData:
     def __init__(self):
-        pass
+        self.I = np.array([0.0, 1.0])
     
     def domain(self):
-        return np.array([0, 1])
+        return self.I 
 
     def init_mesh(self, n=1):
         mesh = StructureIntervalMesh(self.I, nx = self.nx)
