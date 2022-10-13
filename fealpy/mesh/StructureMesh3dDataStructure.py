fcb663efb936e85011da7526886b56d1bb10dda1
Huayi Wei <weihuayi@xtu.edu.cn>
2022-10-13 17:43:06 +0800

    update

--- fealpy/mesh/StructureMesh3dDataStructure.py
+++ fealpy/mesh/StructureMesh3dDataStructure.py
@@ -3,9 +3,9 @@ import numpy as np
 class StructureHexMeshDataStructure():
     # The following local data structure should be class properties
     localEdge = np.array([
-        (0, 1), (3, 2), (5, 4), (6, 7),
-        (2, 0), (1, 3), (4, 6), (7, 5),
-        (0, 4), (5, 1), (6, 2), (3, 7)])
+        (0, 1), (2, 3), (4, 5), (6, 7),
+        (0, 2), (1, 3), (4, 6), (5, 7),
+        (0, 4), (1, 5), (2, 6), (3, 7)])
     localFace = np.array([
         (0, 2, 6, 4), (1, 5, 7, 3),  # bottom and top faces
         (0, 4, 5, 1), (2, 3, 7, 6),  # left and right faces
