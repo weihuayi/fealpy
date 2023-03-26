9b9e64338c734488e6b7cdfdbeac0510b643e061
Huayi Wei <weihuayi@xtu.edu.cn>
2023-03-26 18:07:07 +0800

    udpate

--- test/fem/test_bilinear_form.py
+++ test/fem/test_bilinear_form.py
@@ -5,9 +5,11 @@ from fealpy.fem import BilinearForm
 
 
 def test_truss():
+
     from fealpy.mesh import EdgeMesh
     from fealpy.functionspace import LagrangeFiniteElementSpaceOnEdgeMesh as Space
     from fealpy.fem import TrussStructureIntegrator
+    
     mesh = EdgeMesh.from_tower()
     GD = mesh.geo_dimension()
     space = Space(mesh, p=1)
