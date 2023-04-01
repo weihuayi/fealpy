c7a2c302247bccbd3610da6f8224e639e7a8e3c9
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-01 14:13:40 +0800

    add mesh mesh1d

--- fealpy/mesh/Mesh.py
+++ fealpy/mesh/Mesh.py
@@ -1,37 +1,68 @@
 import numpy as np
 
-
 class Mesh:
-    
     def number_of_cells(self):
-        pass
+        raise NotImplementedError
 
     def number_of_nodes(self):
-        pass
+        raise NotImplementedError
 
     def number_of_edges(self):
-        pass
+        raise NotImplementedError
 
     def number_of_faces(self):
-        pass
+        raise NotImplementedError
 
     def geo_dimension(self):
-        pass
+        raise NotImplementedError
 
     def top_dimension(self):
-        pass
+        raise NotImplementedError
+
+    def integrator(self, k):
+        raise NotImplementedError
+
+    def entity(self, etype, index=np.s_[:]):
+        raise NotImplementedError
+
+    def entity_barycenter(self, etype, index=np.s_[:]):
+        raise NotImplementedError
+
+    def entity_measure(self, etype, index=np.s_[:]):
+        raise NotImplementedError
+
+    def shape_function(self, p):
+        raise NotImplementedError
+
+    def grad_shape_function(self, p, index=np.s_[:])
+        raise NotImplementedError
+
+    def interpolation_points(self)
+        raise NotImplementedError
+
+    def cell_to_ipoint(self, p, index=np.s_[:]):
+        raise NotImplementedError
+
+    def edge_to_ipoint(self, p, index=np.s_[:]):
+        raise NotImplementedError
+
+    def face_to_ipoint(self, p, index=np.s_[:]):
+        raise NotImplementedError
+
+    def node_to_ipoint(self, p, index=np.s_[:]):
+        raise NotImplementedError
 
     def add_plot(self):
-        pass
+        raise NotImplementedError
 
     def find_node(self):
-        pass
+        raise NotImplementedError
 
     def find_edge(self):
-        pass
+        raise NotImplementedError
 
     def find_face(self):
-        pass
+        raise NotImplementedError
 
     def find_cell(self):
-        pass
+        raise NotImplementedError
