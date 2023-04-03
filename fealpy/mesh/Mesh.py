7c3655ea1729ef8e567bd5a2117add9406f5186c
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-03 10:10:21 +0800

    update Mesh.py Mesh1d.py

--- fealpy/mesh/Mesh.py
+++ fealpy/mesh/Mesh.py
@@ -19,7 +19,13 @@ class Mesh:
     def top_dimension(self):
         raise NotImplementedError
 
+    def uniform_refine(self):
+        raise NotImplementedError
+
     def integrator(self, k):
+        raise NotImplementedError
+
+    def number_of_entities(self, etype, index=np.s_[:]):
         raise NotImplementedError
 
     def entity(self, etype, index=np.s_[:]):
@@ -55,14 +61,140 @@ class Mesh:
     def add_plot(self):
         raise NotImplementedError
 
-    def find_node(self):
-        raise NotImplementedError
+    def find_node(self, axes, node=None, index=np.s_[:],
+            showindex=False, color='r',
+            markersize=20, fontsize=24, fontcolor='k', multiindex=None):
 
-    def find_edge(self):
-        raise NotImplementedError
+        if node is None:
+            node = self.entity('node')
+
+        GD = self.geo_dimension()
+
+        if GD == 1:
+            node = np.r_['1', node, np.zeros_like(node)]
+            GD = 2
+
+        if index == np.s_[:]:
+            index = range(node.shape[0])
+        elif (type(index) is np.int_):
+            index = np.array([index], dtype=np.int_)
+        elif (type(index) is np.ndarray) and (index.dtype == np.bool_):
+            index, = np.nonzero(index)
+        elif (type(index) is list) and (type(index[0]) is np.bool_):
+            index, = np.nonzero(index)
+        else:
+            raise ValueError("the type of index is not correct!")
+
+
+        if (type(color) is np.ndarray) and (np.isreal(color[0])):
+            umax = color.max()
+            umin = color.min()
+            norm = colors.Normalize(vmin=umin, vmax=umax)
+            mapper = cm.ScalarMappable(norm=norm, cmap='rainbow')
+            color = mapper.to_rgba(color)
+
+        bc = node[index]
+        if GD == 2:
+            axes.scatter(bc[..., 0], bc[..., 1], c=color, s=markersize)
+            if showindex:
+
+                if multiindex is not None:
+                    if (type(multiindex) is np.ndarray) and (len(multiindex.shape) > 1):
+                        for i, idx in enumerate(multiindex):
+                            s = str(idx).replace('[', '(')
+                            s = s.replace(']', ')')
+                            s = s.replace(' ', ',')
+                            axes.text(bc[i, 0], bc[i, 1], s,
+                                    multialignment='center',
+                                    fontsize=fontsize, 
+                                    color=fontcolor)
+                    else:
+                        for i, idx in enumerate(multiindex):
+                            axes.text(bc[i, 0], bc[i, 1], str(idx),
+                                    multialignment='center',
+                                    fontsize=fontsize, 
+                                    color=fontcolor) 
+                else:
+                    for i in range(len(index)):
+                        axes.text(bc[i, 0], bc[i, 1], str(index[i]),
+                                multialignment='center', fontsize=fontsize, 
+                                color=fontcolor) 
+        else:
+            axes.scatter(bc[..., 0], bc[..., 1], bc[..., 2], c=color, s=markersize)
+            if showindex:
+                if multiindex is not None:
+                    if (type(multiindex) is np.ndarray) and (len(multiindex.shape) > 1):
+                        for i, idx in enumerate(multiindex):
+                            s = str(idx).replace('[', '(')
+                            s = s.replace(']', ')')
+                            s = s.replace(' ', ',')
+                            axes.text(bc[i, 0], bc[i, 1], bc[i, 2], s,
+                                    multialignment='center',
+                                    fontsize=fontsize, 
+                                    color=fontcolor)
+                    else:
+                        for i, idx in enumerate(multiindex):
+                            axes.text(bc[i, 0], bc[i, 1], bc[i, 2], str(idx),
+                                    multialignment='center',
+                                    fontsize=fontsize, 
+                                    color=fontcolor) 
+                else:
+                    for i in range(len(index)):
+                        axes.text(bc[i, 0], bc[i, 1], bc[i, 2], str(index[i]),
+                                 multialignment='center', fontsize=fontsize, color=fontcolor) 
+
+    def find_edge(self, axes, index=np.s_[:], 
+            showindex=False,
+            color='r', markersize=20,
+            fontsize=24, fontcolor='k'):
+
+        GD = self.geo_dimension()
+        node = self.entity('node')
+        bc = self.entity_barycenter('edge', index=index)
+
+        if GD == 1:
+            node = np.r_['1', bc, np.zeros_like(node)]
+            GD = 2
+
+        if index == np.s_[:]:
+            index = range(bc.shape[0])
+        elif (type(index) is np.int_):
+            index = np.array([index], dtype=np.int_)
+        elif (type(index) is np.ndarray) and (index.dtype == np.bool_):
+            index, = np.nonzero(index)
+        elif (type(index) is list) and (type(index[0]) is np.bool_):
+            index, = np.nonzero(index)
+        else:
+            raise ValueError("the type of index is not correct!")
+
+        if (type(color) is np.ndarray) & (np.isreal(color[0])):
+            umax = color.max()
+            umin = color.min()
+            norm = colors.Normalize(vmin=umin, vmax=umax)
+            mapper = cm.ScalarMappable(norm=norm, cmap='rainbow')
+            color = mapper.to_rgba(color)
+
+        bc = bc[index]
+        if GD == 2:
+            axes.scatter(bc[:, 0], bc[:, 1], c=color, s=markersize)
+            if showindex:
+                for i in range(len(index)):
+                    axes.text(bc[i, 0], bc[i, 1], str(index[i]),
+                            multialignment='center', fontsize=fontsize, 
+                            color=fontcolor) 
+        else:
+            axes.scatter(bc[:, 0], bc[:, 1], bc[:, 2], c=color, s=markersize)
+            if showindex:
+                for i in range(len(index)):
+                    axes.text(
+                            bc[i, 0], bc[i, 1], bc[i, 2],
+                            str(index[i]),
+                            multialignment='center',
+                            fontsize=fontsize, color=fontcolor)
 
     def find_face(self):
         raise NotImplementedError
 
     def find_cell(self):
         raise NotImplementedError
+
