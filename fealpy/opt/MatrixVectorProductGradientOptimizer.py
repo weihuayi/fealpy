f75da5db900ec5bf95166ebf13538beb3cad4198
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-04 09:21:13 +0800

    udpate

--- fealpy/opt/MatrixVectorProductGradientOptimizer.py
+++ fealpy/opt/MatrixVectorProductGradientOptimizer.py
@@ -11,17 +11,17 @@ from .line_search import wolfe_line_search
 class MatrixVectorProductGradientOptimizer(Optimizer):
     def __init__(self, problem: Problem) -> None:
         super().__init__(problem)
-        self.P = problem['Preconditioner']
+        self.P = problem.Preconditioner
         assert self.P is not None
 
     def run(self):
         problem = self.problem
-        x = problem['x0']
+        x = problem.x0
 
         f, g = self.fun(x)
         gnorm = norm(g) 
-        alpha = problem['StepLength']  # 初始步长参数
-        for i in range(1, options['MaxIters']):
+        alpha = problem.StepLength  # 初始步长参数
+        for i in range(1, problem.MaxIters):
             d = self.P@x
             gtd = np.dot(g, d)
             alpha, xalpha, falpha, galpha = wolfe_line_search(x, f, gtd, d, self.fun, alpha)
@@ -31,15 +31,15 @@ class MatrixVectorProductGradientOptimizer(Optimizer):
             g = galpha
             gnorm = norm(g)
 
-            if alpha < problem['StepLengthTol']:
+            if alpha < problem.StepLengthTol:
                 break
 
-            if problem['Print']:
+            if problem.Print:
                 print(f'current step {i}, StepLength = {alpha}, ', end='')
                 print(f'nfval = {self.NF}, f = {f}, gnorm = {gnorm}')
 
-            if gnorm < problem['NormGradTol']:
-                print(f"The norm of current gradient is {gnorm}, which is smaller than the tolerance {problem['NormGradTol']}")
+            if gnorm < problem.NormGradTol:
+                print(f"The norm of current gradient is {gnorm}, which is smaller than the tolerance {problem.NormGradTol}")
                 break
 
         return x, f, g
