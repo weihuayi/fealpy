2aeca469265a8d8aaa38a7a990f542674c31be05
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-08 10:45:43 +0800

    update IntervalMesh

---
+++ test/mesh/test_interval_mesh.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pytest
+
+from fealpy.mesh import IntervalMesh
+
+
+def test_interval_domain():
+    mesh = IntervalMesh.from_interval_domain([0, 1], nx=10)
+
+    fig, axes = plt.subplots()
+    mesh.add_plot(axes)
+    mesh.find_node(axes, showindex=True)
+    mesh.find_cell(axes, showindex=True) 
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    test_interval_domain()
+
