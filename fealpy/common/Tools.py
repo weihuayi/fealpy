e93b4436bbf8902609670145e426cd6af0b3afb3
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-11 22:09:40 +0800

    fix np.int bug in fealpy/common/Tools.py

--- fealpy/common/Tools.py
+++ fealpy/common/Tools.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def ranges(nv, start = 0):
     shifts = np.cumsum(nv)
-    id_arr = np.ones(shifts[-1], dtype=np.int)
+    id_arr = np.ones(shifts[-1], dtype=np.int_)
     id_arr[shifts[:-1]] = -np.asarray(nv[:-1])+1
     id_arr[0] = start
     return id_arr.cumsum()
