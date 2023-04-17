253427f20b3474c107c32bff6b15ff904aa7bb5a
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-17 10:19:27 +0800

    update

--- fealpy/fem/__init__.py
+++ fealpy/fem/__init__.py
@@ -16,6 +16,7 @@ from .ScalarSourceIntegrator import ScalarSourceIntegrator
 
 # Integrator for vector case
 from .VectorSourceIntegrator import VectorSourceIntegrator
+from .VectorDiffusionIntegrator import VectorDiffusionIntegrator
 
 from .TrussStructureIntegrator import TrussStructureIntegrator
 from .DiffusionIntegrator import DiffusionIntegrator
