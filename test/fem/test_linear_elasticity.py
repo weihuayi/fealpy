cd947d4f17222527a74752f501e31475780f77b6
Huayi Wei <weihuayi@xtu.edu.cn>
2023-04-23 09:02:11 +0800

    udpate

--- test/fem/test_linear_elasticity.py
+++ test/fem/test_linear_elasticity.py
@@ -35,6 +35,9 @@ def test_linear_elasticity_lfem(p, n):
     bform.assembly()
 
     lform = LinearForm(vspace)
+    lform.add_domain_integrator(VectorSourceIntegrator(pde.source, q=1)
+    lform.add_boundary_integrator(VectorNeumannBCIntegrator(pde.neumann,
+        threshold=pde.is_neumann_boundary, q=1)
     lform.assembly()
 
     A = bform.get_matrix()
