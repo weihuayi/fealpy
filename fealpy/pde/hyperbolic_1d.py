77b871287d03c172a1f98643bbdddefefb3f1197
BellaLq <2655493031@qq.com>
2023-05-10 14:40:51 +0800

    update

---
+++ fealpy/pde/hyperbolic_1d.py
@@ -0,0 +1,85 @@
+import numpy as np
+from fealpy.decorator import cartesian
+
+class ExcitationTubePDEData:
+    def __init__(self, D=(0, 2), T=(0, 4)):
+        """
+        @brief 模型初始化函数
+        @param[in] D 模型空间定义域
+        @param[in] T 模型时间定义域
+        """
+        self._domain = D 
+        self._duration = T 
+
+    def domain(self):
+        """
+        @brief 空间区间
+        """
+        return self._domain
+
+    def duration(self):
+        """
+        @brief 时间区间
+        """
+        return self._duration 
+        
+    @cartesian
+    def solution(self, p, t):
+        """
+        @brief 真解函数
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+
+        @return 真解函数值
+        """
+        val = np.zeros_like(p)
+        flag1 = p <= t
+        flag2 = p > t+1
+        flag3 = ~flag1 & ~flag2
+        
+        val[flag1] = 1
+        val[flag3] = 1 - p[flag3] + t[flag3]
+        val[flag2] = p[flag2] - t[flag2] - 1
+        
+        return val
+
+    @cartesian
+    def init_solution(self, p, t):
+        """
+        @brief 真解函数
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+
+        @return 真解函数值
+        """
+        val = np.zeros_like(p)
+        val = abs(p-1)
+        
+        return val
+        
+    @cartesian
+    def source(self, p, t):
+        """
+        @brief 方程右端项 
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+
+        @return 方程右端函数值
+        """
+        return 0.0
+
+    @cartesian    
+    def dirichlet(self, p, t):
+        """
+        @brief Dirichlet 边界条件
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+        """
+        return np.ones(t.shape)
+        
+    def a(self):
+        return 1
