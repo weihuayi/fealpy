29b1f8cc9e4b19051a42989fb648eb55228c7bf7
Huayi Wei <weihuayi@xtu.edu.cn>
2023-05-18 16:01:17 +0800

    update

--- fealpy/functionspace/conforming_scalar_ve_space_2d.py
+++ fealpy/functionspace/conforming_scalar_ve_space_2d.py
@@ -8,11 +8,11 @@ from ..quadrature import GaussLegendreQuadrature
 from ..quadrature import PolygonMeshIntegralAlg
 from .ScaledMonomialSpace2d import ScaledMonomialSpace2d
 
-class CVEMDof2d():
+class CSVEDof2d():
     def __init__(self, mesh, p):
         self.p = p
         self.mesh = mesh
-        #self.cell2dof, self.cell2dofLocation = self.cell_to_dof()
+        self.cell2dof = self.cell_to_dof()
 
     def is_boundary_dof(self, threshold=None):
         idx = self.mesh.ds.boundary_edge_index()
@@ -39,6 +39,41 @@ class CVEMDof2d():
         return self.mesh.number_of_local_ipoints(self.p, iptype=doftype)
 
     def interpolation_points(self, index=np.s_[:]):
-        return self.mesh.interpolation_points(self.p,scale=0.3)
+        return self.mesh.interpolation_points(self.p, scale=0.3)
 
 
+class ConformingScalarVESpace2d():
+    def __init__(self, mesh, p=1, q=None, bc=None):
+        """
+        p: the space order
+        q: the index of integral formular
+        bc: user can give a barycenter for every mesh cell
+        """
+        self.mesh = mesh
+        self.p = p
+        self.smspace = ScaledMonomialSpace2d(mesh, p, q=q, bc=bc)
+        self.cellmeasure = self.smspace.cellmeasure
+        self.dof = CSVEDof2d(mesh, p)
+
+        self.itype = self.mesh.itype
+        self.ftype = self.mesh.ftype
+        self.stype = 'cvem' # 空间类型
+
+    def number_of_global_dofs(self):
+        return self.dof.number_of_global_dofs()
+
+    def number_of_local_dofs(self, doftype='all'):
+        return self.dof.number_of_local_dofs(doftype=doftype)
+
+    def interpolation_points(self, index=np.s_[:]):
+        return self.dof.interpolation_points()
+
+    def array(self, dim=None, dtype=np.float64):
+        gdof = self.number_of_global_dofs()
+        if dim is None:
+            shape = gdof
+        elif type(dim) is int:
+            shape = (gdof, dim)
+        elif type(dim) is tuple:
+            shape = (gdof, ) + dim
+        return np.zeros(shape, dtype=dtype)
--- fealpy/functionspace/conforming_vector_ve_space_2d.py
+++ fealpy/functionspace/conforming_vector_ve_space_2d.py
@@ -1,155 +1,85 @@
 import numpy as np
 from numpy.linalg import inv
 
-#from .Function import Function
-#from .ScaledMonomialSpace2d import ScaledMonomialSpace2d
+from .Function import Function
+from .ScaledMonomialSpace2d import ScaledMonomialSpace2d
+
+class CVVEDof2d:
+    def __init__(self, mesh, p):
+        self.mesh = mesh
+        self.p = p
+        self.itype = self.mesh.itype
+        self.cell2dof = self.cell_to_dof()
+
+    def edge_to_dof(self, index=np.s_[:]):
+        e2p = self.mesh.edge_to_ipoint(self.p, index=index)
+        NE, ldof = e2p.shape
+        edge2dof = np.zeros(NE, 2*ldof, dtype=e2p.dtype)
+        edge2dof[:, 0::2] = 2*e2p
+        edge2dof[:, 1::2] = edge2dof[:, 0::2] + 1
+        return edge2dof
+
+    face_fo_dof = edge_to_dof
+
+    def cell_to_dof(self, index=np.s_[:]):
+        """
+        """
+        cell = self.mesh.ds._cell
+
+        if p == 1:
+            cellLocation = self.mesh.ds.cellLocation
+            cell2dof = np.zeros(2*len(cell), dtype=cell.dtype)
+            cell2dof[0::2] = 2*cell
+            cell2dof[1::2] = cell2dof[0::2] + 1
+            return np.hsplit(cell2dof, 2*cellLocation[1:-1])[index]
+
+        NC = self.mesh.number_of_cells()
+        ldof = self.number_of_local_dofs(doftype='all')
+
+        location = np.zeros(NC+1, dtype=self.itype)
+        location[1:] = np.add.accumulate(ldof)
+
+        cell2dof = np.zeros(location[-1], dtype=self.itype)
+
+        edge2dof = self.edge_to_dof()
+        edge2cell = self.ds.edge_to_cell()
+
+        edof = self.number_of_local_dofs(doftype='edge')
+        idx = location[edge2cell[:, [0]]] + edge2cell[:, [2]]*(edof-2)+ np.arange(edof-2)
+        cell2dof[idx] = edge2dof[:, 0:edof-2]
+
+        isInEdge = (edge2cell[:, 0] != edge2cell[:, 1])
+        idx = (location[edge2cell[isInEdge, 1]] + edge2cell[isInEdge, 3]*(edof-2)).reshape(-1, 1) + np.arange(edof-2) 
+        cell2dof[idx] = edge2dof[isInEdge, edof-1:1:-1]
+
+        NN = self.number_of_nodes()
+        NV = self.mesh.ds.number_of_vertices_of_cells()
+        NE = self.number_of_edges()
+        cdof = self.number_of_local_dofs(p, iptype='cell') 
+        idx = (location[:-1] + NV*2*p).reshape(-1, 1) + np.arange(cdof)
+        cell2dof[idx] = 2*NN + NE*2*(p-1) + np.arange(NC*cdof).reshape(NC, cdof)
+
+        return np.hsplit(cell2dof, location[1:-1])[index]
+
+    def number_of_global_dofs(self):
+        return 2*self.mesh.number_of_global_ipoints(self.p)
+
+    def number_of_local_dofs(self, doftype='all'):
+        return 2*self.mesh.number_of_local_ipoints(self.p, iptype=doftype)
+
+    def interpolation_points(self, index=np.s_[:]):
+        return self.mesh.interpolation_points(self.p, scale=0.3)
 
 
-class ConformingNavierStokesVEMSpace2d:
+class ConformingVectorVESpace2d:
     def __init__(self, mesh, p=1):
         self.mesh = mesh
         self.p = p
         self.itype = mesh.itype
         self.ftype = mesh.ftype
+        self.dof = CVVEDof2d(mesh, p)
 
-        self.dof = CNSVEMDof2d(mesh, p)
 
-
-class CNSVEMDof2d:
-    def __init__(self, mesh, p):
-        self.p = p
-        self.mesh = mesh
-
-    def is_boundary_dof(self):
-        mesh = self.mesh
-        gdof = self.number_of_global_dofs()
-        edge2dof = self.edge_to_dof()
-        
-        isBdDof = np.zeros(gdof, dtype=np.bool_)
-        isBdEdge = mesh.ds.boundary_edge_flag()
-        isBdDof[edge2dof[isBdEdge]] = True   
-        return isBdDof
-
-    def edge_to_dof(self):
-        mesh = self.mesh
-        p = self.p
-        
-        NE = mesh.number_of_edges()
-        NN = mesh.number_of_nodes()
-        edge = mesh.entity('edge')
-        edge2dof = np.zeros((NE, (p+1)*2), dtype=np.int_)
-        edge2dof[:, [0, p]] = edge
-        edge2dof[:, [p+1, -1]] = edge + NN
-        edge2dof[:, 1:p] = 2*NN + np.arange(NE*(p-1)).reshape(NE, p-1)
-        edge2dof[:, p+2:-1] = edge2dof[:, 1:p] + NE*(p-1)    
-        return edge2dof
-
-    def cell_to_dof(self):
-        mesh = self.mesh
-        p = self.p
-        ldof = self.number_of_local_dofs()
-        edge2dof = self.edge_to_dof()
-        
-        
-        isBdEdge = mesh.ds.boundary_edge_flag()
-        edge2cell = mesh.ds.edge_to_cell()
-        NN = mesh.number_of_nodes()
-        NC = mesh.number_of_cells()
-        NE = mesh.number_of_edges()
-        NV = mesh.ds.number_of_vertices_of_cells()
-        
-        
-        cell2dofLocation = np.zeros(NC+1, dtype=np.int_)  
-        cell2dofLocation[1:] = np.add.accumulate(ldof)
-        cell2dof = np.zeros(cell2dofLocation[-1], dtype=np.int_)
-        
-        rx = edge2dof[np.arange(NE)][:, 0:p+1]
-        ry = edge2dof[np.arange(NE)][:, p+1:]
-       
-        idxrx = cell2dofLocation[edge2cell[:, 0]]+p*edge2cell[:, 2]
-        idxrx = idxrx.reshape(-1,1)+np.arange(p+1)
-        cell2dof[idxrx] = rx
-            
-        idxry = cell2dofLocation[edge2cell[:, 0]]+p*edge2cell[:, 2]+p*NV[edge2cell[:, 0]]
-        idxry = idxry.reshape(-1,1)+np.arange(p+1)
-        cell2dof[idxry] = ry            
-       
-        idxlx = cell2dofLocation[edge2cell[~isBdEdge][:, 1]]+p*edge2cell[~isBdEdge][:, 3]
-        idxlx = idxlx.reshape(-1,1)+np.arange(p)
-        cell2dof[idxlx] = rx[~isBdEdge][:, ::-1][..., 0:p]    
-       
-        idxly = cell2dofLocation[edge2cell[~isBdEdge][:, 1]]+p*edge2cell[~isBdEdge][:, 3]+p*NV[edge2cell[~isBdEdge][:, 1]]
-        idxly = idxly.reshape(-1,1)+np.arange(p)
-        cell2dof[idxly] = ry[~isBdEdge][:, ::-1][..., 0:p]
-    
-        
-        idx1 = (2*NV*p).reshape(-1,1)+cell2dofLocation[0:-1].reshape(-1,1)+np.arange((p-2)*(p-1)/2)
-        data1 = np.arange(((p-2)*(p-1)/2)*NC)+2*NN+2*NE*(p-1)
-        cell2dof[idx1.flatten().astype(int)] = data1
-    
-        idx2 = (2*NV*p+(p-2)*(p-1)/2).reshape(-1,1)+cell2dofLocation[0:-1].reshape(-1,1)+np.arange(p*(p+1)/2-1)
-        data2 = np.arange((p*(p+1)/2-1)*NC)+2*NN+2*NE*(p-1)+NC*((p-2)*(p-1)/2)
-        cell2dof[idx2.flatten().astype(int)] = data2
-        
-        return np.hsplit(cell2dof, cell2dofLocation[1:-1])
-
-    def number_of_global_dofs(self):
-        """
-        @brief 返回全局自由度的个数
-        """
-        return self.mesh.number_of_global_ipoints(self.p)*2
-    
-    def number_of_local_dofs(self, doftype='all'):
-        """
-        @brief  返回每个单元自由度的个数
-        """
-        return self.mesh.number_of_local_ipoints(self.p, iptype=doftype)*2
-        
-    def interpolation_points(self, index=np.s_[:], scale:float=0.4):
-        p = self.p
-        mesh = self.mesh
-        node = mesh.entity('node')
-        if p==1:
-            return np.vstack((node,node))
-        gdof = self.number_of_global_dofs()
-        GD = mesh.geo_dimension()
-        NN = mesh.number_of_nodes()
-        NE = mesh.number_of_edges()
-        NC = mesh.number_of_cells()
-        
-        start = 0
-        ipoint = np.zeros((gdof,GD),dtype=np.float_)
-        ipoint[start:2*NN,:] = np.vstack((node,node))
-        
-        start += 2*NN
-        edge = self.mesh.entity('edge')
-        qf = self.mesh.integrator(p+1, etype='edge', qtype='lobatto')
-        bcs = qf.quadpts[1:-1, :]
-        eips = np.einsum('ij, ...jm->...im', bcs, node[edge, :]).reshape(-1, GD)
-        print(ipoint[start:(p-1)*NE*2,:].shape,(p-1)*NE*2)
-        ipoint[start:start+(p-1)*NE*2, :] = np.vstack((eips,eips))  
-        start += 2*(p-1)*NE
-        if p == 2:
-            ipoint[start:, :] = np.vstack((self.mesh.entity_barycenter('cell'),self.mesh.entity_barycenter('cell')))
-            return ipoint
-        h = np.sqrt(self.mesh.cell_area())[:, None]*scale
-        bc = self.mesh.entity_barycenter('cell')
-        t = np.array([
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [0.5, np.sqrt(3)/2]],dtype=np.float_)
-        
-        t -= np.array([0.5, np.sqrt(3)/6.0], dtype=np.float_)
-
-        tri = np.zeros((NC, 3, GD))
-        tri[:, 0, :] = bc + t[0]*h
-        tri[:, 1, :] = bc + t[1]*h
-        tri[:, 2, :] = bc + t[2]*h
-
-        bcs = self.mesh.multi_index_matrix(2*p-2)/(2*p-2)
-        ipoint[start:start+(p-2)*(p-1)//2,:] = np.einsum('ij, ...jm->...im',
-                bcs[:(p-1)*(p-2)//2,:], tri).reshape(-1, GD)
-        return ipoint
 
 
 
