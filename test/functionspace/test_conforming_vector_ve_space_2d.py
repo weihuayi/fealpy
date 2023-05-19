cdef0b47bc7a5e6b39587bf9f254da5891f5cf3c
gao <1115633022@qq.com>
2023-05-19 19:58:17 +0800

    <conforming_vector_ve_sapce_2d.interpolation_point>

--- test/functionspace/test_conforming_vector_ve_space_2d.py
+++ test/functionspace/test_conforming_vector_ve_space_2d.py
@@ -4,38 +4,74 @@ import matplotlib.pyplot as plt
 
 from fealpy.mesh import TriangleMesh
 from fealpy.mesh.polygon_mesh import PolygonMesh
-from fealpy.functionspace.conforming_vector_ve_space_2d import CNSVEMDof2d
-from fealpy.functionspace.conforming_scalar_ve_space_2d import CVEMDof2d
+from fealpy.functionspace.conforming_vector_ve_space_2d import CVVEDof2d 
 
-@pytest.mark.parametrize("p", range(1, 10))
-def test_dof(p):
+
+def test_dof_3(p):
     tmesh = TriangleMesh.from_one_triangle()
     tmesh.uniform_refine()
     pmesh = PolygonMesh.from_mesh(tmesh) 
-    dof = CNSVEMDof2d(pmesh, p)
+    dof = CVVEDof2d(pmesh, p)
+
     isBdDof = dof.is_boundary_dof()
-    print(isBdDof)
-    dof1 = CVEMDof2d(pmesh, p)
-    isBdDof1 = dof1.is_boundary_dof()
-    print(isBdDof1)
+    result = np.array([True,  True,  True,  True , True,  True,  True , True, True,  True,  True,  True,True,  True,  True,  True , True,  True,  True,True,  True,  True, True,True, True,  True,  True,  True,  True,  True,  True,  True,  True,  True,  True, True,False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False,False, False, False, False, False, False, False, False, False, False, False, False, False])
+    np.testing.assert_equal(isBdDof, result)
+
     gdof = dof.number_of_global_dofs()
-    gdof1 = dof1.number_of_global_dofs()
-    print(gdof)
-    print(gdof1)
+    assert gdof==72
+
     local = dof.number_of_local_dofs()
-    print(local)
+    result = np.array([24, 24, 24, 24])
+    np.testing.assert_equal(local, result)
+
     edge2dof = dof.edge_to_dof()
-    edge2dof1 = dof1.edge_to_dof()
-    print(edge2dof,edge2dof1)
+    result = np.array([[ 0,  1, 12, 13, 14, 15,  6,  7],
+         [ 8 , 9, 16, 17, 18 ,19,  0,  1],
+         [ 6,  7, 20, 21, 22, 23,  2,  3],
+         [ 2,  3 ,24, 25 ,26, 27 ,10 ,11],
+         [ 4,  5 ,28 ,29 ,30 ,31  ,8  ,9],
+         [10, 11, 32 ,33 ,34 ,35  ,4  ,5],
+         [ 6,  7, 36 ,37, 38 ,39 , 8  ,9],
+         [10, 11, 40, 41, 42, 43,  6,  7],
+         [ 8,  9, 44, 45, 46, 47, 10, 11]])
+    np.testing.assert_equal(edge2dof, result)
+
     cell2dof = dof.cell_to_dof()
-    cell2dof1 = dof1.cell_to_dof()
-    print(cell2dof,cell2dof1)
+    result = [np.array([ 0,  1, 12, 13, 14, 15,  6,  7, 36, 37, 38, 39,  8,  9, 16, 17, 18,
+       19, 48, 49, 50, 51, 52, 53]), np.array([ 6,  7, 20, 21, 22, 23,  2,  3, 24, 25, 26, 27, 10, 11, 40, 41, 42,
+       43, 54, 55, 56, 57, 58, 59]), np.array([ 8,  9, 44, 45, 46, 47, 10, 11, 32, 33, 34, 35,  4,  5, 28, 29, 30,
+       31, 60, 61, 62, 63, 64, 65]), np.array([11, 10, 47, 46, 45, 44,  9,  8, 39, 38, 37, 36,  7,  6, 43, 42, 41,
+       40, 66, 67, 68, 69, 70, 71])]
+    for a0, a1 in zip(cell2dof, result):
+        np.testing.assert_equal(a0, a1) 
+
+    if False:
+        fig, axes = plt.subplots()
+        pmesh.add_plot(axes)
+        pmesh.find_node(axes, showindex=True)
+        pmesh.find_cell(axes, showindex=True)
+        pmesh.find_edge(axes, showindex=True)
+        plt.show()
+
+def test_interpolation_points_4(p):
+    tmesh = TriangleMesh.from_one_triangle()
+    tmesh.uniform_refine()
+    pmesh = PolygonMesh.from_mesh(tmesh) 
+
+    dof = CVVEDof2d(pmesh, p)
+    ips = dof.interpolation_points(scale=0.3)
+    print(ips)
 
     fig, axes = plt.subplots()
     pmesh.add_plot(axes)
-    pmesh.find_node(axes, showindex=True)
+    pmesh.find_node(axes, node=ips, showindex=True)
     pmesh.find_cell(axes, showindex=True)
     pmesh.find_edge(axes, showindex=True)
     plt.show()
+
+
+
+
 if __name__ == "__main__":
-    test_dof(3)
+    #test_dof_3(3)
+    test_interpolation_points_4(4)
