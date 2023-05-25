ab7ba72485f09c9e00a8003c777da422ee87f897
gao <1115633022@qq.com>
2023-05-25 21:48:16 +0800

    <conforming_scalar_vem_h1_projector.righthand>

--- test/vem/test_conforming_scalar_vem_h1_projector.py
+++ test/vem/test_conforming_scalar_vem_h1_projector.py
@@ -9,13 +9,13 @@ from fealpy.vem.conforming_scalar_vem_h1_projector import ConformingScalarVEMH1P
 from fealpy.mesh import MeshFactory as MF
 from fealpy.mesh.polygon_mesh import PolygonMesh
 def test_assembly_cell_righthand_side(p,plot=False):
-    nx = 1
-    ny = 1
+    nx = 2
+    ny = 2
     dim = 2
     domain = np.array([0, 1, 0, 1])
     mesh = MF.boxmesh2d(domain, nx, ny, meshtype ='poly')
     space = ConformingVirtualElementSpace2d(mesh, p=p)
-    B = space.matrix_B()
+    realB = space.matrix_B()
     if plot:
         fig ,axes = plt.subplots()
         mesh.add_plot(axes)
@@ -30,7 +30,16 @@ def test_assembly_cell_righthand_side(p,plot=False):
     space =  ConformingScalarVESpace2d(mesh, p=p)
     b = ConformingScalarVEMH1Projector2d()
     B = b.assembly_cell_righthand_side(space)
-    print(B)
+
+    ldof = mesh.number_of_local_ipoints(p)
+    a = np.add.accumulate(ldof)
+    NC = mesh.number_of_cells()
+    location = np.zeros(NC+1, dtype=np.int_)
+    location[1:] = a
+    for i in range(NC):
+        np.testing.assert_equal(realB[:,location[i]:location[i+1]], B[i])
+        i = i+1
+
     if plot:
         fig ,axes = plt.subplots()
         mesh.add_plot(axes)
