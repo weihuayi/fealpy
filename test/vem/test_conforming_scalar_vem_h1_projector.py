2a9879036e94f38676728c8721be3177e18529df
gao <1115633022@qq.com>
2023-05-29 21:36:22 +0800

    fealpy/vem/conforming_scalar_vem_h1_projector.py

--- test/vem/test_conforming_scalar_vem_h1_projector.py
+++ test/vem/test_conforming_scalar_vem_h1_projector.py
@@ -18,6 +18,8 @@ def test_assembly_cell_righthand_side_and_dof_matrix(p,plot=False):
     realB = space.matrix_B()
     H = space.H
     realD = space.matrix_D(H)
+    realG = space.matrix_G(realB, realD)
+    realPI = space.matrix_PI_1(realG,realB)
 
     if plot:
         fig ,axes = plt.subplots()
@@ -33,7 +35,10 @@ def test_assembly_cell_righthand_side_and_dof_matrix(p,plot=False):
     space =  ConformingScalarVESpace2d(mesh, p=p)
     b = ConformingScalarVEMH1Projector2d()
     B = b.assembly_cell_righthand_side(space)
-    D = b.assembly_cell_dof_matrix(space,H)
+    D = b.assembly_cell_dof_matrix(space)
+    G = b.assembly_cell_lefthand_side(space)
+    PI = b.assembly_cell_H1_matrix(space)
+
 
     ldof = mesh.number_of_local_ipoints(p)
     a = np.add.accumulate(ldof)
@@ -41,11 +46,17 @@ def test_assembly_cell_righthand_side_and_dof_matrix(p,plot=False):
     location = np.zeros(NC+1, dtype=np.int_)
     location[1:] = a
     for i in range(NC):
-        np.testing.assert_allclose(realB[:,location[i]:location[i+1]],
-                B[i],atol=1e-14)
+        np.testing.assert_allclose(realB[:,location[i]:location[i+1]], B[i],atol=1e-14)
         np.testing.assert_allclose(realD[location[i]:location[i+1],:], D[i], atol=1e-14)
+        if p>1:
+            np.testing.assert_allclose(realG[i], G[i] ,atol=1e-10)
+        np.testing.assert_allclose(realPI[i], PI[i], atol=1e-10)
+
         #np.testing.assert_equal(realB[:,location[i]:location[i+1]], B[i])
         #np.testing.assert_equal(realD[location[i]:location[i+1],:], D[i])
+        #np.testing.assert_equal(realG[i], G[i])
+        #np.testing.assert_equal(realPI[i], PI[i])
+
         i = i+1
 
     if plot:
