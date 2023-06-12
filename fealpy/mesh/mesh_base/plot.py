bee4809629e234cfe946c3f4aad28447066988aa
AlbertZyy <zhengyy0621@foxmail.com>
2023-06-12 22:59:16 +0800

    mesh: update plot

--- fealpy/mesh/mesh_base/plot.py
+++ fealpy/mesh/mesh_base/plot.py
@@ -30,50 +30,25 @@ class Plotable():
     """
     _ploter_class: Optional[str] = None
 
-    @property
-    def add_plot(self):
-        if not isinstance(self, Mesh):
-            raise TypeError("Plotable only works for mesh type,"
-                            f"but got {self.__class__.__name__}.")
-
-        from ..plotting.classic import get_ploter
-
-        if self._ploter_class is not None:
-            return get_ploter(self._ploter_class)(self)
-        else:
-            raise Exception('MeshPloter of the type of mesh should be specified'
-                            'before drawing. If a mesh is inherited from Plotable,'
-                            'use MeshClass.set_ploter(MeshPloterClass) to specify.')
-
     @classmethod
     def set_ploter(cls, ploter: str):
         cls._ploter_class = ploter
 
-    def find_entity(self, axes, etype: Union[int, str], index=np.s_[:],
-                    showindex: bool=False, color='r', markersize=20,
-                    fontcolor='k', fontsize=24):
+    @property
+    def add_plot(self):
+        """
+        @brief Show the mesh.
+        """
+        from ..plotting.classic import get_ploter
+        return get_ploter(self._ploter_class)(self)
+
+    @property
+    def find_entity(self):
         """
         @brief Show the barycenter of each entity.
         """
-        from ..plotting import artist as A
-        from ..plotting.classic import array_color_map
-
-        if not isinstance(self, Mesh):
-            raise TypeError("Plotable only works for mesh type,"
-                            f"but got {self.__class__.__name__}.")
-
-        bc = self.entity_barycenter(etype=etype, index=index)
-        if bc.ndim == 1:
-            bc = bc[:, None]
-
-        if isinstance(color, np.ndarray) and np.isreal(color[0]):
-            mapper = array_color_map(color, 'rainbow')
-            color = mapper.to_rgba(color)
-
-        A.scatter(axes=axes, points=bc, color=color, markersize=markersize)
-        if showindex:
-            A.show_index(axes=axes, location=bc, number=index,
-                         fontcolor=fontcolor, fontsize=fontsize)
+        from ..plotting.classic import get_ploter
+        return get_ploter('finder')(self)
 
     def find_node(self, axes, *,
             node: Optional[NDArray]=None,
@@ -100,29 +75,20 @@ class Plotable():
         """
         if node is None:
             return self.find_entity(
-                    axes, 'node', index=index,
+                    axes, etype_or_node='node', index=index,
                     showindex=showindex,
                     color=color,
                     markersize=markersize,
                     fontsize=fontsize,
                     fontcolor=fontcolor)
         else:
-            from ..plotting import artist as A
-            from ..plotting.classic import array_color_map
-
-            if node.ndim == 1:
-                node_ = node[:, None]
-            else:
-                node_ = node
-
-            if isinstance(color, np.ndarray) and np.isreal(color[0]):
-                mapper = array_color_map(color, 'rainbow')
-                color = mapper.to_rgba(color)
-
-        A.scatter(axes=axes, points=node_, color=color, markersize=markersize)
-        if showindex:
-            A.show_index(axes=axes, location=node_, number=index,
-                            fontcolor=fontcolor, fontsize=fontsize)
+            return self.find_entity(
+                    axes, etype_or_node=node, index=index,
+                    showindex=showindex,
+                    color=color,
+                    markersize=markersize,
+                    fontsize=fontsize,
+                    fontcolor=fontcolor)
 
     def find_edge(self, axes,
             index=np.s_[:],
