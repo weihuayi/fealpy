c96df71a0f3ee0bc77ff87e9c1b76eea03b06cd0
Huayi Wei <weihuayi@xtu.edu.cn>
2023-06-17 21:51:08 +0800

    update

--- fealpy/mesh/octree.py
+++ fealpy/mesh/octree.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix
 
 from .hexahedron_mesh import HexahedronMesh 
-from .polyhedron_mesh import PolyhedronMesh 
 from ..common import ranges
 
 class Octree(HexahedronMesh):
