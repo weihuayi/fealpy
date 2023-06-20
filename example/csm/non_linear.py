7965c4b5f0ea7e7f5df12f75500a34f97bfb0c27
Huayi Wei <weihuayi@xtu.edu.cn>
2023-06-20 11:54:14 +0800

    udpate

--- example/csm/non_linear.py
+++ example/csm/non_linear.py
@@ -7,29 +7,29 @@ from fealpy.opt.MatrixVectorProductGradientOptimizer import MatrixVectorProductG
 
 class TwoNonlinearSpingsProblem(Problem):
     def __init__(self):
+        self.F = np.array([0.0, 100.0])
         x0 = np.zeros(2, dtype=np.float64)
         super().__init__(x0, self.energy)
+        self.MaxIters = 1000 
+        self.Preconditioner = self.tagnent_stiffness_matrix
 
     def energy(self, x):
-        f = np.array([0, 100])
-        U = np.array([25*x[0]**2 + 500/3*x[0]**3 + 50*(x[1]-x[0])**2 + 200/3*(x[1]-x[0])**3 - f[1]*x[1]])
-        P = np.array([300*x[0]**2+400*x[0]*x[1]-200*x[1]**2+150*x[0]-100*x[1],
-                     200*x[0]**2-400*x[0]*x[1]+200*x[1]**2-100*x[0]+100*x[1]])
+        F = self.F
+        U = 25.0 * x[0]**2 + 500 * x[0]**3 / 3.0 + \
+                50.0 * (x[1] - x[0])**2 + 200.0 * (x[1] - x[0])**3 / 2.0 - F[1]*x[1]
+
+        P = np.array([
+            300*x[0]**2+400*x[0]*x[1]-200*x[1]**2+150*x[0]-100*x[1],
+            200*x[0]**2-400*x[0]*x[1]+200*x[1]**2-100*x[0]+100*x[1]
+            ])
         return U, P
 
     def tagnent_stiffness_matrix(self, x):
-        Kt = np.array([[600*x[0]+400*x[1]+150, 400*(x[0]-x[1])-100],
-                       [400*(x[0]-x[1])-100, 400*x[1]-400*x[0]+100]])
+        Kt = np.array([
+            [600*x[0]+400*x[1]+150, 400*(x[0]-x[1])-100],
+            [400*(x[0]-x[1])-100, 400*x[1]-400*x[0]+100]
+            ])
         return Kt
-
-    def newton_raphson_preconditioner(self, x):
-        f = np.array([0, 100])
-        P = self.energy(x)[1]
-        R = f - P
-        Kt = self.tagnent_stiffness_matrix(x)
-        delx = np.linalg.solve(Kt, R)
-        x_next = x + delx
-        return x_next
 
 def two_nonlinear_springs_opt():
     problem = TwoNonlinearSpingsProblem()
