3f58234d36d582f75c627930d231175f7a3c7649
Huayi Wei <weihuayi@xtu.edu.cn>
2023-06-21 17:00:24 +0800

    update

--- fealpy/opt/newton_raphson_optimizer.py
+++ fealpy/opt/newton_raphson_optimizer.py
@@ -6,26 +6,21 @@ class NewtonRaphsonOptimizer(Optimizer):
     def __init__(self, problem: Problem) -> None:
         super().__init__(problem)
         self.P = problem.Preconditioner
-        assert self.P is not None
 
     def run(self):
         problem = self.problem
-        x = problem.x0
-        iter = 0
-        xold = x
-        P = problem.energy(x)[1]
-        R = problem.F - P
-        conv = np.sum(R**2)/(1+np.sum(problem.F**2))
-        c = 0
+        x0 = problem.x0
+        f0, grad0 = problem.objective(x0)
+        for i in range(problem.MaxIters):
+            du = -self.P(x0) @ grad0
+            x0 += du
+            f, grad = problem.objective(x0)
 
-        while conv > problem.StepLengthTol and iter < problem.MaxIters:
-            Kt = calculate_Kt(u)
-            delu = np.linalg.solve(Kt, R)
-            u = uold + delu
-            P = calculate_P(u)
-            R = f - P
-            conv = np.sum(R**2)/(1+np.sum(f**2))
-            c = np.abs(u_exact[1] - u[1])/np.abs(u_exact[1] - uold[1])**2 if iter > 0 else 0
-            uold = u
-            iter += 1
-        return u
+            if np.abs(f - f0) < problem.FunValDiff:
+                print()
+                break
+
+            if np.linalg.norm(grad) < problem.NormGradTol:
+                print()
+                break
+        return x0 
