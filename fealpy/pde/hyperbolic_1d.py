113e4ed4bdcd06fb99420317c8042f53baeaf4e1
brighthe <brighthe98@gmail.com>
2023-08-07 21:29:20 +0800

    update

--- fealpy/pde/hyperbolic_1d.py
+++ fealpy/pde/hyperbolic_1d.py
@@ -69,5 +69,78 @@ class Hyperbolic1dPDEData:
         """
         return np.ones(p.shape)
 
+    @cartesian    
+    def is_dirichlet_boundary(self, p):
+        """
+        @brief 判断给定的点是否在 Dirichlet 边界上
+
+        @param[in] p numpy.ndarray, 空间点
+
+        @return bool, 如果 p 在 Dirichlet 边界上则返回 True, 否则返回 False
+        """
+        return np.isclose(p, self._domain[0])
+
     def a(self):
         return 1
+
+
+class Hyperbolic1dSinData:
+    def __init__(self, D = (0, 1), T = (0, 0.5)):
+        """
+        @brief 模型初始化函数
+        @param[in] D 模型空间定义域
+        @param[in] T 模型时间定义域
+        """
+        self._domain = D 
+        self._duration = T 
+
+    def domain(self):
+        """
+        @brief 空间区间
+        """
+        return self._domain
+
+    def duration(self):
+        """
+        @brief 时间区间
+        """
+        return self._duration 
+        
+    @cartesian
+    def solution(self, p, t):
+        return 1 + np.sin(2 * np.pi * (p + 2 * t))
+
+    @cartesian
+    def init_solution(self, p):
+        """
+        @brief 初始解
+
+        @param[in] p numpy.ndarray, 空间点
+
+        @return 初始解函数值
+        """
+        return 1 + np.sin(2 * np.pi * p)
+
+    @cartesian    
+    def dirichlet(self, p, t):
+        """
+        @brief Dirichlet 边界条件
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+        """
+        return 1 + np.sin(4 * np.pi * t)
+
+    @cartesian    
+    def is_dirichlet_boundary(self, p):
+        """
+        @brief 判断给定的点是否在 Dirichlet 边界上
+
+        @param[in] p numpy.ndarray, 空间点
+
+        @return bool, 如果 p 在 Dirichlet 边界上则返回 True, 否则返回 False
+        """
+        return np.isclose(p, self._domain[1])
+
+    def a(self):
+        return -2
