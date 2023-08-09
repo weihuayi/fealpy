dc06183197a2bac93063cd0cfeeec0d930960cc4
ChaosTHL <2250213115@qq.com>
2023-08-09 23:12:04 +0800

    add BoxNBCSOlution in _init_文件

--- fealpy/pinn/modules/__init__.py
+++ fealpy/pinn/modules/__init__.py
@@ -1,7 +1,7 @@
 
 from .module import TensorMapping, Solution, ZeroMapping, Fixed, Extracted, Projected
 from .linear import StackStd, MultiLinear
-from .boundary import BoxDBCSolution
+from .boundary import BoxDBCSolution,BoxNBCSolution
 from .attention import GradAttention
 from .rfm import RandomFeature, RandomFeatureFlat, PoU, PoUSin
 from .loss import ScaledMSELoss
