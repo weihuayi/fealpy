fe68a3bad40de5ace0e4b8fb93f76fd8e0da1e40
AlbertZyy <zhengyy0621@foxmail.com>
2023-08-18 22:03:11 +0800

    update rfm

--- fealpy/ml/modules/__init__.py
+++ fealpy/ml/modules/__init__.py
@@ -3,5 +3,7 @@ from .module import TensorMapping, Solution, ZeroMapping, Fixed, Extracted, Proj
 from .linear import StackStd, MultiLinear
 from .boundary import BoxDBCSolution, BoxDBCSolution1d, BoxDBCSolution2d, BoxNBCSolution
 from .attention import GradAttention
-from .rfm import RandomFeatureUnit, LocalRandomFeature, RandomFeature, PoU, PoUSin
+from .rfm import RandomFeaturePoUSpace, LocalRandomFeatureSpace, RandomFeatureSpace
+from .activate import Sin, Cos, Tanh
+from .pou import PoUA, PoUSin
 from .loss import ScaledMSELoss
