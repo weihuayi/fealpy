72b2f9b1a623ed4cd1a4e82eb0cf9f3e3dba4045
ChaosTHL <2250213115@qq.com>
2023-08-26 17:13:54 +0800

    Add two examples

--- example/ml/Helmholtz_example1_rfm_dirchlet_2d.py
+++ example/ml/Helmholtz_example1_rfm_dirchlet_2d.py
@@ -1,28 +1,32 @@
+import time
 from math import sqrt
 
 import torch
 from torch import Tensor, sin
 from scipy.sparse.linalg import spsolve
 from scipy.sparse import csr_matrix
+from matplotlib import pyplot as plt
 
-from fealpy.ml.modules import RandomFeaturePoUSpace, PoUSin, Cos, RFFunction, Solution
+from fealpy.ml.modules import RandomFeatureSpace, Cos, Function, Solution
 from fealpy.mesh import UniformMesh2d, TriangleMesh
 
 #方程形式
 
 """
-    \Delta u(x,y) + 200 * u(x,y) = 0 ,   (x,y)\in \Omega
-    u(x,y) = \sin(10*x + 10*y) ,         (x,y)\in \partial\Omega
+
+    \Delta u(x,y) + k**2 * u(x,y) = 0 ,                            (x,y)\in \Omega
+    u(x,y) = \sin(\sqrt{k**2/2} * x + \sqrt{k**2/2} * y) ,         (x,y)\in \partial\Omega
 
 """
-k = torch.sqrt(torch.tensor(200))#波数
+
+k = torch.tensor(50)#波数
 
 #真解
 
 def real_solution(p: Tensor):
     x = p[:, 0:1]
     y = p[:, 1:2]
-    return sin(10 * x + 10 * y)
+    return sin(torch.sqrt(k**2/2) * x + torch.sqrt(k**2/2) * y)
 
 #边界条件
 
@@ -37,21 +41,15 @@ def source(p: Tensor):
 
 #超参数
 
-EXT = 2
-H = 2*1/EXT
-Jn = 512
+Jn = 5000
 
-EXTC = 90
+EXTC = 150
 HC = 2*1/EXTC
 
 #用网格进行配置点采样
+start_time = time.time()
 
-mesh = UniformMesh2d((0, EXT, 0, EXT), (H, H), origin=(-1, -1))
-node = torch.from_numpy(mesh.entity('node')).clone()
-space = RandomFeaturePoUSpace(2, Jn, Cos(), PoUSin(), centers=node, radius=H/2,
-                              bound=(k, torch.pi), print_status=True)
-
-
+space = RandomFeatureSpace(2, Jn, Cos(), bound=(k, torch.pi))
 mesh_col = UniformMesh2d((0, EXTC, 0, EXTC), (HC, HC), origin=(-1, -1))
 _bd_node = mesh_col.ds.boundary_node_flag()
 col_in = torch.from_numpy(mesh_col.entity('node', index=~_bd_node))
@@ -61,9 +59,9 @@ mesh_err = TriangleMesh.from_box([-1, 1, -1, 1], nx=50, ny=50)
 
 #计算基函数的值
 
-laplace_phi = space.L(col_in) / sqrt(col_in.shape[0])
-phi_in = space.U(col_in) / sqrt(col_in.shape[0])
-phi_bd = space.U(col_bd) / sqrt(col_bd.shape[0])
+laplace_phi = space.basis_laplace(col_in) / sqrt(col_in.shape[0])
+phi_in = space.basis_value(col_in) / sqrt(col_in.shape[0])
+phi_bd = space.basis_value(col_bd) / sqrt(col_bd.shape[0])
 
 #组装矩阵
 
@@ -77,33 +75,36 @@ A = csr_matrix(A_tensor.cpu().numpy())
 b = csr_matrix(b_tensor.cpu().numpy())
 
 um = spsolve(A.T@A, A.T@b)
-solution = RFFunction(space, torch.from_numpy(um))
+solution = Function(space, torch.from_numpy(um))
 
 #计算L2误差
 
 error = solution.estimate_error_tensor(real_solution, mesh=mesh_err)
 print(f"L-2 error: {error.data}")
+end_time = time.time()     # 记录结束时间
+training_time = end_time - start_time   # 计算训练时间
+print("训练时间为：", training_time, "秒")
 
 # 可视化
 
-from matplotlib import pyplot as plt
 fig = plt.figure()
-axes = fig.add_subplot(131, projection='3d')
-solution.add_surface(axes, box=[-1, 1, -1, 1], nums=[40, 40])
+axes = fig.add_subplot(131)
+Solution(solution).add_pcolor(axes, box=[-1, 1, -1, 1], nums=[300, 300])
 axes.set_xlabel('x')
 axes.set_ylabel('y')
-axes.set_zlabel('phi')
+axes.set_title('u')
 
-axes = fig.add_subplot(132, projection='3d')
-Solution(real_solution).add_surface(axes, box=[-1, 1, -1, 1], nums=[40, 40])
+axes = fig.add_subplot(132)
+Solution(real_solution).add_pcolor(axes, box=[-1, 1, -1, 1], nums=[300, 300])
 axes.set_xlabel('x')
 axes.set_ylabel('y')
-axes.set_zlabel('phi')
+axes.set_title('u_RFM')
 
-axes = fig.add_subplot(133, projection='3d')
-solution.diff(real_solution).add_surface(axes, box=[-1, 1, -1, 1], nums=[40, 40])
+axes = fig.add_subplot(133)
+qm = solution.diff(real_solution).add_pcolor(axes, box=[-1, 1, -1, 1], nums=[200, 200])
 axes.set_xlabel('x')
 axes.set_ylabel('y')
-axes.set_zlabel('phi')
+axes.set_title('diff')
+fig.colorbar(qm)
 
 plt.show()
