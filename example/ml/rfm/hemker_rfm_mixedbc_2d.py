891770be1b8dc831f2a378604146eefc6fe7082b
AlbertZyy <zhengyy0621@foxmail.com>
2023-09-08 20:13:34 +0800

    update example

--- example/ml/rfm/hemker_rfm_mixedbc_2d.py
+++ example/ml/rfm/hemker_rfm_mixedbc_2d.py
@@ -6,19 +6,20 @@ Equation:
 
 import torch
 from torch import Tensor
+from torch.nn import init
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 
 from fealpy.mesh import UniformMesh2d
 from fealpy.ml.modules import (
-    RandomFeatureSpace, PoUSpace, Cos, PoUSin, Function, Tanh,
+    RandomFeatureSpace, PoUSpace, Cos, PoUSin, Function,
     Solution
 )
 from fealpy.ml.sampler import Collocator, CircleCollocator
 
 PI = torch.pi
 
-ep = torch.tensor(0.1, dtype=torch.float64)
+ep = torch.tensor(0.01, dtype=torch.float64)
 b = torch.tensor([1.0, 0.0], dtype=torch.float64)
 
 def boundary_left(p: Tensor):
@@ -33,26 +34,29 @@ def boundary_circle(p: Tensor):
 def source(p: Tensor):
     return torch.zeros((p.shape[0], 1), dtype=p.dtype)
 
-Jn = 64
+Jn = 50
 
 mesh = UniformMesh2d((0, 6, 0, 3), (2, 2), origin=(-3, -3))
 node = torch.from_numpy(mesh.entity('node'))
 
 def factory(i: int):
     if i in {5, 6, 9, 10}:
-        sp = RandomFeatureSpace(2, Jn*4, Cos(), bound=(8.0, PI))
+        sp = RandomFeatureSpace(2, Jn*4, Cos(), bound=(16.0, PI))
+        init.normal_(sp.frequency, 0, 8)
     elif i in {0, 1, 2, 3}:
         sp = RandomFeatureSpace(2, 8, Cos(), bound=(0.5, PI))
+        init.normal_(sp.frequency, 0, 0.5)
     else:
-        sp = RandomFeatureSpace(2, Jn, Cos(), bound=(2.0, PI))
+        sp = RandomFeatureSpace(2, Jn, Cos(), bound=(4.0, PI))
+        init.normal_(sp.frequency, 0, 4.0)
     return sp
 
-space = PoUSpace(factory, centers=node, radius=1.0, pou=PoUSin())
+space = PoUSpace(factory, centers=node, radius=1.0, pou=PoUSin(), print_status=True)
 
 
 ### generate collocation points
 
-N = 300
+N = 200
 col_in = Collocator([-3, 9, -3, 3], [2*N, N]).run()
 col_in = col_in[col_in[:, 0]**2 + col_in[:, 1]**2 > 1, :]
 
@@ -102,8 +106,8 @@ model = Function(space, 1, torch.from_numpy(um))
 
 
 from matplotlib import pyplot as plt
-fig = plt.figure()
-axes = fig.add_subplot(211)
+fig = plt.figure("RFM for Hemker")
+axes = fig.add_subplot(111)
 
 class RemoveCircle(Solution):
     def forward(self, p: Tensor) -> Tensor:
@@ -113,8 +117,20 @@ class RemoveCircle(Solution):
         return ret
 
 qm = RemoveCircle(model).add_pcolor(axes, [-3, 9, -3, 3], [400, 200])
+axes.set_aspect('equal')
 fig.colorbar(qm)
 
-axes = fig.add_subplot(212)
+fig = plt.figure("LOADS")
+axes = fig.add_subplot(221)
+axes.scatter(space.partitions[1].space.frequency[:, 0], um[space.partition_basis_slice(1)])
+axes.set_title("PART-1")
+axes = fig.add_subplot(222)
+axes.scatter(space.partitions[5].space.frequency[:, 0], um[space.partition_basis_slice(5)])
+axes.set_title("PART-5")
+axes = fig.add_subplot(223)
+axes.scatter(space.partitions[9].space.frequency[:, 0], um[space.partition_basis_slice(9)])
+axes.set_title("PART-9")
+axes = fig.add_subplot(224)
 axes.scatter(space.partitions[13].space.frequency[:, 0], um[space.partition_basis_slice(13)])
+axes.set_title("PART-13")
 plt.show()
