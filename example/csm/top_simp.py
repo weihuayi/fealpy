6dfd3651faeaff4582f9d514eea3dca7b21182a2
brighthe <brighthe98@gmail.com>
2023-10-05 20:34:25 +0800

    update

--- example/csm/top_simp.py
+++ example/csm/top_simp.py
@@ -5,7 +5,7 @@ from scipy.sparse import lil_matrix, csc_matrix
 from scipy.sparse.linalg import spsolve
 
 class TopSimp:
-    def __init__(self, nelx: int = 6, nely: int = 2, volfrac: float = 0.5, 
+    def __init__(self, nelx: int = 60, nely: int = 20, volfrac: float = 0.5, 
                  E: float = 1.0, nu: float = 0.3, penal: float = 3.0, rmin: float = 1.5):
         self._nelx = nelx
         self._nely = nely
@@ -16,7 +16,7 @@ class TopSimp:
         self._rmin = rmin
         self._x = np.full((nely, nelx), volfrac)
 
-    def lk(self) -> np.ndarray:
+    def lk(self):
         E, nu = self._E, self._nu
         k = [1/2 - nu/6, 1/8 + nu/8, -1/4 - nu/12, -1/8 + 3*nu/8, -1/4 + nu/12, -1/8 - nu/8, nu/6, 1/8 - 3*nu/8]
         KE = E / (1 - nu**2) * np.array([
@@ -33,8 +33,7 @@ class TopSimp:
         return KE
     
 
-    def FE(self, x: np.ndarray) -> np.ndarray:
-        nelx, nely, penal = self._nelx, self._nely, self._penal  
+    def FE(self, nelx, nely, penal, x):
         KE = self.lk()
         K = lil_matrix( ( 2*(nelx+1)*(nely+1), 2*(nelx+1)*(nely+1) ) )
         F = lil_matrix( ( 2*(nelx+1)*(nely+1), 1) )
@@ -46,7 +45,7 @@ class TopSimp:
                 n2 = (nely+1) * (elx+1) + ely
                 edof = np.array([2*n1, 2*n1+1, 2*n2, 2*n2+1, 2*n2+2, 2*n2+3, 2*n1+2, 2*n1+3])
 
-                K[np.ix_(edof, edof)] += x[ely, elx] ** penal * KE    
+                K[np.ix_(edof, edof)] += x[ely, elx] ** penal * KE
 
         F[1] = -1
         fixeddofs = np.union1d(np.arange(0, 2*(nely+1), 2), np.array([2*(nelx+1)*(nely+1) - 1]))
@@ -58,23 +57,27 @@ class TopSimp:
         
         return U
 
-    def check(self, x, dc):
 
-        dcn = np.zeros((self._nely, self._nelx))
+    def check(self, nelx, nely, rmin, x, dc):
+        assert np.all(dc <= 0), "dc should be non-positive (usually negative)"
 
-        for i in range(self._nelx):
-            for j in range(self._nely):
+        dcn = np.zeros((nely, nelx))
+
+        for i in range(nelx):
+            for j in range(nely):
                 sum_val = 0.0  
-                for k in range( max( i - int(self._rmin), 0 ), min( i + int(self._rmin), self._nelx ) ):
-                    for l in range( max( j - int(self._rmin), 0 ), min( j + int(self._rmin), self._nely ) ):
-                        fac = self._rmin - np.sqrt((i - k)**2 + (j - l)**2)
+                for k in range( max( i - int(rmin), 0 ), min( i + int(rmin) + 1, nelx ) ):
+                    for l in range( max( j - int(rmin), 0 ), min( j + int(rmin) + 1, nely ) ):
+                        fac = rmin - np.sqrt((i - k)**2 + (j - l)**2)
                         sum_val += max(0, fac)
                         dcn[j, i] += max(0, fac) * x[l, k] * dc[l, k]
                 dcn[j, i] /= (x[j, i] * sum_val)
 
         return dcn
 
-    def OC(self, x, dc):
+
+    def OC(self, nelx, nely, volfrac, x, dc):
+        assert np.all(dc <= 0), "dc should be non-positive (usually negative)"
 
         l1, l2 = 0, 1e5
         move = 0.2  
@@ -82,13 +85,14 @@ class TopSimp:
 
         while (l2 - l1) > 1e-4:
             lmid = 0.5 * (l2 + l1)
-            tmp1 = x - move
-            tmp2 = x + move
-            tmp3 = x * np.sqrt(-dc / lmid)
-            tmp4 = np.minimum(tmp2, tmp3)
-            tmp5 = np.maximum(tmp1, tmp4)
+            tmp0 = x - move
+            tmp1 = x + move
+            tmp2 = x * np.sqrt(-dc / lmid)
+            tmp3 = np.minimum(tmp1, tmp2)
+            tmp4 = np.minimum(1, tmp3) 
+            tmp5 = np.maximum(tmp0, tmp4)
             xnew = np.maximum(0.001, tmp5)
-            if np.sum(xnew) - self._volfrac * self._nelx * self._nely > 0:
+            if np.sum(xnew) - volfrac * nelx * nely > 0:
                 l1 = lmid
             else:
                 l2 = lmid
@@ -96,7 +100,7 @@ class TopSimp:
         return xnew
 
     def optimize(self):
-        nelx, nely, penal, x = self._nelx, self._nely, self._penal, self._x
+        nelx, nely, rmin, penal, x, volfrac = self._nelx, self._nely, self._rmin, self._penal, self._x, self._volfrac
 
         loop = 0
         change = 1.0
@@ -104,9 +108,7 @@ class TopSimp:
         while change > 0.01:
             loop += 1
             xold = np.copy(x)
-            print("oldx:", x.round(4))
-            U = self.FE(x)
-            # print("U:", U.round(4))
+            U = self.FE(nelx, nely, penal, x)
             KE = self.lk()
             c = 0 
             dc = np.zeros((nely, nelx)) 
@@ -120,14 +122,11 @@ class TopSimp:
                     c += x[ely, elx]**penal * Ue.T @ KE @ Ue 
                     dc[ely, elx] = -penal * x[ely, elx]**(penal - 1) * Ue.T @ KE @ Ue
 
-            print("olddc:", dc.round(4))
-            dc = self.check(x, dc)
-            print("newdc:", dc.round(4))
-            x = self.OC(x, dc)
-            print("newx:", x.round(4))
+            dc = self.check(nelx, nely, rmin, x, dc)
+            x = self.OC(nelx, nely, volfrac, x, dc)
 
             change = np.max(np.abs(x - xold))
-            print(f' Iter.: {loop:4d} Objective.: {c:10.4f} Volfrac.: {np.sum(x)/(self._nelx*self._nely):6.3f} change.: {change:6.3f}')
+            print(f' Iter.: {loop:4d} Objective.: {c:10.4f} Volfrac.: {np.sum(x)/(nelx*nely):6.3f} change.: {change:6.3f}')
             
             plt.imshow(-x, cmap='gray')
             plt.axis('off')
@@ -139,5 +138,5 @@ class TopSimp:
         plt.show()
 
 # 创建一个TopSimp对象实例
-tsp = TopSimp()
+tsp = TopSimp(nelx=60, nely=20)
 print(tsp.optimize())
