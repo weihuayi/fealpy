cb2c59a7fd2acedaf2a6ae76057d0d9477f3d6fe
pengmiaoying <530334303@qq.com>
2023-11-14 16:50:29 +0800

    update

--- fealpy/cfd/ns_mac_solver.py
+++ fealpy/cfd/ns_mac_solver.py
@@ -22,6 +22,21 @@ class NSMacSolver():
         result = diags([1, -1],[Nrow, -Nrow],(N,N), format='csr')
 
         return result/(2*dx)
+    
+    def grad_vx(self):
+        mesh = self.vmesh
+        dx = mesh.h[0]
+        Nrow = mesh.node.shape[1]
+        Ncol = mesh.node.shape[0]
+        N = Nrow*Ncol
+
+        result = diags([1, -1],[Nrow, -Nrow],(N,N), format='csr')
+        index = np.arange(0,N)
+        result[index[:Nrow],index[:Nrow]] = 2
+        result[index[:Nrow],index[:Nrow]+Nrow] = 2/3
+        result[index[-Nrow:],index[-Nrow:]] = -2
+        result[index[-Nrow:],index[-Nrow:]-Nrow] = -2/3
+        return result/(2*dx)
 
     def grad_uy(self):
         mesh = self.umesh
@@ -42,6 +57,15 @@ class NSMacSolver():
         result[index[:-1], index[:-1]+1] = 0
         return result/(2*dy)
     
+    def grad_vy(self):
+        mesh = self.vmesh
+        dy = mesh.h[1]
+        Nrow = mesh.node.shape[1]
+        Ncol = mesh.node.shape[0]
+        N = Nrow*Ncol
+        result = diags([1, -1],[1, -1],(N,N), format='lil')
+        return result/(2*dy)
+    
     def Tuv(self):
         mesh  = self.umesh
         Nrow = mesh.node.shape[1]
@@ -53,6 +77,30 @@ class NSMacSolver():
         result[index,index+index//Nrow+1] = 1
         result[index,index+index//Nrow-Nrow-1] = 1
         result[index,index+index//Nrow-Nrow] = 1
+        return result/4
+    
+    def Tvu(self):
+        mesh = self.vmesh
+        Nrow = mesh.node.shape[1]
+        Ncol = mesh.node.shape[0]
+        N = Nrow*Ncol
+        result = np.zeros((N,N))
+        arr = np.arange(0,N)
+        split_array = np.array_split(arr,Ncol)
+        lists = [sub_array.tolist() for sub_array in split_array]
+        num = len(lists)
+        for i in range(num):
+            i_array = np.ones_like(lists[i])
+            result[lists[i],lists[i]-i*i_array] = 1
+            result[lists[i],lists[i]-i*i_array-1] = 1
+        for i in range(num-1):
+            i_array = np.ones_like(lists[i])
+            result[lists[i],lists[i]-i*i_array+Ncol*i_array] = 1
+            result[lists[i],lists[i]-i*i_array-1+Ncol*i_array] = 1
+        index = lists[-1][:Nrow-1]
+        N_array = np.ones_like(index)
+        result[index,index] = 1
+        result[index,index+N_array] = 1
         return result/4
     
     def laplace_u(self):
@@ -73,6 +121,22 @@ class NSMacSolver():
 
         return result/(dx*dy)
     
+    def laplace_v(self):
+        mesh = self.vmesh
+        dx,dy = mesh.h
+        Nrow = mesh.node.shape[1]
+        Ncol = mesh.node.shape[0]
+        N = Nrow*Ncol
+        result = diags([-1, 1, 1, 1, 1],[0, 1, -1, Nrow, -Nrow],(N,N), format='lil')
+
+        index = np.arange(0,N)
+        result[index[:Nrow],index[:Nrow]] = -6
+        result[index[-Nrow:],index[-Nrow:]] = -6
+        result[index[:Nrow],index[:Nrow]+Nrow] = 4/3
+        result[index[-Nrow:],index[-Nrow:]-Nrow] = 4/3
+
+        return result/(dx*dy) 
+    
     def grand_uxp(self):
         mesh = self.pmesh
         dx = mesh.h[0]
@@ -84,9 +148,34 @@ class NSMacSolver():
         result1 = vstack([result, A], format='lil')   
         return result1/dx
     
+    def grand_vyp(self):
+        mesh = self.pmesh
+        dx = mesh.h[0]
+        Nrow = mesh.node.shape[1]
+        Ncol = mesh.node.shape[0]
+        N = Nrow*Ncol
+        N1 = Nrow*Ncol + Nrow
+        result = diags([0],[0],(N1,N), format='lil')
+        arr = np.arange(0,N1)
+        split_array = np.array_split(arr,Nrow)
+        lists = [sub_array.tolist() for sub_array in split_array]
+        num = len(lists)
+        for i in range(num-1):
+            i_array= np.ones_like(lists[i])
+            result[lists[i],lists[i]-i*i_array] = 1
+            result[lists[i],lists[i]-i*i_array-1] = -1
+        index = lists[-1][:Nrow]
+        num_array = np.ones_like(index)
+        result[index,index-(num-1)*num_array] = 1
+        result[index,index-(num-1)*num_array-1] = -1
+        return result/dx
+    
     def source_Fx(self, pde ,t):
         mesh = self.umesh
         nodes = mesh.entity('node')
         source = pde.source_F(nodes,t) 
         return source
+    
+    
+
 
