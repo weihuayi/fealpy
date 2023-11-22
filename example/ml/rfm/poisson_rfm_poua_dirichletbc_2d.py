c150ccb955d77927a4f55e812430bebb736b76c5
AlbertZyy <zhengyy0621@foxmail.com>
2023-11-22 17:22:58 +0800

    update example

--- example/ml/rfm/poisson_rfm_poua_dirichletbc_2d.py
+++ example/ml/rfm/poisson_rfm_poua_dirichletbc_2d.py
@@ -8,12 +8,12 @@ Exact solution:
   \\exp^{-0.5(x^2 + y^2)}
 """
 
-from time import time
-
 import torch
 from torch import Tensor, exp
 from scipy.sparse.linalg import spsolve
+from scipy.linalg import solve
 
+from fealpy import ml as fml
 from fealpy.ml.modules import PoUSpace, Cos, RandomFeatureSpace
 from fealpy.ml.operators import (
     Form, ScalerDiffusion, ScalerMass,
@@ -21,6 +21,7 @@ from fealpy.ml.operators import (
 )
 from fealpy.ml.sampler import ISampler, InterfaceSampler
 from fealpy.mesh import UniformMesh2d, TriangleMesh
+
 
 PI = torch.pi
 
@@ -47,7 +48,8 @@ H = 1.0/EXT
 Jn = 100
 N = 100
 
-start_time = time()
+tmr = fml.timer()
+tmr.send(None)
 
 def factory(i: int):
     sp = RandomFeatureSpace(2, Jn, Cos(), bound=(PI/2, PI))
@@ -56,6 +58,7 @@ def factory(i: int):
 # 按均匀网格分区 构建 RFM 模型
 mesh = UniformMesh2d((0, EXT, 0, EXT), (H, H), origin=(0, 0))
 space = PoUSpace.from_uniform_mesh(factory, mesh, part_loc='node', print_status=True)
+tmr.send("model")
 
 # 获得采样点
 col_in = ISampler([0, 1, 0, 1], mode='linspace').run(N, N)
@@ -68,6 +71,7 @@ col_bd = torch.cat([col_left, col_right, col_btm, col_top], dim=0)
 _css = InterfaceSampler(mesh, part_loc='node', mode='linspace')
 col_sub = _css.run(20, entity_type=True)
 sub2part, sub2normal = _css.sub_to_part(return_normal=True)
+tmr.send("collocate")
 
 # 组装矩阵 求解
 form = Form(space)
@@ -76,16 +80,18 @@ form.add(col_bd, ScalerMass(), boundary)
 form.add(col_sub, Continuous0(sub2part))
 form.add(col_sub, Continuous1(sub2part, sub2normal))
 
-A, b = form.assembly(rescale=100.)
-um = space.function(torch.from_numpy(spsolve(A, b)))
+A, b = form.assembly(rescale=100., return_sparse=False)
+fml.ridge(A, 1e-6)
+tmr.send("assemble")
+um = space.function(torch.from_numpy(solve(A, b)))
+tmr.send("solve")
 
-end_time = time()
 
 mesh_err = TriangleMesh.from_box([0., 1., 0., 1.], nx=16, ny=16)
 error = um.estimate_error_tensor(exact_solution, mesh=mesh_err)
+tmr.send("stop")
 print(f"L-2 error: {error.item()}")
-print(f"Time: {end_time - start_time}")
-
+print(f"Maxdof: {um.um.abs().max()}")
 
 # Visualize
 
