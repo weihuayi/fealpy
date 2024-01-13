94b8825bcdb178a9f6b7297f2679b4a6345b65e8
chenchunyu <cbtxs@qq.com>
2024-01-13 21:46:25 +0800

    update

--- fealpy/mesh/tetrahedron_mesh.py
+++ fealpy/mesh/tetrahedron_mesh.py
@@ -34,14 +34,17 @@ class TetrahedronMeshDataStructure(Mesh3dDataStructure):
             face2edgeSign[:, i] = (face[:, n[i]] == edge[face2edge[:, i], 0])
         return face2edgeSign
 
-    def cell_to_face_permutation(self):
+    def cell_to_face_permutation(self, locFace = None):
+        if locFace is None:
+            locFace = self.localFace
+
         c2f  = self.cell_to_face()
         cell = self.cell
         face = self.face
         face_g_idx = np.argsort(face)
 
         c2f_glo = face[c2f.reshape(-1)]
-        c2f_loc = cell[:, self.localFace].reshape(-1, 3)
+        c2f_loc = cell[:, locFace].reshape(-1, 3)
 
         c2f_glo = np.argsort(c2f_glo, axis=1)
         c2f_glo = np.argsort(c2f_glo, axis=1)
