2c47b78b43e15f2b65147f1c2a9950d9c4bcd44d
Huayi Wei <weihuayi@xtu.edu.cn>
2024-01-25 15:21:46 +0800

    update

---
+++ fealpy/solver/cupy_solver.py
@@ -0,0 +1,9 @@
+import cupy as cp
+import cupyx.scipy.sparse.linalg as cpx
+from cupyx.scipy.sparse.linalg import LinearOperator as CuPyLinearOperator
+
+
+class CupySolver():
+
+    def __init__(self):
+        pass
