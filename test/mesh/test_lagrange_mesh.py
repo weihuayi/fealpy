2e6c939e7c23d0d1303a0c829d60e45c142f8d30
BellaLq <2655493031@qq.com>
2024-03-22 16:01:53 +0800

    testlagrangemesh

--- test/mesh/test_lagrange_mesh.py
+++ test/mesh/test_lagrange_mesh.py
@@ -4,30 +4,49 @@ from fealpy.mesh.lagrange_triangle_mesh import LagrangeTriangleMesh
 from fealpy.geometry import SphereSurface
 from fealpy.mesh.lagrange_mesh import LagrangeMesh
 import pytest
+
+p = 1
+
+surface = SphereSurface() # 以原点为球心，1为半径的球
  
+node, cell = surface.init_mesh(meshtype='tri', returnnc=True)
+
+mesh = LagrangeTriangleMesh(node, cell, surface=surface, p=p)
+
+node = mesh.entity('node')
+cell = mesh.entity('cell')
+
+fname = f"test.vtu"
+mesh.to_vtk(fname=fname)
+ 
+multiIndex = mesh.multi_index_matrix(p=p, etype=2)
+bc = multiIndex / p # 插值点的重心坐标
+print('bc:', bc)
+
+NN = mesh.number_of_nodes()
+NC = mesh.number_of_cells()
+
+
 def test_jacobi_matrix():
 
-    p = 2
+    J = mesh.jacobi_matrix(bc)
+    #print('J', J)
+    print('J.shape:', J.shape)
 
-    surface = SphereSurface() # 以原点为球心，1为半径的球
-     
-    node, cell = surface.init_mesh(meshtype='tri', returnnc=True)
-
-    mesh = LagrangeTriangleMesh(node, cell, surface=surface, p=p)
-
-    node = mesh.entity('node')
-    cell = mesh.entity('cell')
-
-    fname = f"test.vtu"
-    mesh.to_vtk(fname=fname)
-
-    multiIndex = mesh.multi_index_matrix(p=p, etype=2)
-    bc = multiIndex / p # 插值点的重心坐标
-    print('bc.shape:', bc.shape)
-
-    J, gphi = mesh.jacobi_matrix(bc)
-    #print('J:', J)
-
+def test_unit_normal():
     
+    n = mesh.unit_normal(bc)
+    #print('n:', n)
+    print('n.shape:', n.shape)
+
+def test_first_fundamental_form():
+
+    G = mesh.first_fundamental_form(bc)
+    #print('G:', G)
+    print('G.shape:', G.shape)
+
+
 if __name__ == "__main__":
-   test_jacobi_matrix() 
+   test_jacobi_matrix()
+   test_unit_normal()
+   test_first_fundamental_form()
