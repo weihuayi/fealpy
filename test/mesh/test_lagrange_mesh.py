b7be0e5e98b757ff3d35a99438f78797f11e9a5d
BellaLq <2655493031@qq.com>
2024-03-29 21:03:36 +0800

    update test_lagrange

--- test/mesh/test_lagrange_mesh.py
+++ test/mesh/test_lagrange_mesh.py
@@ -4,8 +4,9 @@ from fealpy.mesh.lagrange_triangle_mesh import LagrangeTriangleMesh
 from fealpy.geometry import SphereSurface
 from fealpy.mesh.lagrange_mesh import LagrangeMesh
 import pytest
+import ipdb
 
-p = 1
+p = 3
 
 surface = SphereSurface() # 以原点为球心，1为半径的球
  
@@ -18,14 +19,14 @@ cell = mesh.entity('cell')
 
 fname = f"test.vtu"
 mesh.to_vtk(fname=fname)
- 
-multiIndex = mesh.multi_index_matrix(p=p, etype=2)
+
+#ipdb.set_trace()
+multiIndex = mesh.multi_index_matrix(p, etype=2)
 bc = multiIndex / p # 插值点的重心坐标
 print('bc:', bc)
 
 NN = mesh.number_of_nodes()
 NC = mesh.number_of_cells()
-
 
 def test_jacobi_matrix():
 
@@ -34,7 +35,6 @@ def test_jacobi_matrix():
     print('J.shape:', J.shape)
 
 def test_unit_normal():
-    
     n = mesh.unit_normal(bc)
     #print('n:', n)
     print('n.shape:', n.shape)
