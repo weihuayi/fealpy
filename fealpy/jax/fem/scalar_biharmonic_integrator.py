bfed41f6b625d55d9fcfc91e2880f9f4f325ad73
tiantian0347 <tiantian0347@126.com>
2024-04-06 14:44:05 +0800

    update

--- fealpy/jax/fem/scalar_biharmonic_integrator.py
+++ fealpy/jax/fem/scalar_biharmonic_integrator.py
@@ -24,12 +24,15 @@ class ScalarBiharmonicIntegrator:
         bcs, ws = qf.get_quadrature_points_and_weights()
 
         # 计算与单元无关的部分
-        R = space.grad_basis(bcs, variable='u') # (NQ, ldof, TD+1)
-        M = jnp.einsum('q, qik, qjl->ijkl', ws, R, R)
-
+        R = space.basis(bcs, variable='u') # (NQ, ldof)
+        print(R.shape)
+        R0 = jax.hessian(R)
+        M = jnp.einsum('q, qik, qjl->ijkl', ws, R0, R0)
+        
         # 计算与单元相关的部分
         cm = mesh.entity_measure()
         glambda = mesh.grad_lambda()
+
 
         # 计算最终的刚度矩阵
         A = jnp.einsum('c, ckm, clm, ijkl->cij', cm, glambda, glambda, M)
