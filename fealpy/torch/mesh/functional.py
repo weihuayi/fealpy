34910ef2a1fc70cc6f793dae335383d581ac94a3
Flantori <zhengyy0621@foxmail.com>
2024-05-01 21:09:02 +0800

    update torch module

--- fealpy/torch/mesh/functional.py
+++ fealpy/torch/mesh/functional.py
@@ -5,7 +5,7 @@ from functools import reduce
 
 import numpy as np
 import torch
-from torch import Tensor
+from torch import Tensor, norm, det, cross
 
 _dtype = torch.dtype
 
@@ -67,23 +67,79 @@ def multi_index_matrix(p: int, etype: int, *, dtype=None, device=None) -> Tensor
 
 
 def shape_function(bc: Tensor, p: int=1, mi: Optional[Tensor]=None, *,
-                   dtype=None, device=None):
+                   dtype=None, device=None) -> Tensor:
     r"""Shape function"""
     if p <= 0:
-        raise ValueError("p must be positive integer.")
+        raise ValueError("p must be a positive integer.")
     if p == 1:
         return bc
     TD = bc.shape[-1] - 1
     itype = torch.int
+    dtype = dtype or bc.dtype
     shape = bc.shape[:-1] + (p+1, TD+1)
     mi = mi or multi_index_matrix(p, etype=TD, dtype=itype, device=device)
     c = torch.arange(1, p+1, dtype=itype, device=device)
     P = 1.0 / torch.cumprod(c, dim=0)
     t = torch.arange(0, p, dtype=itype, device=device)
     A = torch.ones(shape, dtype=dtype, device=device)
-    A[..., 1:, :] = p*bc.unsqueeze(-2) - t.reshape(-1, 1)
+    torch.sub(p*bc.unsqueeze(-2), t.reshape(-1, 1), out=A[..., 1:, :])
     A = torch.cumprod(A, dim=-2).clone()
     A[..., 1:, :].mul_(P.reshape(-1, 1))
     idx = torch.arange(TD + 1, dtype=itype, device=device)
     phi = torch.prod(A[..., mi, idx], dim=-1)
     return phi
+
+
+### Leangth of edges
+def edge_length(points: Tensor, out=None) -> Tensor:
+    r"""Edge length.
+
+    Args:
+        points: Tensor(..., 2, GD).
+
+    Returns:
+        Tensor(...,).
+    """
+    return norm(points[..., 0, :] - points[..., 1, :], dim=-1, out=out)
+
+
+### Simplex (2D Triangle, 3D Tetrahegron, 4D...) measure
+def simplex_measure(points: Tensor):
+    r"""
+    Args:
+        edges: Tensor(..., NVC, GD).
+        out: Tensor(...,), optional.
+
+    Returns:
+        Tensor(...,).
+    """
+    if points.size(-2) != points.size(-1) + 1:
+        raise RuntimeError("The geometric dimension of points must be NVC-1"
+                           "to form a simplex.")
+    edges = points[..., 1:, :] - points[..., :-1, :]
+    return det(edges).mul_(0.5)
+
+
+### Triangle
+def tri_area_3d(points: Tensor, out=None):
+    return cross(points[..., 1, :] - points[..., 0, :],
+                 points[..., 2, :] - points[..., 0, :], dim=-1, out=out)
+
+
+def tri_grad_lambda_2d(points: Tensor):
+    r"""
+    Args:
+        points: Tensor(..., 3, 2).
+
+    Returns:
+        Tensor(..., 3, 2).
+    """
+    e0 = points[..., 2, :] - points[..., 1, :]
+    e1 = points[..., 0, :] - points[..., 2, :]
+    e2 = points[..., 1, :] - points[..., 0, :]
+    nv = det(torch.stack([e0, e1], dim=-2))
+    return torch.tensor([
+        [-e0[1], e0[0]],
+        [-e1[1], e1[0]],
+        [-e2[1], e2[0]]
+    ], dtype=points.dtype, device=points.device).div(nv)
--- fealpy/torch/mesh/triangle_mesh.py
+++ fealpy/torch/mesh/triangle_mesh.py
@@ -1,17 +1,22 @@
 
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 import torch
 from torch import Tensor
+
+from fealpy.torch.mesh.mesh_base import _S
 
 from .. import logger
 from . import functional as F
 from . import mesh_kernel as K
 from .mesh_base import HomoMeshDataStructure, HomoMesh
 
+Index = Union[Tensor, int, slice]
 _dtype = torch.dtype
 _device = torch.device
+
+_S = slice(None)
 
 
 class TriangleMeshDataStructure(HomoMeshDataStructure):
@@ -28,11 +33,10 @@ class TriangleMeshDataStructure(HomoMeshDataStructure):
             (1, 2, 0),
             (2, 0, 1)], **kwargs)
 
-        self.construct()
-
     def total_face(self):
         return self.cell[..., self.localFace].reshape(-1, 2)
 
+    # TODO: this is not correct. So, face2cell is unavailable.
     def construct(self) -> None:
         kwargs = {'dtype': self.itype, 'device': self.device}
         NC = self.cell.shape[0]
@@ -63,8 +67,69 @@ class TriangleMeshDataStructure(HomoMeshDataStructure):
 
 
 class TriangleMesh(HomoMesh):
+    def __init__(self, node: Tensor, cell: Tensor) -> None:
+        self.node = node
+        self.ds = TriangleMeshDataStructure(node.shape[0], cell)
 
-    def shape_function(self, bc: Tensor, p: int=1, mi: Optional[Tensor]=None):
+        GD = node.size(-1)
+
+        if GD == 2:
+            self._cell_area = F.simplex_measure
+            self._grad_lambda = F.tri_grad_lambda_2d
+        elif GD == 3:
+            self._cell_area = F.tri_area_3d
+            self._grad_lambda = F.tri_grad_lambda_3d
+        else:
+            logger.warn(f"{GD}D triangle mesh is not well supported: "
+                        "cell_area and grad_lambda are not available. "
+                        "Any operation involving them will fail.")
+
+    def cell_to_ipoint(self, p: int, index: Index=None) -> Tensor:
+        cell = self.ds.cell
+        if p == 1:
+            return cell[index]
+
+        mi = self.multi_index_matrix(p, 2)
+        idx0, = torch.nonzero(mi[:, 0] == 0)
+        idx1, = torch.nonzero(mi[:, 1] == 0)
+        idx2, = torch.nonzero(mi[:, 2] == 0)
+
+        face2cell = self.ds.face_to_cell()
+        NN = self.number_of_nodes()
+        NE = self.number_of_edges()
+        NC = self.number_of_cells()
+
+        e2p = self.edge_to_ipoint(p)
+        ldof = self.number_of_local_ipoint()
+
+    def grad_lambda(self, index: Index=_S):
+        return self._grad_lambda(self.node[self.ds.cell[index]])
+
+    def shape_function(self, bc: Tensor, p: int=1, *, index: Index=_S,
+                       variable: str='u', mi: Optional[Tensor]=None) -> Tensor:
         TD = bc.shape[-1] - 1
         mi = mi or F.multi_index_matrix(p, TD, dtype=self.ds.itype, device=self.device)
-        return K.simplex_shape_function(bc, p, mi)
+        phi = K.simplex_shape_function(bc, p, mi)
+        if variable == 'u':
+            return phi
+        elif variable == 'x':
+            return phi.unsqueeze_(0)
+        else:
+            raise ValueError("Variable type is expected to be 'u' or 'x', "
+                             f"but got '{variable}'.")
+
+    def grad_shape_function(self, bc: Tensor, p: int=1, *, index: Index=_S,
+                            variable: str='u', mi: Optional[Tensor]=None) -> Tensor:
+        TD = bc.shape[-1] - 1
+        mi = mi or F.multi_index_matrix(p, TD, dtype=self.ds.itype, device=self.device)
+        R = K.simplex_grad_shape_function(bc, p, mi)
+        if variable == 'u':
+            return R
+        elif variable == 'x':
+            Dlambda = self.grad_lambda(index=index)
+            gphi = torch.einsum('...ij, kjm -> k...im', Dlambda, R)
+            # NOTE: the subscript 'k': cell, 'i': dof, 'j': bc, 'm': dimension, '...': batch
+            return gphi
+        else:
+            raise ValueError("Variable type is expected to be 'u' or 'x', "
+                             f"but got '{variable}'.")
