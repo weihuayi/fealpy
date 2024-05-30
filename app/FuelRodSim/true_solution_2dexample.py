40f99ab5ea2913d60016b647d47722d1aa6e0005
Liujiawangmath <15674760037@163.com>
2024-05-30 18:35:42 +0800

    清理重复文件，抛物方程模型自动求解，测试算例分为6个直接运行的py文件

--- app/FuelRodSim/true_solution_2dexample.py
+++ app/FuelRodSim/true_solution_2dexample.py
@@ -2,7 +2,7 @@
 from fealpy.mesh import TriangleMesh
 from app.FuelRodSim.HeatEquationData import Parabolic2dData
 from app.FuelRodSim.heat_equation_solver import HeatEquationSolver
-pde=Parabolic2dData()
+pde=Parabolic2dData('exp(-2*pi**2*t)*sin(pi*x)*sin(pi*y)','x','y','t')
 nx = 20
 ny = 20
 mesh = TriangleMesh.from_box([0, 1, 0, 1], nx,ny)
@@ -10,7 +10,10 @@ node = mesh.node
 print(node.shape)
 isBdNode = mesh.ds.boundary_node_flag()
 p0=pde.init_solution(node) #准备一个初值
-Box2dslover = HeatEquationSolver(mesh,pde,160,isBdNode,p0=p0,alpha_caldding=1,layered=False,output='./rusult_true_solution_2d')
-Box2dslover.solve()
-Box2dslover.plot_exact_solution() # 绘制真解
-Box2dslover.plot_error()
+Box2dsolver = HeatEquationSolver(mesh,pde,160,isBdNode,p0=p0,alpha_caldding=1,layered=False,output='./rusult_box2dtesttest_to')
+Box2dsolver.solve()
+Box2dsolver.plot_exact_solution() # 绘制真解
+Box2dsolver.plot_error()
+Box2dsolver.plot_exact_solution_heatmap() # 绘制真解
+Box2dsolver.plot_error_heatmap()
+Box2dsolver.plot_error_over_time() 
