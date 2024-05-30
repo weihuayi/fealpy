3e8175a07b84fdb33161216bb852de1960588fb1
chenchunyu <cbtxs@qq.com>
2024-05-30 21:50:12 +0800

    update

--- app/svads3d/harmonic_map.py
+++ app/svads3d/harmonic_map.py
@@ -4,9 +4,9 @@ from scipy.sparse.linalg import spsolve
 from dataclasses import dataclass
 
 from fealpy.mesh import TriangleMesh
-from fealpy.functionspace import LagrangeFiniteElementSpace
+from fealpy.functionspace import LagrangeFESpace
 
-from fealpy.fem import BilinearForm, LinearForm
+from fealpy.fem import BilinearForm, LinearForm, DirichletBC
 from fealpy.fem import VectorDiffusionIntegrator
 
 @dataclass
@@ -21,7 +21,6 @@ class HarmonicMapData:
     mesh: TriangleMesh
     didx: np.ndarray
     dval: np.ndarray
-    
 
 
 def sphere_harmonic_map(data : HarmonicMapData):
@@ -37,26 +36,27 @@ def sphere_harmonic_map(data : HarmonicMapData):
     GD = dval.shape[1]
     gdof = NN*GD 
 
-    space = LagrangeFiniteElementSpace(mesh, p=1)
+    space = LagrangeFESpace(mesh, p=1)
     space.doforder = 'vdims'
 
     bform = BilinearForm((space, )*GD)
     bform.add_domain_integrator(VectorDiffusionIntegrator())
     S = bform.assembly() # 刚度矩阵
+    SS = S.copy()
 
     # 1. 计算初值
     ## 1.1 狄利克雷边界条件处理
     idof = np.ones(gdof, dtype=np.bool_) # 内部自由度
     idof[didx[:, None] * GD + np.arange(GD)] = False
+    f = np.zeros(gdof, dtype=np.float64)
 
     N = gdof-len(didx)*GD # 内部自由度个数
     I = np.arange(N)
     d = np.ones(N, dtype=np.float64)
     T = csr_matrix((d, (I, np.where(idof)[0])), shape=(N, gdof))
 
-    f = np.zeros(gdof, dtype=np.float64)
     f[~idof] = dval.flatten()
-    f = (T@S@f)
+    f = -(T@S@f)
     S = T@S@T.T
 
     ## 1.2 解方程
@@ -67,10 +67,20 @@ def sphere_harmonic_map(data : HarmonicMapData):
     uh = vh/np.linalg.norm(vh, axis=1, keepdims=True)
     uh = uh.reshape(-1)
 
+    def extend_fun(u):
+        ue = np.zeros(gdof, dtype=np.float64)
+        ue[~idof] = dval.flat
+        ue[idof] = u
+        return ue
+    def compute_energy(u):
+        ue = extend_fun(u) 
+        return (SS@ue).dot(ue)
+    E = compute_energy(uh)
+    print("init energy: ", E)
+
     # 2. 迭代求解
     I = np.tile(np.arange(N//GD), (GD, 1)).T.flatten()
     J = np.arange(N)
-    E = (S@uh).dot(uh)
     while True:
         ## 2.1 计算 C 并组装矩阵
         C = csr_matrix((uh, (I, J)), shape=(N//GD, N))
@@ -78,7 +88,7 @@ def sphere_harmonic_map(data : HarmonicMapData):
 
         ## 2.2 计算右端 b
         b = np.zeros(N+N//GD, dtype=np.float64)
-        b[:N] = -S@uh
+        b[:N] = -T@SS@extend_fun(uh)
 
         ## 2.3 解方程
         x = spsolve(A, b)
@@ -87,15 +97,12 @@ def sphere_harmonic_map(data : HarmonicMapData):
         ## 2.4 归一化
         vh = uh.reshape(-1, GD) + wh.reshape(-1, GD)
         uh = (vh/np.linalg.norm(vh, axis=1, keepdims=True)).reshape(-1)
-        E0 = (S@uh).dot(uh)
-        if np.linalg.norm(E - E0) < 1e-8:
+        E0 = compute_energy(uh)
+        print("energy: ", E0)
+        if np.linalg.norm(E - E0) < 1e-2:
             break
         E = E0
-
-    fun = np.zeros((NN*GD))
-    fun[idof] = uh
-    fun[~idof] = dval.flat
-    return fun.reshape(-1, GD)
+    return extend_fun(uh).reshape(-1, GD)
 
 
 
