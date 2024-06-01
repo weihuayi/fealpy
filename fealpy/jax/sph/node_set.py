3214fedfeefb653f9c92e0d60431b9535387985b
pengmiaoying <530334303@qq.com>
2024-06-01 17:59:51 +0800

    update

--- fealpy/jax/sph/node_set.py
+++ fealpy/jax/sph/node_set.py
@@ -1,13 +1,22 @@
 
 import jax.numpy as jnp
-
-
+from scipy.spatial import cKDTree 
+import jax.random
 class NodeSet():
 
-    def __init__(self, NN, node=None):
+    def __init__(self, NN, node=None, h=None):
         self.NN = NN
         self.nodedata['node'] = node
+        self.h = h
 
 
     def number_of_node(self):
         return self.NN 
+       
+    def interpolate(self, u, node, dim=1, kernel=1):
+        node = self.nodedata['node']
+    
+    def find_neighbors(self):
+        tree = cKDTree(self.node)
+        neighbors = tree.query_ball_tree(tree,self.h)
+        return neighbors
