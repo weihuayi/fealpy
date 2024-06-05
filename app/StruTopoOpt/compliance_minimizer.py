8971cfd9795979225e988d225796f1d59764b545
brighthe98@gmail.com <brighthe98@gmail.com>
2024-06-05 20:12:51 +0800

    Successfully calculated compliance and tested the correctness of the projection filter

--- app/StruTopoOpt/compliance_minimizer.py
+++ app/StruTopoOpt/compliance_minimizer.py
@@ -1,8 +1,16 @@
-import numpy as np
 import jax
 import jax.numpy as jnp
 from jax import jit, value_and_grad
 from utilfuncs import Mesher
+ 
+def test_projectionFilter(rho, beta, c0, isOn):
+    if isOn:
+        v1 = jnp.tanh(c0 * beta)
+        nm = v1 + jnp.tanh(beta * (rho - c0))
+        dnm = v1 + jnp.tanh(beta * (1. - c0))
+        return nm / dnm
+    else:
+        return rho
 
 class ComplianceMinimizer:
     def __init__(self, 
@@ -15,7 +23,7 @@ class ComplianceMinimizer:
         # 默认网格参数
         if mesh is None:
             nelx, nely = 60, 30
-            elemSize = np.array([1., 1.])
+            elemSize = jnp.array([1., 1.])
             mesh = {'nelx': nelx, 'nely': nely, 'elemSize': elemSize,
                     'ndof': 2 * (nelx + 1) * (nely + 1), 'numElems': nelx * nely}
         else:
@@ -31,31 +39,33 @@ class ComplianceMinimizer:
         
         # 默认投影参数
         if projection is None:
-            projection = {'isOn': False, 'beta': 4, 'c0': 0.5}
+            projection = {'isOn': True, 'beta': 4, 'c0': 0.5}
         
         # 默认边界条件和载荷
         if bc is None:
             example = 1
             if example == 1:
-                force = np.zeros((mesh['ndof'], 1))
-                dofs = np.arange(mesh['ndof'])
+                force = jnp.zeros((mesh['ndof'], 1))
+                dofs = jnp.arange(mesh['ndof'])
                 fixed = dofs[0:2 * (nely + 1):1]
-                free = jnp.setdiff1d(np.arange(mesh['ndof']), fixed)
-                force[2 * (nelx + 1) * (nely + 1) - 2 * nely + 1, 0] = -1
+                free = jnp.setdiff1d(jnp.arange(mesh['ndof']), fixed)
+                # JAX 数组是不可变的，不能直接进行赋值操作。需要使用 JAX 提供的 .at[].set() 方法来进行修改
+                force = force.at[2 * (nelx + 1) * (nely + 1) - 2 * nely + 1, 0].set(-1)
                 symXAxis = False
                 symYAxis = False
             elif example == 2:
-                force = np.zeros((mesh['ndof'], 1))
-                dofs = np.arange(mesh['ndof'])
+                force = jnp.zeros((mesh['ndof'], 1))
+                dofs = jnp.arange(mesh['ndof'])
                 fixed = dofs[0:2 * (nely + 1):1]
-                free = jnp.setdiff1d(np.arange(mesh['ndof']), fixed)
-                force[2 * (nelx + 1) * (nely + 1) - (nely + 1), 0] = -1
+                free = jnp.setdiff1d(jnp.arange(mesh['ndof']), fixed)
+                # JAX 数组是不可变的，不能直接进行赋值操作。需要使用 JAX 提供的 .at[].set() 方法来进行修改
+                force = force.at[2 * (nelx + 1) * (nely + 1) - (nely + 1), 0].set(-1)
                 symXAxis = True
                 symYAxis = False
             else:
-                force = np.zeros((mesh['ndof'], 1))
-                fixed = np.array([])
-                free = np.array([])
+                force = jnp.zeros((mesh['ndof'], 1))
+                fixed = jnp.array([])
+                free = jnp.array([])
                 symXAxis = False
                 symYAxis = False
 
@@ -68,6 +78,7 @@ class ComplianceMinimizer:
         
         # 初始化 Mesher 类并获取初始刚度矩阵
         M = Mesher()
+        self.edofMat, self.idx = M.getMeshStructure(mesh)
         self.K0 = M.getK0(self.material)
 
         # 设置全局体积约束
@@ -82,9 +93,54 @@ class ComplianceMinimizer:
         self.projection = projection
 
     def computeCompliance(self, rho):
-        # 将具体实现填充在这里
-        pass
+
+        @jit
+        # 投影滤波器
+        def projectionFilter(rho):
+            if(self.projection['isOn']):
+                v1 = jnp.tanh(self.projection['c0'] * self.projection['beta'])
+                nm = v1 + jnp.tanh(self.projection['beta'] * (rho - self.projection['c0']))
+                dnm = v1 + jnp.tanh(self.projection['beta'] * (1. - self.projection['c0']))
+                return nm/dnm
+            else:
+                return rho
+
+        @jit
+        # SIMP 材料插值模型
+        def materialModel(rho):
+            E = self.material['Emin'] + \
+                (self.material['Emax'] - self.material['Emin']) * \
+                (rho + 0.01) ** self.material['penal']
+            return E
+        
+        @jit
+        # 组装全局刚度矩阵
+        def assembleK(E):
+            K_asm = jnp.zeros((self.mesh['ndof'], self.mesh['ndof']))
+            K_elem = (self.K0.flatten()[jnp.newaxis]).T
+
+            K_elem = (K_elem*E).T.flatten()
+            K_asm = K_asm.at[(self.idx)].add(K_elem)
+            return K_asm
+        
+        @jit
+        # 直接法求解线性方程组
+        def solveKuf(K): 
+            u_free = jax.scipy.linalg.solve(K[self.bc['free'],:][:,self.bc['free']], \
+                                        self.bc['force'][self.bc['free']], check_finite=False)
+            u = jnp.zeros((self.mesh['ndof']))
+            u = u.at[self.bc['free']].set(u_free.reshape(-1))
+            return u
+        
+        rho = projectionFilter(rho)
+        E = materialModel(rho)
+        K = assembleK(E)
+        u = solveKuf(K)
+        J = jnp.dot(self.bc['force'].T, u)[0]
+
+        return J
 
     def computeConstraints(self, rho, epoch):
         # 将具体实现填充在这里
         pass
+
