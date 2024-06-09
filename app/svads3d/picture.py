27caa42817efa318a275a9b6025727eaf4bcc4dc
concha <ck1193838646@163.com>
2024-06-09 22:00:47 +0800

    填充超创项目程序内容。

--- app/svads3d/picture.py
+++ app/svads3d/picture.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
+import cv2
 from typing import Union
-from .camera import Camera
 
 
 class Picture():
@@ -16,50 +16,104 @@ class Picture():
             mesh : 图像上生成的网格。
         """
 
-    data: dict = None
-    feature_point: Union[list[np.ndarray], np.ndarray] = None
-    feature_line: Union[list[np.ndarray], np.ndarray] = None
-    camera: Camera = None
+    fname = None
+    mark_board = None
+    feature_point: list = None
+    feature_line: list = []
+    camera = None
     mesh = None
 
-    def __init__(self, data: dict):
+    def __init__(self, data_path, fname, mark_board):
         """
         初始化图片信息，处理特征点等特征信息。
         1. 读取图片
         2. 处理特征点
         @param data: 相关数据，包括图片信息。
         """
-        self.data = data
-        self.feature_point = None
-        self.feature_line = None
-        pass
+        self.data_path = data_path
+        self.fname = fname
+        # 读取图像
+        image = cv2.imread(self.data_path + self.fname, cv2.IMREAD_GRAYSCALE)
+        if image is None:
+            raise FileNotFoundError
+        else:
+            self.image = image
+        self.mark_board = np.array(mark_board).reshape((2, -1, 2))
+        self.feature_point = mark_board
+        self.center, self.radius = self.get_center_and_radius()
 
-    def add_feature_point(self, feature_point: Union[list[np.ndarray], np.ndarray]):
+
+    def add_feature_point(self, feature_point: Union[list[np.ndarray], np.ndarray, list]):
         """
         手动添加特征点。
         @param feature_point: 特征点。
         @return:
         """
-        pass
+        if isinstance(feature_point, list):
+            self.feature_point.extend(feature_point)
+        else:
+            self.feature_point.append(feature_point)
 
-    def meshing(self, mesh_type: MeshingType):
+    def meshing(self, mesh_type):
         """
         根据网格化方案，已有特征点[特征线]生成图像网格。
         @param mesh_type: 图像对应的相机对象。
         @return:
         """
+        raise NotImplemented
         self.mesh = None
-        pass
 
     def to_camera(self, *args):
         """
         将图像上的点或网格映射到自己对应的相机对象。
-        @param args: 需要被映射的点或网格。
-        @return:
+        @param args[0]: 需要被映射的点或网格对象。
+        @param args[1]: 映射方式。
+        @return: 映射后的相机坐标系下的点坐标。
         """
         assert self.camera is not None, "相机未初始化！"
-        if type(args[0]) in [list[np.ndarray], np.ndarray]:
-            pass
-        else:
-            pass
+        if type(args[0]) in [list[np.ndarray], np.ndarray, list]:
+            point = np.array(args[0])
+            NN = len(point)
 
+            fx = self.camera.K[0, 0]
+            fy = self.camera.K[1, 1]
+            u0 = self.camera.K[0, 2]
+            v0 = self.camera.K[1, 2]
+            node = np.zeros((NN, 3), dtype=np.float64)
+
+            node[:, 0] = point[:, 0] - u0
+            node[:, 1] = point[:, 1] - v0
+
+            phi = np.arctan2(fx * node[:, 1], (fy * node[:, 0]))
+            phi[phi < 0] = phi[phi < 0] + np.pi
+
+            idx = np.abs(fx * np.cos(phi)) > 1e-10
+            rho = np.zeros_like(phi)
+            rho[idx] = node[idx, 0] / (fx * np.cos(phi[idx]))
+            rho[~idx] = node[~idx, 1] / (fy * np.sin(phi[~idx]))
+
+            if args[1] == 'L':
+                theta = rho
+
+            node[:, 0] = np.sin(theta) * np.cos(phi)
+            node[:, 1] = np.sin(theta) * np.sin(phi)
+            node[:, 2] = np.cos(theta)
+            return node
+        else:
+            raise NotImplemented
+
+    def get_center_and_radius(self):
+        image = self.image
+        # 对图像进行阈值处理，保留非黑色区域
+        _, thresholded = cv2.threshold(image, 70, 255, cv2.THRESH_BINARY)
+
+        # 找到非黑色区域的轮廓
+        contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # 寻找最大轮廓
+        max_contour = max(contours, key=cv2.contourArea)
+
+        # 使用最小外接圆找到中心和半径
+        center, radius = cv2.minEnclosingCircle(max_contour)
+
+        return center, radius
