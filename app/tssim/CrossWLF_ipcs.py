c9ede0b8a66098e379b621f59254c22014ef8055
wpx <1143615697@qq.com>
2024-06-25 14:38:23 +0800

    update

--- app/tssim/CrossWLF_ipcs.py
+++ app/tssim/CrossWLF_ipcs.py
@@ -118,10 +118,10 @@ for i in range(1):
     A2 = uD0@A2@uD0 + uD1
     b2[is_u_bdof] = uxx.flatten()[is_u_bdof]
     u1[:] = spsolve(A2,b2).reshape((2,-1))
-        
+    
     #tempture
-    A3 = NSSolver.temperature_A(u1, C, rho, lam, pde.Pe) 
-    b3 = NSSolver.temperature_b(T0, u1, p1, eta, Br, pde.Pe, rho, C) 
+    #A3 = NSSolver.temperature_A(u1, C, rho, lam, pde.Pe) 
+    #b3 = NSSolver.temperature_b(T0, u1, p1, eta, Br, pde.Pe, rho, C) 
 
 fname = 'test' + 'test_.vtu'
 mesh.nodedata['u0'] = u0.transpose(1,0)
