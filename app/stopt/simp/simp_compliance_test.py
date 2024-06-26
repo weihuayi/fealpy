4477fd3c297bda933e97a31bd904258550ecfade
brighthe98@gmail.com <brighthe98@gmail.com>
2024-06-26 19:49:28 +0800

    update

--- app/stopt/simp/simp_compliance_test.py
+++ app/stopt/simp/simp_compliance_test.py
@@ -5,7 +5,8 @@ from fealpy.functionspace import LagrangeFESpace as Space
 from utilfuncs import compute_filter
 
 class TopSimp:
-    def __init__(self, mesh=None, space=None, bc=None, material=None, filter=None):
+    def __init__(self, mesh=None, space=None, bc=None, material=None, \
+                filter=None, global_volume_constraints=None):
         '''
         Initialize the topology optimization problem.
 
@@ -18,11 +19,18 @@ class TopSimp:
 
         # Default mesh parameters
         if mesh is None:
-            nelx, nely = 6, 4
-            domain = [0, 6, 0, 4]
-            hx = (domain[1] - domain[0]) / nelx
-            hy = (domain[3] - domain[2]) / nely
-            mesh = UniformMesh2d(extent=(0, nelx, 0, nely), h=(hx, hy), origin=(domain[0], domain[2]))
+            nx, ny = 3, 2
+            domain = [0, 3, 0, 2]
+            hx = (domain[1] - domain[0]) / nx
+            hy = (domain[3] - domain[2]) / ny
+            mesh = UniformMesh2d(extent=(0, nx, 0, ny), h=(hx, hy), origin=(domain[0], domain[2]))
+            import matplotlib.pyplot as plt
+            fig = plt.figure()
+            axes = fig.gca()
+            mesh.add_plot(axes)
+            mesh.find_node(axes, showindex=True)
+            mesh.find_cell(axes, showindex=True)
+            plt.show()
 
         # Default space parameters
         if space is None:
@@ -38,8 +46,10 @@ class TopSimp:
                 gdof = vspace[0].number_of_global_dofs()
                 vgdof = gdof * GD
                 force = np.zeros( (vgdof, 1) )
-                force[1, 0] = -1
-                fixeddofs = np.union1d( np.arange(0, 2*(nely+1), 2), np.array([2*(nelx+1)*(nely+1) - 1]) )
+                force[vgdof-1, 0] = -1
+                #force[1, 0] = -1
+                fixeddofs = np.arange(0, 2*(mesh.ny+1), 1)
+                # np.union1d( np.arange(0, 2*(nely+1), 2), np.array([2*(nelx+1)*(nely+1) - 1]) )
 
             bc = {'force': force, 'fixeddofs': fixeddofs}
 
@@ -53,11 +63,15 @@ class TopSimp:
             H, Hs = compute_filter(mesh, filter_radius)
             ft = {'type':1, 'H':H, 'Hs':Hs}
 
+        if global_volume_constraints is None:
+            global_volume_constraints = {'isOn':True, 'volfrac':0.5}
+
         self.mesh = mesh
         self.space = space
         self.bc = bc
         self.material = material
         self.ft = ft
+        self.global_volume_constraints = global_volume_constraints
 
     # SIMP 材料插值模型
     def material_model(self, rho):
@@ -78,19 +92,18 @@ class TopSimp:
         bform = BilinearForm(vspace)
         bform.add_domain_integrator(integrator)
         KK = integrator.assembly_cell_matrix(space=vspace)
-        print("KK:", KK.shape, "\n", KK[0].round(4))
+        #print("KK:", KK.shape, "\n", KK[0].round(4))
         K = bform.assembly()
-        print("K:", K.shape, "\n", K.toarray().round(4))
+        #print("K:", K.shape, "\n", K.toarray().round(4))
 
         dflag = self.bc['fixeddofs']
-        print("dflag:", dflag)
         uh.flat[dflag] = 0
 
         F = self.bc['force']
-        print("F:", F.shape, "\n", F.T.round(4))
+        #print("F:", F.shape, "\n", F.T.round(4))
         F -= K@uh.reshape(-1, 1)
         F[dflag.flat] = uh.reshape(-1, 1)[dflag.flat]
-        print("F:", F.shape, "\n", F.T.round(4))
+        #print("F:", F.shape, "\n", F.T.round(4))
 
         bdIdx = np.zeros(K.shape[0], dtype=np.int_)
         bdIdx[dflag.flat] = 1
@@ -98,7 +111,7 @@ class TopSimp:
         D0 = spdiags(1-bdIdx, 0, K.shape[0], K.shape[0])
         D1 = spdiags(bdIdx, 0, K.shape[0], K.shape[0])
         K = D0@K@D0 + D1
-        print("K:", K.shape, "\n", K.toarray().round(4))
+        #print("K:", K.shape, "\n", K.toarray().round(4))
 
         # 线性方程组求解
         uh.flat[:] = spsolve(K, F)
@@ -106,12 +119,14 @@ class TopSimp:
         ldof = self.space.number_of_local_dofs()
         vldof = GD * ldof
         cell2dof = vspace[0].cell_to_dof()
+        print("cell2dof:", cell2dof)
         NC = self.mesh.number_of_cells()
-        ue = np.zeros((NC, vldof, 1))
+        ue = np.zeros((NC, vldof))
 
         reshaped_uh = uh.reshape(-1)
         # 每个单元的自由度（每个节点两个自由度）
         updated_cell2dof = np.repeat(cell2dof*GD, GD, axis=1) + np.tile(np.array([0, 1]), (NC, ldof))
+        print("updated_cell2dof:", updated_cell2dof.shape, "\n", updated_cell2dof)
         idx = np.array([0, 1, 4, 5, 6, 7, 2, 3], dtype=np.int_)
         # 用 Top 中的自由度替换 FEALPy 中的自由度
         updated_cell2dof = updated_cell2dof[:, idx]
@@ -148,7 +163,6 @@ class TopSimp:
     def compute_compliance_sensitivity(self, rho):
 
         uh, ue = self.fe_analysis(rho)
-        c1 = np.dot(self.bc['force'].T, uh.flat)
 
         nu = self.material['nu']
         E0 = self.material['E0']
@@ -170,3 +184,8 @@ class TopSimp:
         dc = np.einsum('i, j -> ', temp1, temp2)
 
         return dc
+    
+    def compute_global_volume_constraint(self, rho):
+        g = np.mean(rho) / self.global_volume_constraints['vf'] - 1.
+
+        return g
