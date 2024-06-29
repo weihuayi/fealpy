698a22f21248e68d38d9150133b542408c1e7aa3
brighthe98@gmail.com <brighthe98@gmail.com>
2024-06-29 19:50:22 +0800

    update

--- test/np/space/test_lagrangefe.py
+++ test/np/space/test_lagrangefe.py
@@ -1,20 +1,17 @@
 from fealpy.np.functionspace import LagrangeFESpace as Space
 from fealpy.np.mesh.triangle_mesh import TriangleMesh
-
+import numpy as np
 
 mesh = TriangleMesh.from_box(box=[0, 1, 0, 1], nx=2, ny=2)
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# axes = fig.gca()
-# mesh.add_plot(axes)
-# mesh.find_cell(axes, showindex=True, color='k', marker='s', markersize=2, fontsize=8, fontcolor='k')
-# mesh.find_node(axes, showindex=True, color='r', marker='o', markersize=2, fontsize=8, fontcolor='r')
-# plt.show()
 space = Space(mesh, p=1, ctype='C')
-GD = 2
-vspace = GD*(space, )
-uh = vspace[0].function(dim=GD)
+uh = space.function(dim=2)
 print("uh:", uh.shape, "\n", uh)
+
+bcs = np.array([[1, 0, 0], [0, 1, 0]], dtype=np.float64)
+test = uh(bcs)
+print("test:", test)
+
+
 gdof = vspace[0].number_of_global_dofs()
 vgdof = gdof * GD
 ldof = vspace[0].number_of_local_dofs()
