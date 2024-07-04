9a0b95e77c2997b44b55ab1d0f9ddf605d5b88e0
wpx <1143615697@qq.com>
2024-07-04 09:21:07 +0800

    update

--- fealpy/torch/fem/__init__.py
+++ fealpy/torch/fem/__init__.py
@@ -8,6 +8,7 @@ from .linear_form import LinearForm
 from .scalar_diffusion_integrator import ScalarDiffusionIntegrator
 from .scalar_mass_integrator import ScalarMassIntegrator
 from .scalar_convection_integrator import ScalarConvectionIntegrator
+from .press_work_integrator import PressWorkIntegrator, PressWorkIntegrator1
 
 ### Cell Source
 from .scalar_source_integrator import ScalarSourceIntegrator
