7d2f88887142f921e047173cb5458c4e4901aae1
BellaLq <2655493031@qq.com>
2024-07-04 22:07:35 +0800

    update np.mesh lagrange_mesh and lagrange_triangle_mesh

--- fealpy/np/mesh/lagrange_triangle_mesh.py
+++ fealpy/np/mesh/lagrange_triangle_mesh.py
@@ -1,10 +1,12 @@
-from typing import Optional, Union, List
+from typing import Optional, Union, List,Tuple
 
 import numpy as np
 from numpy.typing import NDArray
 
-from fealpy.np.mesh.mesh_base import _S
-from fealpy.np.mesh.quadrature import Quadrature
+from .mesh_base import _S
+from .lagrange_mesh import LagrangeMesh
+from .triangle_mesh import TriangleMesh
+from .quadrature import Quadrature
 
 from .. import logger
 from . import functional as F
@@ -16,10 +18,73 @@ _S = slice(None)
 
 
 class LagrangeTriangleMesh(LagrangeMesh):
+    def __init__(self, node: NDArray, cell: NDArray, p=1, surface=None):
+        super().__init__(TD=2)
+        mesh = TriangleMesh(node, cell)
+        NN = mesh.number_of_nodes()
 
+        self.ftype = node.dtype
+        self.itype = cell.dtype
+        self.meshtype = 'ltri'
 
-    def __init__(self):
-        pass
+        self.p = p
+        self.surface = surface
 
+        self.node = mesh.interpolation_points(p)
+
+        if surface is not None:
+            self.node,_ = surface.project(self.node)
+        cell = mesh.cell_to_ipoint(p)
+
+        self.nodedata = {}
+        self.edgedata = {}
+        self.celldata = {}
+        self.meshdata = {}
+    
+    @classmethod
     def from_triangle_mesh(cls, mesh, p):
-        pass
+        node = mesh.entity("node")
+        cell = mesh.entity("cell")
+        return cls(node, cell, p=p)
+ 
+    def vtk_cell_type(self, etype='cell'):
+        """
+        @berif  返回网格单元对应的 vtk类型。
+        """
+        if etype in {'cell', 2}:
+            VTK_LAGRANGE_TRIANGLE = 69
+            return VTK_LAGRANGE_TRIANGLE 
+        elif etype in {'face', 'edge', 1}:
+            VTK_LAGRANGE_CURVE = 68
+            return VTK_LAGRANGE_CURVE
+
+    def to_vtk(self, etype='cell', index=np.s_[:], fname=None):
+        """
+        Parameters
+        ----------
+
+        @berif 把网格转化为 VTK 的格式
+        """
+        from .vtk_extent import vtk_cell_index, write_to_vtu
+
+        node = self.entity('node')
+        GD = self.geo_dimension()
+        if GD == 2:
+            node = np.concatenate((node, np.zeros((node.shape[0], 1), dtype=self.ftype)), axis=1)
+
+        cell = self.entity(etype)[index]
+        cellType = self.vtk_cell_type(etype)
+        idx = vtk_cell_index(self.p, cellType)
+        NV = cell.shape[-1]
+
+        cell = np.r_['1', np.zeros((len(cell), 1), dtype=cell.dtype), cell[:, idx]]
+        cell[:, 0] = NV
+
+        NC = len(cell)
+        if fname is None:
+            return node, cell.flatten(), cellType, NC 
+        else:
+            print("Writting to vtk...")
+            write_to_vtu(fname, node, NC, cellType, cell.flatten(),
+                    nodedata=self.nodedata,
+                    celldata=self.celldata)
