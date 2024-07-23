95db060da87693e19f169f4696e7a2f7bb2e9b9c
BellaLq <2655493031@qq.com>
2024-07-23 21:04:34 +0800

    update lagrangetroanglemesh

--- fealpy/np/mesh/lagrange_triangle_mesh.py
+++ fealpy/np/mesh/lagrange_triangle_mesh.py
@@ -46,7 +46,6 @@ class LagrangeTriangleMesh(LagrangeMesh):
         self.celldata = {}
         self.meshdata = {}
 
-
     def construct(self):
         pass
 
@@ -66,7 +65,7 @@ class LagrangeTriangleMesh(LagrangeMesh):
 
     
     @classmethod
-    def from_triangle_mesh(cls, mesh, p, surface=None):
+    def from_triangle_mesh(cls, mesh, p:int, surface=None):
         node = mesh.interpolation_points(p)
         cell = mesh.cell_to_ipoint(p)
         if surface is not None:
@@ -78,8 +77,24 @@ class LagrangeTriangleMesh(LagrangeMesh):
         lmesh.cell2edge = mesh.cell_to_edge()
         lmesh.edge  = mesh.edge_to_ipoint(p)
         return lmesh 
+
+    # quadrature
+    def quadrature_formula(self, q: int, etype: Union[int, str]='cell'):
+        from .quadrature import TriangleQuadrature
+        from .quadrature import GaussLegendreQuadrature
+
+        if isinstance(etype, str):
+            etype = estr2dim(self, etype)
+        if etype == 2:
+            quad = TriangleQuadrature(q)
+        elif etype == 1:
+            quad = GaussLegendreQuadrature(q)
+        else:
+            raise ValueError(f"Unsupported entity or top-dimension: {etype}")
+
+        return quad
     
-    def cell_area(self, q=None, index=np.s_[:]):
+    def cell_area(self, q=None, index: Index=_S):
         """
         Calculate the area of a cell.
         """
@@ -87,7 +102,7 @@ class LagrangeTriangleMesh(LagrangeMesh):
         q = p if q is None else q
         GD = self.geo_dimension()
 
-        qf = self.integrator(q, etype='cell')
+        qf = self.quadrature_formula(q, etype='cell')
         bcs, ws = qf.get_quadrature_points_and_weights()
         J = self.jacobi_matrix(bcs, index=index)
         n = np.cross(J[..., 0], J[..., 1], axis=-1)
@@ -95,7 +110,67 @@ class LagrangeTriangleMesh(LagrangeMesh):
             n = np.sqrt(np.sum(n**2, axis=-1))
         a = np.einsum('i, ij->j', ws, n)/2.0
         return a
-    
+
+    def cell_unit_normal(self, bc: NDArray, index: Index=_S):
+        """
+        计算曲面情形下，积分点处的单位法线方向。
+        """
+        J = self.jacobi_matrix(bc, index=index)
+
+        # n.shape 
+        n = np.cross(J[..., 0], J[..., 1], axis=-1)
+        if self.GD == 3:
+            l = np.sqrt(np.sum(n**2, axis=-1, keepdims=True))
+            n /= l
+        return n
+
+    def bc_to_point(self, bc: NDArray, index: Index=_S, etype='cell'):
+        """
+        """
+        node = self.node
+        TD = bc.shape[-1] - 1
+        entity = self.entity(etype=TD)[index] # 
+        phi = self.shape_function(bc) # (NQ, 1, ldof)
+        p = np.einsum('ijk, jkn->ijn', phi, node[entity])
+        return p
+
+    def shape_function(self, bc: NDArray, p=None):
+        """
+        Returns the value of the shape function of the mesh element at the integration point,
+        otherwise returns the value of the spatial basis function at the integration point.
+        """
+        p = self.p if p is None else p
+        phi = F.simplex_shape_function(bc, p)
+        return phi[..., None, :]
+
+    def grad_shape_function(self, bc: NDArray, p=None, index: Index=_S, variables='u'):
+        """
+        Notes
+        -----
+        计算单元形函数关于参考单元变量 u=(xi, eta) 或者实际变量 x 梯度。
+
+        lambda_0 = 1 - xi - eta
+        lambda_1 = xi
+        lambda_2 = eta
+
+        """
+        p = self.p if p is None else p 
+        TD = bc.shape[-1] - 1
+        if TD == 2:
+            Dlambda = np.array([[-1, -1], [1, 0], [0, 1]], dtype=self.ftype)
+        else:
+            Dlambda = np.array([[-1], [1]], dtype=self.ftype)
+        R = F.simplex_grad_shape_function(bc, p) # (..., ldof, TD+1)
+        gphi = np.einsum('...ij, jn->...in', R, Dlambda) # (..., ldof, TD)
+
+        if variables == 'u':
+            return gphi[..., None, :, :] #(..., 1, ldof, TD)
+        elif variables == 'x':
+            G, J = self.first_fundamental_form(bc, index=index, return_jacobi=True)
+            G = np.linalg.inv(G)
+            gphi = np.einsum('...ikm, ...imn, ...ln->...ilk', J, G, gphi) 
+            return gphi
+
     def vtk_cell_type(self, etype='cell'):
         """
         @berif  返回网格单元对应的 vtk类型。
