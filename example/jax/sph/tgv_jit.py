b4a162eba2637e0440a04bcaa1be3eab13d13d30
wpx <1143615697@qq.com>
2024-07-26 10:39:58 +0800

    update

--- example/jax/sph/tgv_jit.py
+++ example/jax/sph/tgv_jit.py
@@ -11,9 +11,12 @@ from fealpy.jax.sph.jax_md.partition import Sparse
 from fealpy.jax.sph.kernel_function import QuinticKernel
 from jax_md import space
 from jax import ops, vmap
-from jax import lax
+from jax import lax, jit
 import matplotlib.pyplot as plt
 import warnings
+import time
+
+
 
 EPS = jnp.finfo(float).eps
 dx = 0.02
@@ -51,39 +54,20 @@ neighbor_fn = partition.neighbor_list(
     num_partitions=mesh.nodedata["position"].shape[0],
     pbc=[True, True, True],
 )
+forward = solver.forward_wrapper(displacement, kernel)
+
+advance = TimeLine(forward, shift)
+advance = jit(advance)
 ##数据类型？？
 warnings.filterwarnings("ignore", category=FutureWarning)
 neighbors = neighbor_fn.allocate(mesh.nodedata["position"], num_particles=mesh.nodedata["position"].shape[0])
 
-for i in range(t_num+2):
-    
-    mesh.nodedata['mv'] += 1.0*dt*mesh.nodedata["dmvdt"]
-    mesh.nodedata['tv'] = mesh.nodedata['mv']
-    #mesh.nodedata['tv'] = mesh.nodedata['mv'] + 0.5*dt*mesh.nodedata['dtvdt']
-    mesh.nodedata["position"] = shift(mesh.nodedata["position"], 1.0 * dt * mesh.nodedata["tv"])
-
-    neighbors = neighbors.update(mesh.nodedata["position"], num_particles=mesh.nodedata["position"].shape[0])
-    
-    r = mesh.nodedata["position"]
-    i_s, j_s = neighbors.idx
-    r_i_s, r_j_s = r[i_s], r[j_s]
-    dr_i_j = vmap(displacement)(r_i_s, r_j_s)
-    dist = space.distance(dr_i_j)
-    w_dist = vmap(kernel.value)(dist)  
-    e_s = dr_i_j / (dist[:, None] + EPS) 
-    grad_w_dist_norm = vmap(kernel.grad_value)(dist)
-    grad_w_dist = grad_w_dist_norm[:, None] * e_s
-
-
-    mesh.nodedata['rho'] = solver.compute_rho(mesh.nodedata['mass'], i_s, w_dist)
-    p = solver.tait_eos(mesh.nodedata['rho'],c0,rho0)
-    background_pressure_tvf = solver.tait_eos(jnp.zeros_like(p), c0, rho0)
-    
-    mesh.nodedata["dmvdt"] = solver.compute_mv_acceleration(\
-            mesh.nodedata, i_s, j_s, dr_i_j, dist, grad_w_dist_norm, p)
-    #dtvdt = solver.compute_tv_acceleration(state, i_s,j_s, grad_w_dist)
-    
-    #fname = path + 'test_'+ str(i+1).zfill(10) + '.h5'
-    #solver.write_h5(mesh.nodedata, fname)
+start = time.time()
+for i in range(100):
+    print(i)
+    mesh.nodedata, neighbors = advance(dt, mesh.nodedata, neighbors) 
     #fname = path + 'test_'+ str(i+1).zfill(10) + '.vtk'
     #solver.write_vtk(mesh.nodedata, fname)
+
+end = time.time()
+print(end-start)
