400024d3871cf76319b7ec91ebb50f818564031c
wpx <1143615697@qq.com>
2024-07-27 19:12:44 +0800

    update

--- fealpy/jax/sph/solver.py
+++ fealpy/jax/sph/solver.py
@@ -14,7 +14,7 @@ import h5py
 import pyvista
 from typing import Dict
 import enum
-from jax_md import space
+from fealpy.jax.sph.jax_md import space
 
 #设置标签
 class Tag(enum.IntEnum):
@@ -38,7 +38,8 @@ class SPHSolver:
         """Equation of state update pressure"""
         return gamma * c0**2 * ((rho/rho0)**gamma - 1) / rho0 + X
     
-    def tait_eos_p2rho(slef, p, p0, rho0, gamma=1.0, X=0.0):
+    @staticmethod
+    def tait_eos_p2rho(p, p0, rho0, gamma=1.0, X=0.0):
         """Calculate density by pressure."""
         p_temp = p + p0 - X
         return rho0 * (p_temp / p0) ** (1 / gamma)
