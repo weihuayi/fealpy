073e0182a2956500398d8a183c8669d8bb7c8a92
chenchunyu <cbtxs@qq.com>
2024-07-30 10:00:56 +0800

    update

--- test/experimental/mesh/test_hexahedron_mesh.py
+++ test/experimental/mesh/test_hexahedron_mesh.py
@@ -17,12 +17,21 @@ def test_jacobi_matrix():
     bcs, ws = integrator.get_quadrature_points_and_weights()
     jacobi = mesh.jacobi_matrix(bcs)
 
+def test_first_fundamental_form():
+    mesh = HexahedronMesh.from_one_hexahedron()
+    integrator = mesh.quadrature_formula(2, 'cell')
+
+    bcs, ws = integrator.get_quadrature_points_and_weights()
+    jacobi = mesh.jacobi_matrix(bcs)
+
+    fff = mesh.first_fundamental_form(jacobi)
+
+
 
 if __name__ == "__main__":
     #test_bc_to_point()
-    test_jacobi_matrix()
-    a =  bm.zeros(10)
-    print(a.device)
+    #test_jacobi_matrix()
+    test_first_fundamental_form()
 
 
 
