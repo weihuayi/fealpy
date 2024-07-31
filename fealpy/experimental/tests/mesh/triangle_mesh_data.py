e84c6585ce22e0e1c97c7e0f6157ce000ca66d8d
2655493031@qq.com <2655493031@qq.com>
2024-07-31 17:50:34 +0800

    update triangle mesh

--- fealpy/experimental/tests/mesh/triangle_mesh_data.py
+++ fealpy/experimental/tests/mesh/triangle_mesh_data.py
@@ -44,6 +44,46 @@ from_box_data= [
             }
 ]
 
-entity_measure_data = [{"measure0": np.array([0.0], dtype=np.float64),
-    "measure1": np.array([1.0, 1.0, np.sqrt(2)], dtype=np.float64),
-    "measure": np.array([0.5], dtype=np.float64)}]
+entity_measure_data = [
+        {
+            "node_measure": np.array([0.0], dtype=np.float64),
+            "edge_measure": np.array([1.0, 1.0, np.sqrt(2)], dtype=np.float64),
+            "cell_measure": np.array([0.5], dtype=np.float64)
+            }
+]
+
+grad_lambda_data = [
+        {
+            "val_shape": (62, 3, 2)}
+]
+
+interpolation_point_data = [
+        {
+            "ips": np.array([[0.  , 0.  ],
+                [1.  , 0.  ],
+                [0.  , 1.  ],
+                [0.25, 0.  ],
+                [0.5 , 0.  ],
+                [0.75, 0.  ],
+                [0.  , 0.75],
+                [0.  , 0.5 ],
+                [0.  , 0.25],
+                [0.75, 0.25],
+                [0.5 , 0.5 ],
+                [0.25, 0.75],
+                [0.25, 0.25],
+                [0.5 , 0.25],
+                [0.25, 0.5 ]], dtype=np.float64),
+            "c2p": np.array([[ 0,  3,  8,  4, 12,  7,  5, 13, 14,  
+                6,  1,  9, 10, 11,  2]], dtype=np.int32)
+            }
+] 
+
+
+from_torus_surface_data = [
+        {"R": 3, "r": 1, "Nu": 20, "Nv": 20, "node_shape": (20*20, 3), "cell_shape": (2*20*20, 3)},
+        {"R": 5, "r": 2, "Nu": 30, "Nv": 30, "node_shape": (30*30, 3), "cell_shape": (2*30*30, 3)},
+        {"R": 4, "r": 1.5, "Nu": 15, "Nv": 10, "node_shape": (15*10, 3), "cell_shape": (2*15*10, 3)},
+        {"R": 6, "r": 3, "Nu": 25, "Nv": 10, "node_shape": (25*10, 3), "cell_shape": (2*25*10, 3)}
+]
+
