074a97b0c31219e5784d332136ece0421265767c
brighthe98@gmail.com <brighthe98@gmail.com>
2024-08-01 17:26:27 +0800

    Solved the problem of inconsistent types in structured mesh

--- fealpy/experimental/mesh/uniform_mesh_2d.py
+++ fealpy/experimental/mesh/uniform_mesh_2d.py
@@ -4,21 +4,39 @@ from typing import Union, Optional, Sequence, Tuple, Any
 from .utils import entitymethod, estr2dim
 
 from ..backend import backend_manager as bm 
-from ..typing import TensorLike, Index, _S
+from ..typing import TensorLike, Index, _S, Union, Tuple
 from .. import logger
 
 from .mesh_base import StructuredMesh
 
 class UniformMesh2d(StructuredMesh):
-    def __init__(self,
-                 extent: Tuple[int, int, int, int],
-                 h: Tuple[float, float] = (1.0, 1.0),
-                 origin: Tuple[float, float] = (0.0, 0.0)):
-        super().__init__(TD=2)
+    """
+    Topological data structure of a structured quadrilateral mesh
+
+    The ordering of the nodes in each element is as follows:
+
+    1 ------- 3
+    |         |
+    |         |
+    |         |
+    0 ------- 2
+
+    The ordering of entities in the entire mesh is as follows:
+
+    * Node numbering rule: first in the y direction, then in the x direction
+    * Edge numbering rule: first in the y direction, then in the x direction
+    * Cell numbering rule: first in the y direction, then in the x direction
+
+    """
+    def __init__(self, extent = (0, 1, 0, 1), h = (1.0, 1.0), origin = (0.0, 0.0), 
+                itype=None, ftype=None):
+        
+        super().__init__(TD=2, itype=itype, ftype=ftype)
+
         # Mesh properties
-        self.extent: Tuple[int, int, int, int] = extent
-        self.h: Tuple[float, float] = h
-        self.origin: Tuple[float, float] = origin
+        self.extent = [int(e) for e in extent]
+        self.h = [float(val) for val in h]
+        self.origin = [float(o) for o in origin]
 
         # Mesh dimensions
         self.nx = self.extent[1] - self.extent[0]
@@ -28,6 +46,7 @@ class UniformMesh2d(StructuredMesh):
         self.NF = self.NE
         self.NC = self.nx * self.ny
 
+        # Mesh datas
         self.nodedata = {}
         self.edgedata = {}
         self.facedata = self.edgedata
@@ -38,45 +57,44 @@ class UniformMesh2d(StructuredMesh):
 
         self.face_to_ipoint = self.edge_to_ipoint
 
-    @property
-    def ftype(self) -> Any:
-        h = self.h
-        if h is None:
-            raise RuntimeError('Can not get the float type as the h '
-                               'has not been assigned.')
-        return type(h[0])
-    
-    @property
-    def itype(self) -> Any:
-        extent = self.extent
-        if extent is None:
-            raise RuntimeError('Can not get the int type as the extent '
-                               'has not been assigned.')
-        return type(extent[0])
 
     @entitymethod(0)
-    def _get_node(self):
-        GD = 2
+    def _get_node(self) -> TensorLike:
+        """
+        @berif Generate the nodes in a structured mesh.
+        """
         nx = self.nx
         ny = self.ny
+
         box = [self.origin[0], self.origin[0] + nx * self.h[0],
                self.origin[1], self.origin[1] + ny * self.h[1]]
-        x = bm.linspace(box[0], box[1], nx + 1)
-        y = bm.linspace(box[2], box[3], ny + 1)
+
+        x = bm.linspace(box[0], box[1], nx + 1, dtype=self.ftype)
+        y = bm.linspace(box[2], box[3], ny + 1, dtype=self.ftype)
         xx, yy = bm.meshgrid(x, y, indexing='ij')
-        node = bm.zeros((nx + 1, ny + 1, GD), dtype=self.ftype)
-        #node = bm.zeros((nx + 1, ny + 1, GD), dtype=bm.float64)
-        #node[..., 0] = xx
-        #node[..., 1] = yy
         node = bm.concatenate((xx[..., np.newaxis], yy[..., np.newaxis]), axis=-1)
+
+
+        # if bm.backend_name == 'numpy' or bm.backend_name == 'pytorch':
+        #     node = bm.zeros((nx + 1, ny + 1, GD), dtype=self.ftype)
+        #     # node = bm.zeros((nx + 1, ny + 1, GD), dtype=bm.float64)
+        #     node[..., 0] = xx
+        #     node[..., 1] = yy
+        # elif bm.backend_name == 'jax':
+        #     raise NotImplementedError("Jax backend is not yet implemented.")
+        # else:
+        #     raise NotImplementedError("Backend is not yet implemented.")
+        #node = bm.concatenate((xx[..., np.newaxis], yy[..., np.newaxis]), axis=-1)
 
         return node
     
     @entitymethod(1)
     def _get_edge(self):
+        """
+        @berif Generate the edges in a structured mesh.
+        """
         nx = self.nx
         ny = self.ny
-
         NN = self.NN
         NE = self.NE
 
@@ -134,12 +152,7 @@ class UniformMesh2d(StructuredMesh):
         GD = 2
         if isinstance(etype, str):
            etype = estr2dim(self, etype)
-        # if etype in {'cell', 2}:
-        #     return self.cell[index, ...]
-        # elif etype in {'edge', 'face', 1}:
-        #     return self.edge[index, ...]
-        # elif etype in {'node', 0}:
-        #     return self.node.reshape(-1, GD)[index, ...]
+
         if etype == 2:
             return self.cell[index, ...]
         elif etype == 1:
@@ -152,14 +165,90 @@ class UniformMesh2d(StructuredMesh):
     def entity_measure(self, etype: Union[int, str]) -> TensorLike:
        if isinstance(etype, str):
            etype = estr2dim(self, etype)
+
        if etype == 0:
-           return bm.tensor(0, dtype=self.ftype)
+        return bm.tensor(0, dtype=self.ftype)
+        #return bm.tensor(0, dtype=bm.float64)
+
        elif etype == 1:
            return self.h[0], self.h[1]
        elif etype == 2:
            return self.h[0] * self.h[1]
        else:
            raise ValueError(f"Unsupported entity or top-dimension: {etype}")
+       
+    # def cell_barycenter(self):
+    #     GD = self.geo_dimension()
+    #     nx = self.nx
+    #     ny = self.ny
+    #     box = [self.origin[0] + self.h[0] / 2, self.origin[0] + self.h[0] / 2 + (nx - 1) * self.h[0],
+    #            self.origin[1] + self.h[1] / 2, self.origin[1] + self.h[1] / 2 + (ny - 1) * self.h[1]]
+    #     bc = bm.zeros((nx, ny, 2), dtype=self.ftype)
+    #     bc[..., 0], bc[..., 1] = np.mgrid[
+    #                              box[0]:box[1]:nx * 1j,
+    #                              box[2]:box[3]:ny * 1j]
+    #     return bc
+
+    # def edge_barycenter(self):
+    #     """
+    #     @brief
+    #     """
+    #     bcx = self.edgex_barycenter()
+    #     bcy = self.edgey_barycenter()
+    #     return bcx, bcy
+
+    # ## @ingroup FDMInterface
+    # def edgex_barycenter(self):
+    #     """
+    #     @brief
+    #     """
+    #     GD = self.geo_dimension()
+    #     nx = self.nx
+    #     ny = self.ny
+    #     box = [self.origin[0] + self.h[0] / 2, self.origin[0] + self.h[0] / 2 + (nx - 1) * self.h[0],
+    #            self.origin[1], self.origin[1] + ny * self.h[1]]
+    #     bc = np.zeros((nx, ny + 1, 2), dtype=self.ftype)
+    #     bc[..., 0], bc[..., 1] = np.mgrid[
+    #                              box[0]:box[1]:nx * 1j,
+    #                              box[2]:box[3]:(ny + 1) * 1j]
+    #     return bc
+
+    # ## @ingroup FDMInterface
+    # def edgey_barycenter(self):
+    #     """
+    #     @breif
+    #     """
+    #     GD = self.geo_dimension()
+    #     nx = self.nx
+    #     ny = self.ny
+    #     box = [self.origin[0], self.origin[0] + nx * self.h[0],
+    #            self.origin[1] + self.h[1] / 2, self.origin[1] + self.h[1] / 2 + (ny - 1) * self.h[1]]
+    #     bc = np.zeros((nx + 1, ny, 2), dtype=self.ftype)
+    #     bc[..., 0], bc[..., 1] = np.mgrid[
+    #                              box[0]:box[1]:(nx + 1) * 1j,
+    #                              box[2]:box[3]:ny * 1j]
+    #     return bc
+
+    # def gradient(self, f, order=1):
+    #     """
+    #     @brief 求网格函数 f 的梯度
+    #     """
+    #     hx = self.h[0]
+    #     hy = self.h[1]
+    #     fx, fy = np.gradient(f, hx, hy, edge_order=order)
+    #     return fx, fy
+
+    # ## @ingroup FDMInterface
+    # def divergence(self, f_x, f_y, order=1):
+    #     """
+    #     @brief 求向量网格函数 (fx, fy) 的散度
+    #     """
+
+    #     hx = self.h[0]
+    #     hy = self.h[1]
+    #     f_xx, f_xy = np.gradient(f_x, hx, edge_order=order)
+    #     f_yx, f_yy = np.gradient(f_y, hy, edge_order=order)
+    #     return f_xx + f_yy
     
     def quadrature_formula(self, q, etype:Union[int, str]='cell'):
         from ..quadrature import GaussLegendreQuadrature, TensorProductQuadrature
@@ -200,16 +289,17 @@ class UniformMesh2d(StructuredMesh):
 
         NN = self.number_of_nodes()
         gdof = self.number_of_global_ipoints(p)
-        ipoints = bm.zeros((gdof, GD), dtype=self.ftype)
+        #ipoints = bm.zeros((gdof, GD), dtype=self.ftype)
+        ipoints = bm.zeros((gdof, GD), dtype=bm.float64)
         ipoints[:NN, :] = node
 
         NE = self.number_of_edges()
-        multiIndex = self.multi_index_matrix(p, 1)
+        multiIndex = self.multi_index_matrix(p, 1, dtype=node.dtype)
         w = multiIndex[1:-1, :] / p
         ipoints[NN:NN + (p-1) * NE, :] = bm.einsum('ij, ...jm -> ...im', w,
                 node[edge,:]).reshape(-1, GD)
 
-        w = np.einsum('im, jn -> ijmn', w, w).reshape(-1, 4)
+        w = bm.einsum('im, jn -> ijmn', w, w).reshape(-1, 4)
         ipoints[NN + (p-1) * NE:, :] = bm.einsum('ij, kj... -> ki...', w,
                 node[cell[:]]).reshape(-1, GD)
 
@@ -217,30 +307,33 @@ class UniformMesh2d(StructuredMesh):
     
     def jacobi_matrix(self, bcs: TensorLike, index: Index=_S) -> TensorLike:
         """
-        @brief 计算参考单元 (xi, eta) 到实际 Lagrange 四边形(x) 之间映射的 Jacobi 矩阵。
+        @brief Compute the Jacobi matrix for the mapping from the reference element (xi, eta) 
+               to the actual Lagrange quadrilateral (x)
 
         x(xi, eta) = phi_0 x_0 + phi_1 x_1 + ... + phi_{ldof-1} x_{ldof-1}
         """
         node = self.entity('node')
         cell = self.entity('cell', index=index)
         gphi = self.grad_shape_function(bcs, p=1, variables='u', index=index)
-        J = bm.einsum( 'cim, ...in->...cmn', node[cell[:]], gphi)
+        J = bm.einsum( 'cim, ...in -> ...cmn', node[cell[:]], gphi)
 
         return J
     
     def first_fundamental_form(self, J: TensorLike) -> TensorLike:
         """
-        @brief 由 Jacobi 矩阵计算第一基本形式。
+        @brief Compute the first fundamental form from the Jacobi matrix.
         """
         TD = J.shape[-1]
 
         shape = J.shape[0:-2] + (TD, TD)
-        G = bm.zeros(shape, dtype=self.ftype)
+        #G = bm.zeros(shape, dtype=self.ftype)
+        G = bm.zeros(shape, dtype=bm.float64)
         for i in range(TD):
             G[..., i, i] = bm.einsum('...d, ...d -> ...', J[..., i], J[..., i])
             for j in range(i+1, TD):
                 G[..., i, j] = bm.einsum('...d, ...d -> ...', J[..., i], J[..., j])
                 G[..., j, i] = G[..., i, j]
+                
         return G
        
     def shape_function(self, bcs: TensorLike, p: int=1, 
@@ -250,6 +343,7 @@ class UniformMesh2d(StructuredMesh):
         TD = bcs[0].shape[-1] - 1
         if mi is None:
             mi = bm.multi_index_matrix(p, TD, dtype=self.itype)
+            #mi = bm.multi_index_matrix(p, TD, dtype=bm.int32)
         phi = [bm.simplex_shape_function(val, p, mi) for val in bcs]
         ldof = self.number_of_local_ipoints(p)
 
@@ -260,20 +354,22 @@ class UniformMesh2d(StructuredMesh):
         assert isinstance(bcs, tuple)
 
         Dlambda = bm.array([-1, 1], dtype=self.ftype)
+        #Dlambda = bm.array([-1, 1], dtype=bm.float64)
 
         phi0 = bm.simplex_shape_function(bcs[0], p=p)
         R0 = bm.simplex_grad_shape_function(bcs[0], p=p)
-        gphi0 = bm.einsum('...ij, j -> ...i', R0, Dlambda) # (..., ldof)
+        gphi0 = bm.einsum('...ij, j -> ...i', R0, Dlambda)
 
         phi1 = bm.simplex_shape_function(bcs[1], p=p)
         R1 = bm.simplex_grad_shape_function(bcs[1], p=p)
-        gphi1 = bm.einsum('...ij, j->...i', R1, Dlambda) # (..., ldof)
+        gphi1 = bm.einsum('...ij, j->...i', R1, Dlambda)
 
         n = phi0.shape[0] * phi1.shape[0]
         ldof = phi0.shape[-1] * phi1.shape[-1]
 
         shape = (n, ldof, 2)
-        gphi = bm.zeros(shape, dtype=self.ftype)
+        # gphi = bm.zeros(shape, dtype=self.ftype)
+        gphi = bm.zeros(shape, dtype=bm.float64)
 
         gphi[..., 0] = bm.einsum('im, kn -> ikmn', gphi0, phi1).reshape(-1, ldof)
         gphi[..., 1] = bm.einsum('im, kn -> ikmn', phi0, gphi1).reshape(-1, ldof)
