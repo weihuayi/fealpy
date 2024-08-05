7df81f76f744457b117ad5ad5bec1ca5916c3180
Huayi Wei <weihuayi@xtu.edu.cn>
2024-08-05 15:29:46 +0800

    update

--- fealpy/experimental/opt/gradient_descent_alg.py
+++ fealpy/experimental/opt/gradient_descent_alg.py
@@ -1,8 +1,7 @@
 from ..backend import backend_manager as bm 
 from ..typing import TensorLike, Index, _S
 from .. import logger
-
-from .optimizer_base import Optimizer, Problem
+from .optimizer_base import Optimizer, opt_alg_options
 
 """
 Reference
@@ -12,35 +11,22 @@ https://en.wikipedia.org/wiki/Gradient_descent
 
 
 class GradientDescentAlg(Optimizer):
-
-    def __init__(self, problem: Problem) -> None:
-        super().__init__(problem)
-
-    @classmethod
-    def get_options(
-            cls,
-            x0: TensorLike,
-            objective,
-            NP: int,
-            MaxIters: int = 1000,
-            MaxFunEvals: int = 10000,
-            Print: bool = True,
-            ) -> Problem:
-        return Problem(
-                x0,
-                objective,
-                NP=NP,
-                MaxIters= MaxIters,
-                MaxFunEvals=MaxFunEvals,
-                Print=Print,
-                )
+    def __init__(self, options) -> None:
+        super().__init__(options)
 
     def run(self, queue=None, maxit=None):
         options = self.options
+        obj = options['objective']
+        x0 = options['x0']
+
+        self.f, self.g = obj(x0)
+
+
+
         alpha = options['StepLength']
 
         gnorm = norm(self.g)
-        self.diff = np.Inf
+        self.diff = bm.inf 
 
         if options['Disp']:
             print("The initial F(x): %12.11g, grad:%12.11g, diff:%12.11g"%(self.f, gnorm, self.diff))
@@ -62,12 +48,6 @@ class GradientDescentAlg(Optimizer):
 
             gnorm = norm(self.g)
 
-            if options['Disp']:
-                print("Step %d with F(x): %12.11g, grad:%12.11g, diff:%12.11g"%(i, self.f, gnorm, self.diff))
-
-            if options['Output']:
-                self.fun.output(str(self.NF).zfill(6), queue=queue)
-
             self.NF += 1
             maxg = np.max(np.abs(self.g.flat))
             if (maxg < options['NormGradTol']):
@@ -79,8 +59,4 @@ class GradientDescentAlg(Optimizer):
                     self.diff, options['FunValDiff'])
                 )
                 break
-
-        if options['Output']:
-            self.fun.output('', queue=queue, stop=True)
-
         return self.x, self.f, self.g, self.diff
