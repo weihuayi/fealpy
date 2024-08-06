4aaafc36c51a5957d4f568e71eb07c3dcbbe9f08
liuyang778 <89122231@qq.com>
2024-08-06 14:52:09 +0800

    update hba

--- fealpy/experimental/tests/opt/test_iopt_alg.py
+++ fealpy/experimental/tests/opt/test_iopt_alg.py
@@ -1,8 +1,8 @@
-import numpy as np
+# import numpy as np
 import pytest
 from fealpy.experimental.backend import backend_manager as bm
 from fealpy.experimental.opt import CrayfishOptAlg 
-# from fealpy.experimental.opt import HoneybadgerOptAlg 
+from fealpy.experimental.opt import HoneybadgerOptAlg 
 # from fealpy.experimental.opt import QuantumParticleswarmOptAlg 
 # from fealpy.experimental.opt import SnowmeltOptAlg 
 from fealpy.experimental.tests.opt.iopt_data import iopt_data
@@ -10,29 +10,34 @@ from fealpy.experimental.opt.optimizer_base import Optimizer, opt_alg_options
 
 class TestIOptInterfaces:
 
-    #@pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
-    @pytest.mark.parametrize("backend", ['numpy'])
-    @pytest.mark.parametrize("data", iopt_data)
-    @pytest.mark.parametrize("NP", [100])
-    def test_crayfish_opt_alg(self, backend, data, NP):
-        bm.set_backend(backend)
-        lb, ub = data['domain']
-        x0 = lb + bm.random.rand(NP, data['ndim'])*(ub - lb)
-        option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
-        #problem = CrayfishOptAlg.get_options(x0, data['objective'], NP, data['domain'])
-        optimizer = CrayfishOptAlg(option)
-        optimizer.run()
-
     # @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
+    # @pytest.mark.parametrize("backend", ['numpy'])
     # @pytest.mark.parametrize("data", iopt_data)
-    # @pytest.mark.parametrize("NP", [10, 20, 30, 40])
-    # def test_honeybadger_opt_alg(self, backend, data, NP):
+    # @pytest.mark.parametrize("NP", [100])
+    # def test_crayfish_opt_alg(self, backend, data, NP):
     #     bm.set_backend(backend)
     #     lb, ub = data['domain']
     #     x0 = lb + bm.random.rand(NP, data['ndim'])*(ub - lb)
-    #     problem = HoneybadgerOptAlg.get_options(x0, data['objective'], NP)
-    #     optimizer = HoneybadgerOptAlg(problem)
+    #     option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
+    #     #problem = CrayfishOptAlg.get_options(x0, data['objective'], NP, data['domain'])
+    #     optimizer = CrayfishOptAlg(option)
     #     optimizer.run()
+
+
+
+
+    @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
+    # @pytest.mark.parametrize("backend", ['numpy'])
+    @pytest.mark.parametrize("data", iopt_data)
+    @pytest.mark.parametrize("NP", [100])
+    def test_honeybadger_opt_alg(self, backend, data, NP):
+        bm.set_backend(backend)
+        lb, ub = data['domain']
+        x0 = lb + bm.random.rand(NP, data['ndim'])*(ub - lb)
+        # problem = HoneybadgerOptAlg.get_options(x0, data['objective'], NP,data['domain'])
+        option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
+        optimizer = HoneybadgerOptAlg(option)
+        optimizer.run()
 
     # @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
     # @pytest.mark.parametrize("data", iopt_data)
@@ -57,7 +62,7 @@ class TestIOptInterfaces:
     #     optimizer.run()
  
 if __name__ == "__main__":
-    pytest.main(["./test_iopt_alg.py", "-k", "test_crayfish_opt_alg"])
+    pytest.main(["./test_iopt_alg.py", "-k", "test_honeybadger_opt_alg"])
     #pytest.main(["./test_iopt_alg.py", "-k", "test_honeybadger_opt_alg"])
     #pytest.main(["./test_iopt_alg.py", "-k", "test_quantumparticleswarm_opt_alg"])
     #pytest.main(["./test_iopt_alg.py", "-k", "test_snowmelt_opt_alg"])
