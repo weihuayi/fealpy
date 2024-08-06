fede1f89239bbeed8e4095e5a174857f13100dfa
wilbur <1243702877@qq.com>
2024-08-06 10:44:48 +0800

    更新梯度下降方法，MovingMesh的部分代码，IntervalMesh 的 jax 测试

--- fealpy/experimental/opt/optimizer_base.py
+++ fealpy/experimental/opt/optimizer_base.py
@@ -64,8 +64,8 @@ class Optimizer():
         Return:
             The function value, with gradient value for gradient methods.
         """
-        self.__NF += problem.NP 
-        return self.problem.objective(x)
+        self.__NF += self.options['NP'] 
+        return self.options['objective'](x)
 
 
     def run(self):
