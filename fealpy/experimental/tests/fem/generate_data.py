c6e24a103e30c7e9bb7df8b9346302597a0604da
Liujiawangmath <15674760037@163.com>
2024-08-07 17:22:57 +0800

    update

--- fealpy/experimental/tests/fem/generate_data.py
+++ fealpy/experimental/tests/fem/generate_data.py
@@ -3,16 +3,17 @@ from fealpy.mesh import TriangleMesh
 from fealpy.functionspace import LagrangeFESpace
 import matplotlib.pyplot as plt
 from fealpy.decorator import barycentric, cartesian
-from fealpy.fem import ScalarDiffusionIntegrator
+from fealpy.fem import ScalarMassIntegrator
+
 
 
 
 mesh = TriangleMesh.from_box([0,1,0,1], 1, 1)
 space = LagrangeFESpace(mesh, 2)
-integrator = ScalarDiffusionIntegrator(1, 3)
+integrator = ScalarMassIntegrator(1, 3)
+assembly_cell_matrix = integrator.assembly_cell_matrix(space)
+print(assembly_cell_matrix)
 
-
-print(integrator.assembly_cell_matrix(space))
 
 
 
--- fealpy/experimental/tests/fem/test_scalar_mass_integrator.py
+++ fealpy/experimental/tests/fem/test_scalar_mass_integrator.py
@@ -4,10 +4,10 @@ import pytest
 from fealpy.experimental.backend import backend_manager as bm
 from fealpy.experimental.mesh.triangle_mesh import TriangleMesh
 from fealpy.experimental.functionspace import LagrangeFESpace
-from fealpy.experimental.tests.fem.scalar_diffusion_integrator_data import *
-from fealpy.experimental.fem.scalar_diffusion_integrator import ScalarDiffusionIntegrator
+from fealpy.experimental.tests.fem.scalar_mass_integrator_data import *
+from fealpy.experimental.fem.scalar_mass_integrator import ScalarMassIntegrator
 
-class TestScalarDiffusionIntegrator:
+class TestScalarMassIntegrator:
 
     @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
     @pytest.mark.parametrize("data", triangle_mesh_one_box)
@@ -16,7 +16,7 @@ class TestScalarDiffusionIntegrator:
         
         mesh = TriangleMesh.from_box([0,1,0,1],1,1)
         space = LagrangeFESpace(mesh, 2)
-        integrator = ScalarDiffusionIntegrator(1, 3)
+        integrator = ScalarMassIntegrator(1, 3)
         assembly_cell_matrix = integrator.assembly(space)
         np.testing.assert_array_almost_equal(assembly_cell_matrix ,data["assembly_cell_matrix"], 
                                      err_msg=f" `assembly_cell_matrix` function is not equal to real result in backend {backend}")
