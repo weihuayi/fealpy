60f5ebd6eef9a1c01f89074ce2d03c46cf6af5b5
Huayi Wei <weihuayi@xtu.edu.cn>
2024-08-08 11:56:39 +0800

    update

--- fealpy/experimental/tests/fem/test_bilinear_form.py
+++ fealpy/experimental/tests/fem/test_bilinear_form.py
@@ -1,6 +1,29 @@
 import numpy as np
 import pytest
 from fealpy.experimental.backend import backend_manager as bm
-from fealpy.experimental.mesh.triangle_mesh import TriangleMesh
-from fealpy.experimental.fem.bilinear_form import BilinearForm 
-from fealpy.experimental.tests.fem.bilinear_form_data import *
+
+from fealpy.experimental.functionspace import LagrangeFESpace
+from fealpy.experimental.fem import (
+        BilinearForm, ScalarDiffusionIntegrator
+    )
+
+
+class TestBilinearFormInterface:
+    @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
+    @pytest.mark.parametrize("data", mesh_data)
+    @pytest.mark.parametrize("p", range(1, 5))
+    def test_matmul(backend, data):
+        mesh = data["mesh"]
+        node = mesh.node
+        space = LagrangeFESpace(mesh, p)
+        gdof = space.number_of_global_dofs()
+        kwargs = bm.context(node)
+        x = bm.ones(gdof, **kwargs)
+        bform = BilinearForm(space)
+        bform.add_integrator(ScalarDiffusionIntegrator())
+        y = bform @ x
+        np.testing.assert_array_equal(bm.to_numpy(y), data["y"])
+
+
+if __name__ == "__main__":
+    pytest.main(['./test_bilinear_form', '-k', 'test_matmul'])
