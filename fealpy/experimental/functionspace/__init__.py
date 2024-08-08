b370c29c6c1d5a881f96c1c1a14273e2e0a0e43a
chenchunyu <cbtxs@qq.com>
2024-08-08 21:05:07 +0800

    udpate

--- fealpy/experimental/functionspace/__init__.py
+++ fealpy/experimental/functionspace/__init__.py
@@ -6,3 +6,4 @@ from .dofs import LinearMeshCFEDof
 from .lagrange_fe_space import LagrangeFESpace
 from .tensor_space import TensorFunctionSpace
 from .cm_conforming_fe_space import CmConformingFESpace2d
+from .bernstein_fe_space import BernsteinFESpace
