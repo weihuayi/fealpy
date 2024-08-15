5e4cef16b229d0534dd50b4ded1b724ee54d099e
wpx <1143615697@qq.com>
2024-08-15 19:17:31 +0800

    update

--- fealpy/experimental/fem/__init__.py
+++ fealpy/experimental/fem/__init__.py
@@ -8,6 +8,7 @@ from .scalar_diffusion_integrator import ScalarDiffusionIntegrator
 from .scalar_mass_integrator import ScalarMassIntegrator
 from .scalar_convection_integrator import ScalarConvectionIntegrator
 from .linear_elasticity_integrator import LinearElasticityIntegrator
+from .press_work_integrator import PressWorkIntegrator, PressWorkIntegrator1
 
 ### Cell Source
 from .scalar_source_integrator import ScalarSourceIntegrator
