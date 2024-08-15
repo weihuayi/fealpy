9938bec3660d422ebafb5a1825d3ec231cd37a79
2655493031@qq.com <2655493031@qq.com>
2024-08-15 18:24:45 +0800

    update

---
+++ fealpy/experimental/tests/functionspace/test_parametric_lagrange_fe_space.py
@@ -0,0 +1,7 @@
+import ipdb
+import numpy as np
+import pytest
+from fealpy.experimental.backend import backend_manager as bm
+from fealpy.experimental.mesh.triangle_mesh import TriangleMesh
+from fealpy.experimental.functionspace import ParametricLagrangeFESpace
+from fealpy.experimental.tests.functionspace.parametric_lagrange_fe_space_data import *
