62a9a9622a53ae8f1482cdcf95566ec89117d23c
ChaosTHL <2250213115@qq.com>
2024-08-16 17:50:52 +0800

    _

--- fealpy/experimental/fem/semilinear_form.py
+++ fealpy/experimental/fem/semilinear_form.py
@@ -14,6 +14,71 @@ class SemilinearForm(Form):
 
     def _get_sparse_shape(self):
         pass
+    def _assembly_group(self, group: str, retain_ints: bool=False):
+        if group in self.memory:
+            return self.memory[group]
+ 
+        INTS = self.integrators[group]
+        ct = INTS[0](self.space)
+        print(ct)
+        etg = [INTS[0].to_global_dof(s) for s in self._spaces]
+        if isinstance(ct, tuple):
+            # print(INTS[0](self.space)[0].shape, INTS[0](self.space)[1].shape)
+            # print(INTS[1](self.space).shape)
+            ct_A = ct[0]
+            ct_F = ct[1]
+            for int_ in INTS[1:]:
+                # print(int_(self.space).shape)
+                new_ct_A = int_(self.space)
+                new_ct_F = int_(self.space)
+                fdim_A = min(ct_A.ndim, new_ct_A.ndim)
+                fdim_F = min(ct_F.ndim, new_ct_F.ndim)
+                if ct_A.shape[:fdim_A] != new_ct_A.shape[:fdim_A]:
+                    raise RuntimeError(f"The output of the integrator {int_.__class__.__name__} "
+                                    f"has an incompatible shape {tuple(new_ct_A.shape)} "
+                                    f"with the previous {tuple(ct_A.shape)} in the group '{group}'.")
+                if ct_F.shape[:fdim_F] != new_ct_F.shape[:fdim_F]:
+                    raise RuntimeError(f"The output of the integrator {int_.__class__.__name__} "
+                                    f"has an incompatible shape {tuple(new_ct_F.shape)} "
+                                    f"with the previous {tuple(ct_F.shape)} in the group '{group}'.")
+                if new_ct_A.ndim > ct_A.ndim:
+                    ct_A = new_ct_A + ct_A[None, ...]
+                elif new_ct_A.ndim < ct_A.ndim:
+                    ct_A = ct_A + new_ct_A[None, ...]
+                else:
+                    ct_A = ct_A+ new_ct_A
+
+                if new_ct_F.ndim > ct_F.ndim:
+                    ct_F = new_ct_F + ct_F[None, ...]
+                elif new_ct_F.ndim < ct_F.ndim:
+                    ct_F = ct_F + new_ct_F[None, ...]
+                else:
+                    ct_F = ct_F+ new_ct_F
+
+            if retain_ints:
+                self.memory[group] = ((ct_A, etg), (ct_F, etg))
+
+            return (ct_A, etg),(ct_F, etg)
+            
+        else:
+            for int_ in INTS[1:]:
+                new_ct = int_(self.space)
+                fdim = min(ct.ndim, new_ct.ndim)
+                if ct.shape[:fdim] != new_ct.shape[:fdim]:
+                    raise RuntimeError(f"The output of the integrator {int_.__class__.__name__} "
+                                    f"has an incompatible shape {tuple(new_ct.shape)} "
+                                    f"with the previous {tuple(ct.shape)} in the group '{group}'.")
+                if new_ct.ndim > ct.ndim:
+                    ct = new_ct + ct[None, ...]
+                elif new_ct.ndim < ct.ndim:
+                    ct = ct + new_ct[None, ...]
+                else:
+                    ct = ct + new_ct
+
+            if retain_ints:
+                self.memory[group] = (ct, etg)
+
+            return (ct, etg),(ct, etg)
 
     def _scalar_assembly_A(self, retain_ints: bool, batch_size: int):
 
@@ -28,16 +93,14 @@ class SemilinearForm(Form):
             values = bm.empty(init_value_shape, dtype=space[0].ftype),
             spshape = sparse_shape
         )
-
         for group in self.integrators.keys():
-            group_tensor, e2dofs = self._assembly_group(group, retain_ints)
+            group_tensor, e2dofs = self._assembly_group(group, retain_ints)[0]
             ue2dof = e2dofs[0]
             ve2dof = e2dofs[1] if (len(e2dofs) > 1) else ue2dof
             local_shape = group_tensor.shape[-3:] # (NC, vldof, uldof)
 
             if (batch_size > 0) and (group_tensor.ndim == 3): # Case: no batch dimension
                 group_tensor = bm.stack([group_tensor]*batch_size, axis=0)
-
             I = bm.broadcast_to(ve2dof[:, :, None], local_shape)
             J = bm.broadcast_to(ue2dof[:, None, :], local_shape)
             indices = bm.stack([I.ravel(), J.ravel()], axis=0)
@@ -60,7 +123,7 @@ class SemilinearForm(Form):
         )
 
         for group in self.integrators.keys():
-            group_tensor, e2dofs = self._assembly_group(group, retain_ints)
+            group_tensor, e2dofs = self._assembly_group(group, retain_ints)[1]
 
             if (batch_size > 0) and (group_tensor.ndim == 2):
                 group_tensor = bm.stack([group_tensor]*batch_size, axis=0)
