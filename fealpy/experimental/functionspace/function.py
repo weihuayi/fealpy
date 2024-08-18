8b4752a166f5115ebcdd47f9b62f097cce2f0391
ChaosTHL <2250213115@qq.com>
2024-08-18 18:53:10 +0800

    _

--- fealpy/experimental/functionspace/function.py
+++ fealpy/experimental/functionspace/function.py
@@ -50,7 +50,7 @@ class Function(Generic[_FS]):
         return self.__class__(self.space, -self.array)
 
     def __add__(self, other: Union[TensorLike, Number]):
-        return self.__class__(self.space, self.array + other)
+        return self.__class__(self.space, self.array + other, self.coordtype)
     __radd__ = __add__
 
     def __iadd__(self, other: Union[TensorLike, Number]):
