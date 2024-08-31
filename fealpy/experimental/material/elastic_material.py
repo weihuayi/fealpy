c9e3ad57bf7e65a8c64015e9c9ae89d5c5a9d28d
Huayi Wei <weihuayi@xtu.edu.cn>
2024-08-31 16:24:02 +0800

    update

--- fealpy/experimental/material/elastic_material.py
+++ fealpy/experimental/material/elastic_material.py
@@ -5,8 +5,6 @@ from .material_base import MaterialBase
 class ElasticMaterial(MaterialBase):
     def __init__(self, name):
         super().__init__(name)
-        self.set_property('elastic_modulus', None)
-        self.set_property('poisson_ratio', None)
 
     def calculate_shear_modulus(self):
         E = self.get_property('elastic_modulus')
@@ -35,50 +33,40 @@ class ElasticMaterial(MaterialBase):
             raise ValueError("Elastic modulus and Poisson's ratio must be defined.")
 
 class LinearElasticMaterial(ElasticMaterial):
-    def __init__(self, name):
+    def __init__(self, name, 
+            elastic_modulus: float=1, poisson_ratio:float=0.3, 
+            hypo: str = "3D" ):
         super().__init__(name)
+        self.set_property('elastic_modulus', elastic_modulus)
+        self.set_property('poisson_ratio', poisson_ratio)
+        self.hypothesis = hypothesis
 
-    def plane_stress_elastic_matrix(self):
-        """
-        Calculate the elastic matrix D for plane stress condition.
-        """
-        E = self.get_property('elastic_modulus')
-        nu = self.get_property('poisson_ratio')
-        if E is None or nu is None:
-            raise ValueError("Elastic modulus and Poisson's ratio must be defined.")
-
-        D = E / (1 - nu ** 2) * bm.array([[1, nu, 0],
-                                          [nu, 1, 0],
-                                          [0, 0, (1 - nu) / 2]], dtype=bm.float64)
-        return D
-    
-    def plane_strain_elastic_matrix(self):
-        """
-        Calculate the elastic matrix D for plane strain condition.
-        """
         lam = self.calculate_lame_lambda()
         mu = self.calculate_shear_modulus
-        if lam is None or mu is None:
-            raise ValueError("Lame and Shear modulus must be defined.")
 
-        D = bm.tensor([[2 * mu + lam, lam, 0],
-                    [lam, 2 * mu + lam, 0],
-                    [0, 0, mu]], dtype=bm.float64)
-        return D
-    
+        if hypo == "3D":
+            self.D = bm.tensor([[2 * mu + lam, lam, lam, 0, 0, 0],
+                                      [lam, 2 * mu + lam, lam, 0, 0, 0],
+                                      [lam, lam, 2 * mu + lam, 0, 0, 0],
+                                      [0, 0, 0, mu, 0, 0],
+                                      [0, 0, 0, 0, mu, 0],
+                                      [0, 0, 0, 0, 0, mu]], dtype=bm.float64)
+        elif hypo == "plane_stress":
+            E = self.get_property('elastic_modulus')
+            nu = self.get_property('poisson_ratio')
+            self.D = E / (1 - nu ** 2) * bm.array([
+                [1, nu, 0], 
+                [nu, 1, 0], 
+                [0, 0, (1 - nu) / 2]], dtype=bm.float64)
+        elif hypo == "plane_strain":
+            self.D = bm.tensor([[2 * mu + lam, lam, 0],
+                        [lam, 2 * mu + lam, 0],
+                        [0, 0, mu]], dtype=bm.float64)
+        else:
+            raise ""
+
     def elastic_matrix(self):
         """
         Calculate the elastic matrix D for three-dimensional elasticity.
         """
-        lam = self.calculate_lame_lambda()
-        mu = self.calculate_shear_modulus
-        if lam is None or mu is None:
-            raise ValueError("Lame and Shear modulus must be defined.")
-
-        D = bm.tensor([[2 * mu + lam, lam, lam, 0, 0, 0],
-                                  [lam, 2 * mu + lam, lam, 0, 0, 0],
-                                  [lam, lam, 2 * mu + lam, 0, 0, 0],
-                                  [0, 0, 0, mu, 0, 0],
-                                  [0, 0, 0, 0, mu, 0],
-                                  [0, 0, 0, 0, 0, mu]], dtype=bm.float64)
-        return D
+        return self.D[None, None, ...]
