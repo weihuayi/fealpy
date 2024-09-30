e8da6f573c2dd1a14c1651be9354fd17af2e21f2
BenHBLiu <benlau0603@outlook.com>
2024-09-30 17:45:37 +0800

    update

--- app/TSP/main.py
+++ app/TSP/main.py
@@ -4,8 +4,10 @@ from fealpy.experimental.opt import SnowmeltOptAlg
 from fealpy.experimental.opt import ParticleSwarmOptAlg
 from app.TSP.TSP_data import TSPdata
 from app.TSP.TSP_citys import TravellingSalesmanProblem
-from app.TSP.TSP_citys import gbest2route, soler_tsp_with_algorithm, calD, printroute
+from app.TSP.TSP_citys import gbest2route, soler_tsp_with_algorithm, printroute
 import time
+import matplotlib.pyplot as plt
+
 # bm.set_backend('pytorch')
 
 start_time = time.perf_counter()
@@ -18,8 +20,6 @@ num = 0
 citys = TSPdata[num]['citys']()
 test = TravellingSalesmanProblem(citys)
 test.calD()
-# D = calD(citys)
-# test = TravellingSalesmanProblem(citys, D)
 fobj = lambda x: test.fitness(x)
 NP = 100
 lb, ub = (0, 1)
@@ -28,19 +28,13 @@ qpso_gbest, qpso_gbest_f = soler_tsp_with_algorithm(qpso_optimizer, fobj, lb, ub
 pso_gbest, pso_gbest_f = soler_tsp_with_algorithm(pso_optimizer, fobj, lb, ub, NP, citys.shape[0])
 sao_gbest, sao_gbest_f = soler_tsp_with_algorithm(sao_optimizer, fobj, lb, ub, NP, citys.shape[0])
 
-qpso_route, qpso_route_citys = gbest2route(qpso_gbest, citys)
-pso_route, pso_route_citys = gbest2route(pso_gbest, citys)
-sao_route, sao_route_citys = gbest2route(sao_gbest, citys)
+qpso_route, qpso_route_citys = gbest2route(qpso_gbest, qpso_gbest_f, citys, 'QPSO')
+pso_route, pso_route_citys = gbest2route(pso_gbest, pso_gbest_f, citys, 'PSO')
+sao_route, sao_route_citys = gbest2route(sao_gbest, sao_gbest_f, citys, 'SAO')
 
 end_time = time.perf_counter()
 running_time = end_time - start_time
-print('The best solution obtained by QPSO is:', qpso_route)
-print('The best optimal value of the objective function found by QPSO is:', qpso_gbest_f)
-print('The best solution obtained by PSO is:', pso_route)
-print('The best optimal value of the objective function found by PSO is:', pso_gbest_f)
-print('The best solution obtained by PSO is:', sao_route)
-print('The best optimal value of the objective function found by PSO is:', sao_gbest_f)
-print("Running time: ", running_time)
+
 
 route_citys = bm.array([qpso_route_citys, 
                   pso_route_citys,
@@ -51,4 +45,4 @@ alg_name = ['QPSO',
             'SAO']
 
 printroute(route_citys, citys, alg_name)
-
+plt.show()
