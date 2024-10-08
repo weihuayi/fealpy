f21bd758d547e1964d5ecd2edb4af85de5aafadb
BenHBLiu <benlau0603@outlook.com>
2024-10-08 17:34:47 +0800

    update

--- fealpy/experimental/tests/opt/test_iopt_alg.py
+++ fealpy/experimental/tests/opt/test_iopt_alg.py
@@ -5,6 +5,7 @@ from fealpy.experimental.opt import HoneybadgerOptAlg
 from fealpy.experimental.opt import QuantumParticleSwarmOptAlg 
 from fealpy.experimental.opt import SnowmeltOptAlg 
 from fealpy.experimental.opt import GreyWolfOptimizer
+from fealpy.experimental.opt import HippopotamusOptAlg
 from fealpy.experimental.tests.opt.iopt_data import iopt_data
 from fealpy.experimental.opt.optimizer_base import Optimizer, opt_alg_options
 
@@ -54,6 +55,17 @@ class TestIOptInterfaces:
         optimizer = SnowmeltOptAlg(option)
         gbest, gbest_f = optimizer.run()
 
+    @pytest.mark.parametrize("backend", ['numpy'])
+    @pytest.mark.parametrize("data", iopt_data)
+    @pytest.mark.parametrize("NP", [100])
+    def test_hippopotamus_optimizer(self, backend, data, NP):
+        bm.set_backend(backend)
+        lb, ub = data['domain']
+        x0 = lb + bm.random.rand(NP, data['ndim'])*(ub - lb)
+        option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
+        optimizer = HippopotamusOptAlg(option)
+        optimizer.run()
+
     @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
     @pytest.mark.parametrize("data", iopt_data)
     @pytest.mark.parametrize("NP", [100])
@@ -66,10 +78,10 @@ class TestIOptInterfaces:
         gbest, gbest_f = optimizer.run()
     
 if __name__ == "__main__":
-    pytest.main(["./test_iopt_alg.py", "-k", "test_honeybadger_opt_alg"])
-    pytest.main(["./test_iopt_alg.py", "-k", "test_crayfish_opt_alg"])
-    pytest.main(["./test_iopt_alg.py", "-k", "test_quantumparticleswarm_opt_alg"])
-    pytest.main(["./test_iopt_alg.py", "-k", "test_grey_wolf_optimizer"])
-    pytest.main(["./test_iopt_alg.py", "-k", "test_snowmelt_opt_alg"])
-
+    # pytest.main(["./test_iopt_alg.py", "-k", "test_honeybadger_opt_alg"])
+    # pytest.main(["./test_iopt_alg.py", "-k", "test_crayfish_opt_alg"])
+    # pytest.main(["./test_iopt_alg.py", "-k", "test_quantumparticleswarm_opt_alg"])
+    # pytest.main(["./test_iopt_alg.py", "-k", "test_grey_wolf_optimizer"])
+    # pytest.main(["./test_iopt_alg.py", "-k", "test_snowmelt_opt_alg"])
+    pytest.main(["./test_iopt_alg.py", "-k", "test_hippopotamus_optimizer"])
     
