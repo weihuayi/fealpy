50d39596d579f614f7266a2f6f3432333810af00
BenHBLiu <benlau0603@outlook.com>
2024-10-17 19:42:17 +0800

    update

--- fealpy/opt/__init__.py
+++ fealpy/opt/__init__.py
@@ -10,3 +10,4 @@ from .snowmelt_opt_alg import SnowmeltOptAlg
 from .grey_wolf_optimizer import GreyWolfOptimizer
 from .particle_swarm_opt import ParticleSwarmOptAlg
 from .hippopotamus_opt_alg import HippopotamusOptAlg
+from .Antcolony_opt_alg import AntColonyOptAlg
