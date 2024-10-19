fba5a617bbce92d8c74ce8213ea89682cc83cb4a
brighthe98@gmail.com <brighthe98@gmail.com>
2024-10-19 21:06:59 +0800

    add pde in soptx

--- app/soptx/soptx/filter/filter_properties.py
+++ app/soptx/soptx/filter/filter_properties.py
@@ -10,11 +10,6 @@ class FilterProperties:
     def __init__(self, mesh, rmin: float, ft: int):
         """
         Initialize the filter properties based on the mesh.
-
-        Args:
-            mesh: The mesh object which contains information about the grid.
-            rmin (float): The filter radius, which controls the minimum feature size.
-            ft (int): The filter type, 0 for sensitivity filter, 1 for density filter.
         """
         self.ft = ft
 
