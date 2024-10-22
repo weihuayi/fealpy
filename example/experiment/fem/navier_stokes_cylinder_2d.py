7658917325635d9a83e54225c5bed7d05fa274d6
wpx <1143615697@qq.com>
2024-10-22 10:47:25 +0800

    update

--- example/experiment/fem/navier_stokes_cylinder_2d.py
+++ example/experiment/fem/navier_stokes_cylinder_2d.py
@@ -8,7 +8,6 @@
 	@ref 
 '''  
 from fealpy.backend import backend_manager as bm
-from fealpy.mesh import TriangleMesh
 from fealpy.functionspace import LagrangeFESpace
 from fealpy.functionspace import TensorFunctionSpace
 from fealpy.fem import (
@@ -18,23 +17,20 @@ from fealpy.fem import (
 
 from fealpy.fem import LinearForm, ScalarSourceIntegrator
 from fealpy.fem import DirichletBC
-from fealpy.sparse import COOTensor
-from fealpy.fem import VectorSourceIntegrator
 from fealpy.fem import LinearBlockForm, BlockForm
-from fealpy.solver import spsolve 
+from fealpy.solver import spsolve, cg 
 
 from fealpy.pde.navier_stokes_equation_2d import FlowPastCylinder
-from fealpy.decorator import barycentric, cartesian
 from fealpy.old.timeintegratoralg import UniformTimeLine
 from fealpy.fem import DirichletBC
+from fealpy.utils import timer
 
-from fealpy import logger
 #TODO:mesh.nodedata对tensorspace的情况
 
 backend = 'pytorch'
 #backend = 'numpy'
-device = 'cuda'
-#device = 'cpu'
+#device = 'cuda'
+device = 'cpu'
 bm.set_backend(backend)
 bm.set_default_device(device)
 
@@ -48,9 +44,11 @@ pde = FlowPastCylinder()
 rho = pde.rho
 mu = pde.mu
 
-mesh = pde.mesh(0.01, device = device)
+mesh = pde.mesh(0.05, device = device)
 timeline = UniformTimeLine(0, T, nt)
 dt = timeline.dt
+tmr = timer()
+next(tmr)
 
 pspace = LagrangeFESpace(mesh, p=pdegree)
 space = LagrangeFESpace(mesh, p=udegree)
@@ -74,38 +72,43 @@ P_bform = BilinearForm((pspace, uspace))
 P_bform.add_integrator(PressWorkIntegrator(-1, q=q))
 
 A_bform = BilinearForm(uspace)
-A_bform.add_integrator(ScalarMassIntegrator(rho/dt, q=q))
-A_bform.add_integrator(ScalarDiffusionIntegrator(mu, q=q)) 
-ConvectionIntegrator = ScalarConvectionIntegrator(q=q)
-A_bform.add_integrator(ConvectionIntegrator)
+M = ScalarMassIntegrator(rho/dt, q=q)
+S = ScalarDiffusionIntegrator(mu, q=q)
+D = ScalarConvectionIntegrator(rho, q=q)
+A_bform.add_integrator([M,S,D])
 
 ##LinearForm
 ulform = LinearForm(uspace)
-SourceIntegrator = ScalarSourceIntegrator(q = q)
-ulform.add_integrator(SourceIntegrator)
+f = ScalarSourceIntegrator(q = q)
+ulform.add_integrator(f)
 plform = LinearForm(pspace)
-
+tmr.send('网格和pde生成时间')
 print(f"总共自由度为:{gdof}")
-for i in range(10):
+
+for i in range(5):
     t1 = timeline.next_time_level()
     print("time=", t1)
 
-    ConvectionIntegrator.coef = u0
-    ConvectionIntegrator.clear()
+    tmr.send("其他") 
+    D.coef = u0
+    D.clear()
     A = BlockForm([[A_bform, P_bform],
                    [P_bform.T, None]])
     A = A.assembly()
     
-    SourceIntegrator.source = u0
-    SourceIntegrator.clear() 
+    f.source = u0
+    f.clear() 
     b = LinearBlockForm([ulform, plform]).assembly()
+    tmr.send("组装") 
     
     A,b = DirichletBC((uspace,pspace), gd=(pde.u_dirichlet, pde.p_dirichlet), 
                       threshold=(pde.is_u_boundary, pde.is_p_boundary), method='interp').apply(A, b)
 
     
+    tmr.send("边界处理") 
     x = spsolve(A, b, 'mumps')
-    
+    #x = cg(A, b)
+    tmr.send("求解") 
     u1[:] = x[:ugdof]
     p1[:] = x[ugdof:]
 
@@ -117,4 +120,5 @@ for i in range(10):
         
     u0[:] = u1[:] 
     timeline.advance()
+next(tmr)
 print(bm.sum(bm.abs(u1[:])))
