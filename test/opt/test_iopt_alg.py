cf0b2e9fb063adaa882c028455209bae0f0842b2
BenHBLiu <benlau0603@outlook.com>
2024-11-05 17:11:36 +0800

    updata BKA

--- test/opt/test_iopt_alg.py
+++ test/opt/test_iopt_alg.py
@@ -8,6 +8,7 @@ from fealpy.opt import SnowmeltOptAlg
 from fealpy.opt import GreyWolfOptimizer
 from fealpy.opt import HippopotamusOptAlg
 from fealpy.opt import CrestedPorcupineOpt
+from fealpy.opt import BlackwingedKiteAlg
 from fealpy.opt import initialize
 from fealpy.opt.optimizer_base import Optimizer, opt_alg_options
 
@@ -92,6 +93,17 @@ class TestIOptInterfaces:
         option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
         optimizer = CrestedPorcupineOpt(option)
         gbest, gbest_f = optimizer.run()
+
+    @pytest.mark.parametrize("backend", ['numpy', 'pytorch'])
+    @pytest.mark.parametrize("data", iopt_data)
+    @pytest.mark.parametrize("NP", [130])
+    def test_black_winged_kite_alg(self, backend, data, NP):
+        bm.set_backend(backend)
+        lb, ub = data['domain']
+        x0 = initialize(NP, data['ndim'], ub, lb)
+        option = opt_alg_options( x0, data['objective'], data['domain'] , NP)
+        optimizer = BlackwingedKiteAlg(option)
+        gbest, gbest_f = optimizer.run()
     
 if __name__ == "__main__":
     pytest.main(["./test_iopt_alg.py", "-k", "test_honeybadger_opt_alg"])
@@ -101,3 +113,4 @@ if __name__ == "__main__":
     pytest.main(["./test_iopt_alg.py", "-k", "test_snowmelt_opt_alg"])
     pytest.main(["./test_iopt_alg.py", "-k", "test_hippopotamus_optimizer"])
     pytest.main(["./test_iopt_alg.py", "-k", "test_crested_porcupine_opt"])
+    pytest.main(["./test_iopt_alg.py", "-k", "test_black_winged_kite_alg"])
