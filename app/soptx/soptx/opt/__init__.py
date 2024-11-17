3f2944e9df137bea8bfa8d5784ba0a5ffbb39aed
brighthe98@gmail.com <brighthe98@gmail.com>
2024-11-17 22:36:58 +0800

    reconfiguration soptx

--- app/soptx/soptx/opt/__init__.py
+++ app/soptx/soptx/opt/__init__.py
@@ -1,9 +1,13 @@
-from .base import ObjectiveBase
+from .base import ObjectiveBase, ConstraintBase, OptimizerBase
 from .compliance import ComplianceObjective
 from .volume import VolumeConstraint
+from .oc import OCOptimizer
 
 __all__ = [
     'ObjectiveBase',
+    'ConstraintBase',
+    'OptimizerBase',
     'ComplianceObjective',
     'VolumeConstraint'
+    'OCOptimizer'
 ]
