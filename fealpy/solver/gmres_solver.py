d65060dc6d925163dbd2ecebee093a964fd24045
chenchunyu <cbtxs@qq.com>
2024-11-19 15:46:38 +0800

    update fracturex

--- fealpy/solver/gmres_solver.py
+++ fealpy/solver/gmres_solver.py
@@ -15,7 +15,7 @@ def _to_cupy_data(A, b, x0):
     if isinstance(A.indices(), np.ndarray): # numpy backend
         A =  A.to_scipy() 
         A = cp.sparse.csr_matrix(A.astype(cp.float64))
-    elif A.indices().device.type == "cpu": # torch backend
+    elif bm.device_type(A.indices()) == "cpu": # torch backend
         A = A.device_put("cuda")
         indices = cp.from_dlpack(A.indices())
         data = cp.from_dlpack(A.values())
@@ -25,14 +25,16 @@ def _to_cupy_data(A, b, x0):
         data = cp.from_dlpack(A.values())
         A = cp.sparse.csr_matrix((data, (indices[0], indices[1])), shape=A.shape)
 
-    if isinstance(b, np.ndarray) or b.device.type == "cpu":
+    if isinstance(b, np.ndarray) or bm.device_type(b) == "cpu":
         b = bm.to_numpy(b)
         b = cp.array(b)
-        x0 = bm.to_numpy(x0)
-        x0 = cp.array(x0)
+        if x0 is not None:
+            x0 = bm.to_numpy(x0)
+            x0 = cp.array(x0)
     else:
         b = cp.from_dlpack(b)
-        x0 = cp.from_dlpack(x0)
+        if x0 is not None:
+            x0 = cp.from_dlpack(x0)
     return A, b, x0
 
 
@@ -50,14 +52,14 @@ def _cupy_solve(A, b, tol, x0, maxiter ,atol):
     import cupy as cp
     import cupyx.scipy.sparse.linalg as cpx
 
-    iscpu = isinstance(b, np.ndarray) or b.device.type == "cpu"
+    iscpu = isinstance(b, np.ndarray) or bm.device_type(b) == "cpu"
     A, b, x0 = _to_cupy_data(A, b, x0)
     x, info = cpx.gmres(A, b, tol=tol, x0=x0, maxiter=maxiter, atol=atol)
     if iscpu:
         x = cp.asnumpy(x)
     return x
 
-def _scipy_solve(A, b, tol, x0, maxiter ,atol):
+def _scipy_solve(A, b, tol, x0, maxiter, atol):
     from scipy.sparse.linalg import gmres 
     from scipy.sparse import csr_matrix
 
