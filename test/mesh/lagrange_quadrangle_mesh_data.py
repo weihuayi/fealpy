38c21b884c95bee8acd603d787762ea16825af54
BellaLq <2655493031@qq.com>
2024-11-25 16:13:43 +0800

    update

---
+++ test/mesh/lagrange_quadrangle_mesh_data.py
@@ -0,0 +1,21 @@
+import numpy as np
+from fealpy.geometry.implicit_surface import SphereSurface
+from fealpy.mesh.quadrangle_mesh import QuadrangleMesh
+
+# 定义多个典型的 LagrangeTriangleMesh 对象
+surface = SphereSurface() #以原点为球心，1 为半径的球
+mesh = QuadrangleMesh.from_unit_sphere_surface()
+node = mesh.interpolation_points(3)
+cell = mesh.cell_to_ipoint(3)
+
+from_triangle_mesh_data = [
+        {
+            "p": 3,
+            "surface": surface,
+            "cell": np.array(dtype=np.int32),
+            "NN": 
+            "NE": 
+            "NF": 
+            "NC": 
+                }
+]
