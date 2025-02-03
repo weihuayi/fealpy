b8ba1f9792ffda7227467e39ee5aaecf75aa4fee
Huayi Wei <weihuayi@xtu.edu.cn>
2025-02-03 15:11:38 +0800

    update

---
+++ fealpy/geometry/domain_2d.py
@@ -0,0 +1,59 @@
+
+from .sizing_function import huniform
+from .signed_distance_function import dmin, dcircle, drectangle, ddiff
+from .geoalg import project
+from .implicit_curve import CircleCurve
+from .domain import Domain
+
+class LShapeDomain(Domain):
+    def __init__(self, hmin=0.1, hmax=0.1, fh=None):
+        """
+        """
+        super().__init__(hmin=hmin, hmax=hmax, GD=2)
+        if fh is not None:
+            self.fh = fh
+
+        self.box = [-1.0, 1.0, -1.0, 1.0] 
+        self.fh = fh
+        vertices = bm.array([
+            (-1.0, -1.0),
+            ( 0.0, -1.0),
+            ( 0.0,  0.0),
+            ( 1.0,  0.0),
+            ( 1.0,  1.0),
+            (-1.0,  1.0)
+            ], dtype=bm.float64)
+
+        curves = bm.array([
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+            (5, 0)
+            ], dtype=bm.int32)
+
+        self.facets = {0:vertices, 1:curves}
+
+    def __call__(self, p):
+        """
+        @brief 符号距离函数
+        """
+        d0 = drectangle(p, [-1.0, 1.0, -1.0, 1.0])
+        d1 = drectangle(p, [ 0.0, 1.0, -1.0, 0.0])
+        return ddiff(d0, d1)
+
+    def signed_dist_function(self, p):
+        return self(p) 
+
+    def sizing_function(self, p):
+        return self.fh(p, self)
+
+    def facet(self, dim):
+        return self.facets[dim]
+
+    def meshing_facet_0d(self):
+        return self.facets[0]
+
+    def meshing_facet_1d(self, hmin, fh=None):
+        pass
