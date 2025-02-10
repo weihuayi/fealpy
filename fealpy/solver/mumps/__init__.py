b0c8e35d777bc3af64e8b704e82bc2493c153ecb
Mihe0904 <1614827225@qq.com>
2025-02-10 09:58:38 +0800

    <1.poisson 2.solver/gamg 3.pcg 4.问题：效率 5.待完成：gamg作为预处理子的功能完善>

--- fealpy/solver/mumps/__init__.py
+++ fealpy/solver/mumps/__init__.py
@@ -1,22 +1,17 @@
 from .dmumps_context import DMumpsContext
 from .mumps_solver import MumpsSolver
 
-def spsolve(A, b, comm=None):
-    """Sparse solve A\b."""
+def spsolve(A, b):
+    """Sparse solve A\b.
+    """
 
-    assert A.dtype == 'd' and b.dtype == 'd', "Only double precision supported."
-    with DMumpsContext(par=par, sym=0, comm=comm) as ctx:
-        if ctx.myid == 0:
-            # Set the sparse matrix -- only necessary on
-            ctx.set_centralized_sparse(A)
-            x = b.copy()
-            ctx.set_rhs(x)
+    assert A.dtype == 'd' and b.dtype == 'd', "Now only double precision supported."
+    s = MumpsSolver()
+    return s.solve(A, b)
 
-        # Silence most messages
-        ctx.set_silent()
-
-        # Analysis + Factorization + Solve
-        ctx.run(job=6)
-
-        if ctx.myid == 0:
-            return x
+def spsolve_triangular(A, b, transpose=False):
+    """Solve sparse triangular system Ax=b or A^Tx=b.
+    """
+    assert A.dtype == 'd' and b.dtype == 'd', "Now only double precision supported."
+    s = MumpsSolver()
+    return s.solve_triangular(A, b, transpose=transpose)
--- fealpy/solver/mumps/mumps_solver.py
+++ fealpy/solver/mumps/mumps_solver.py
@@ -6,17 +6,55 @@ _solvers ={
         }
 
 class MumpsSolver:
-    def __init__(self, stype='D', par=1, comm=None, timer=None, logger=None):
+    def __init__(self, stype='D', 
+                 par=1, sym=0, comm=None, 
+                 slient=True, timer=None, logger=None):
         """Initialize the MumpsSolver object.
+
+        Note
+            1. COMMCOMM (integer) must be set by the user on all processors 
+               before the initialization phase (JOB= –1) and
+               must not be changed in further calls 
+            2. PAR (integer) must be initialized by the user on all processors 
+               before the initialization phase (JOB= –1) and is accessed by 
+               MUMPS only during this phase. 
+               0: the host is not involved in the parallel steps of the factorization and solve phases.
+               1: the host is also involved in the parallel steps of the factorization and solve phases.
+            3. SYM (integer) must be initialized by the user on all processors 
+               before the initialization phase (JOB= –1) and is accessed by 
+               MUMPS only during this phase. 
+               0: unsym; 1: spd; 2 symmetric
+            4. ICNTL(5) controls the matrix input format. 
+               0: assembled format (centralized on the host or distributed)
+               1: elemental format
+            5. ICNTL(18): controls the options to distribute the matrix
+            9. ICNTL(9): compute the solution using A or A^T.
         """
         self.timer = timer
         self.logger = logger
-        self.context = _solvers[stype](par=par, comm=comm)
+        self.context = _solvers[stype](par=par, sym=sym, comm=comm)
+        if slient:
+            self.context.set_silent()
 
-    def solve(self, A, b, sym=0, job=6, lower=None):
-        self.context.id.sym = sym
+    def solve(self, A, b):
+        """
+        """
         x = b.copy()
-        self.context.set_matrix(A)
+        self.context.set_centralized_sparse(A)
         self.context.set_rhs(x)
-        self.context.run(job)
+
+        self.context.run(6)
+
         return x 
+
+    def solve_triangular(self, A, b, transpose=False):
+        """Solve triangular system Ax=b or A^Tx=b 
+
+        Parameters:
+            A (CSRTensor): the upper or lower triangular matrix 
+            b (TensorLike): the right hand side
+            transpose (bool): the flag for 
+        """
+        if transpose: # ICNTL(9) compute the solution using A or A^T
+            self.context.set_icntl(9, 2) # solve A^T x = b
+        return self.solve(A, b)
