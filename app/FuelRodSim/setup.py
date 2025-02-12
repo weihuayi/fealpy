8d1b2eed89095a93a8dcb3818048cd75ee78083f
Liujiawangmath <15674760037@163.com>
2025-02-12 09:17:26 +0800

    添加软件的setup

---
+++ app/FuelRodSim/setup.py
@@ -0,0 +1,16 @@
+import os
+import pathlib
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="fuelrodsim",
+    description="Fuel rod simulation",
+    url="",
+    author="Jovan",
+    author_email="",
+    license="GNU",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
