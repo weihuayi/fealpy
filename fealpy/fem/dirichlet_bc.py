6df70c07b7fcef8221a1b62faef330fe79359241
wpx <1143615697@qq.com>
2025-02-20 15:11:15 +0800

    delete backup code

--- fealpy/fem/dirichlet_bc.py
+++ fealpy/fem/dirichlet_bc.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple, Callable, Union, TypeVar
 
 from ..backend import backend_manager as bm
 from ..typing import TensorLike
-from ..sparse import SparseTensor, COOTensor, CSRTensor
+from ..sparse import SparseTensor, COOTensor, CSRTensor, spdiags
 from ..functionspace.space import FunctionSpace
 
 CoefLike = Union[float, int, TensorLike, Callable[..., TensorLike]]
@@ -134,61 +134,12 @@ class DirichletBC():
         A = self.check_matrix(matrix) if check else matrix
         isDDof = self.is_boundary_dof
         kwargs = A.values_context()
-        bdIdx = np.zeros(A.shape[0], dtype=**kwargs)
+        bdIdx = bm.zeros(A.shape[0], **kwargs)
         bdIdx[isDDof.reshape(-1)] = 1
         D0 = spdiags(1-bdIdx, 0, A.shape[0], A.shape[0])
         D1 = spdiags(bdIdx, 0, A.shape[0], A.shape[0])
         A = D0@A@D0 + D1
         return A
-        '''
-        if isinstance(A, COOTensor):
-            indices = A.indices
-            remove_flag = bm.logical_or(
-                isDDof[indices[0, :]], isDDof[indices[1, :]]
-            )
-            retain_flag = bm.logical_not(remove_flag)
-            new_indices = indices[:, retain_flag]
-            new_values = A.values[..., retain_flag]
-            A = COOTensor(new_indices, new_values, A.sparse_shape)
-
-            index = bm.nonzero(isDDof)[0]
-            shape = new_values.shape[:-1] + (len(index), )
-            one_values = bm.ones(shape, **kwargs)
-            one_indices = bm.stack([index, index], axis=0)
-            A1 = COOTensor(one_indices, one_values, A.sparse_shape)
-            return A.add(A1).coalesce()
-
-        elif isinstance(A, CSRTensor):
-            from ..sparse.ops import spdiags
-            isDDof = bm.astype(isDDof, A.ftype)
-            D0 = spdiags(1-isDDof, 0, A.shape[0], A.shape[0])
-            D1 = spdiags(isDDof, 0, A.shape[0], A.shape[0], format='coo')
-            A = (D0@A@D0).tocoo() + D1
-            
-            nnz_per_row = crow[1:] - crow[:-1]
-            remain_flag = bm.repeat(isIDof, nnz_per_row) & isIDof[col] # 保留行列均为内部自由度的非零元素
-            rm_cumsum = bm.concat([ZERO, bm.cumsum(remain_flag, axis=0)], axis=0) # 被保留的非零元素数量累积
-            nnz_per_row = rm_cumsum[crow[1:]] - rm_cumsum[crow[:-1]] + isDDof # 计算每行的非零元素数量
-
-            new_crow = bm.cumsum(bm.concat([ZERO, nnz_per_row], axis=0), axis=0)
-
-            NNZ = new_crow[-1]
-            non_diag = bm.ones((NNZ,), dtype=bm.bool, device=bm.get_device(isDDof)) # Field: non-zero elements
-            loc_flag = bm.logical_and(new_crow[:-1] < NNZ, isDDof)
-            non_diag = bm.set_at(non_diag, new_crow[:-1][loc_flag], False)
-
-            new_col = bm.empty((NNZ,), **indices_context)
-            new_col = bm.set_at(new_col, new_crow[:-1][loc_flag], self.boundary_dof_index)
-            new_col = bm.set_at(new_col, non_diag, col[remain_flag])
-
-            new_values = bm.empty((NNZ,), **kwargs)
-            new_values = bm.set_at(new_values, new_crow[:-1][loc_flag], 1.)
-            new_values = bm.set_at(new_values, non_diag, A.values[remain_flag])
-
-            # A = CSRTensor(new_crow, new_col, new_values)
-            A = CSRTensor(new_crow, new_col, new_values, A.sparse_shape)
-        return A
-        '''
 
     def apply_vector(self, vector: TensorLike, matrix: SparseTensor,
                      uh: Optional[TensorLike]=None,
