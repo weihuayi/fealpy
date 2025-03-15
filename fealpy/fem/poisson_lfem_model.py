7c439359e2b405fd4482d266eaca116e79ac2aab
Huayi Wei <weihuayi@xtu.edu.cn>
2025-03-15 10:53:06 +0800

    update

---
+++ fealpy/fem/poisson_lfem_model.py
@@ -0,0 +1,20 @@
+
+
+class PoissonLFEMModel:
+
+    def ___init__(self):
+        pass
+
+
+    def direct_solve(self):
+        pass
+
+    def amg_solve(self):
+        pass
+
+    def gamg_solve(self):
+        pass
+
+
+    def bisect_solve(self):
+        pass
