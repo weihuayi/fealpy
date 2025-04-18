2a43de034d91809c0a7ffdf4c99f56ba5c441879
wpx <1143615697@qq.com>
2025-04-18 12:48:57 +0800

    update

---
+++ fealpy/cfd/simulation/fem/project_method.py
@@ -0,0 +1,31 @@
+from fealpy.backend import backend_manager as bm
+from abc import ABC, abstractmethod
+
+class ProjectionMethod(ABC):
+    """投影算法抽象基类"""
+    def __init__(self):
+        """
+        :param fem_solver: 关联的FEM主求解器实例
+        """
+        self.validate_dependencies()
+
+    def validate_dependencies(self):
+        """验证必要的属性和空间"""
+        required_attrs = ['_uspace', '_pspace']
+        for attr in required_attrs:
+            if not hasattr(self, attr):
+                raise AttributeError(f"FEM solver缺失必要属性: {attr}")
+    @abstractmethod
+    def predict_velocity(self):
+        """速度预测步"""
+        pass
+    
+    @abstractmethod
+    def pressure(self):
+        pass
+    
+    @abstractmethod
+    def correct_velocity(self):
+        """速度校正步"""
+        pass
+
