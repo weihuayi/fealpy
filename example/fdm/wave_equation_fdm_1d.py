a808eff57a719a78b1aceb59491c5cb69e9ceaf4
2655493031@qq.com <2655493031@qq.com>
2025-05-07 21:00:55 +0800

    update

--- example/fdm/wave_equation_fdm_1d.py
+++ example/fdm/wave_equation_fdm_1d.py
@@ -7,19 +7,20 @@ from fealpy.mesh import UniformMesh
 from fealpy.fdm.wave_operator import WaveOperator
 from fealpy.fdm.dirichlet_bc import DirichletBC
 from fealpy.solver import spsolve
+from functools import partial
 
 pde = StringOscillationPDEData()
 
 # 空间离散
 domain = pde.domain()
-nx = 100
+nx = 400
 hx = (domain[1] - domain[0])/nx
 extent = [0, nx]
 mesh = UniformMesh(domain, extent)
 
 # 时间离散
 duration = pde.duration()
-nt = 1000
+nt = 500
 tau = (duration[1] - duration[0])/nt
 
 # 准备初值
@@ -38,10 +39,9 @@ def advance_explicit(n, *frags):
     if n == 0:
         return uh0, t
     elif n == 1:
-        rx = tau/hx 
+        rx = tau/hx
         uh1[1:-1] = rx**2*(uh0[0:-2] + uh0[2:])/2.0 + (1-rx**2)*uh0[1:-1] + tau*vh0[1:-1]
         gD = lambda p: pde.dirichlet(p, t)
-        mesh.update_dirichlet_bc(gD, uh1)
         return uh1, t
     else:
         wave_operator = WaveOperator(mesh)
@@ -56,26 +56,50 @@ def advance_explicit(n, *frags):
         uh1[:] = uh2
         
         return uh1, t
-
-for i in range(nt+1):
-    u, t = advance_explicit(i)
-    if t in [0.5, 1.0, 1.5, 2.0]:
-        #fig, axes = plt.subplots(2, 1)
-        #x = mesh.entity('node').reshape(-1)
-        #true_solution = pde.solution(x, t)
-        # 计算误差
-        # E = mesh.error(true_solution, u)
-        solution = lambda p: pde.solution(p, t + tau)
-        e = mesh.error(solution, errortype='l2')
-        print(f"the error is {e}")
-        #error = true_solution - u
-        #print(f"At time {t}, Error: {error}")
 '''
-uh1, t = advance_explicit(nt)
-'''
-'''
-box = [0, 1, -0.1, 0.1]
+box = [0, 1, -2, 2]
 fig, axes = plt.subplots()
 mesh.show_animation(fig, axes, box, advance_explicit, fname='explicit.mp4', frames=nt+1)
 plt.show()
 '''
+
+#隐格式
+def advance_implicit(n, theta=0.25, *frags):
+    """
+    @brief 时间步进格式为隐格式
+
+    @param[in] n int, 表示第 n 个时间步（当前时间步） 
+    """
+    t = duration[0] + n*tau
+    if n == 0:
+        return uh0, t
+    elif n == 1:
+        rx = tau/hx 
+        uh1[1:-1] = rx**2*(uh0[0:-2] + uh0[2:])/2.0 + (1-rx**2)*uh0[1:-1] + tau*vh0[1:-1]
+        gD = lambda p: pde.dirichlet(p, t)
+        mesh.update_dirichlet_bc(gD, uh1)
+        return uh1, t
+    else:
+        wave_operator = WaveOperator(mesh)
+        A0, (A1, A2) = wave_operator.implicit_assembly(tau=tau, theta=theta)
+        source = lambda p: pde.source(p, t + tau)
+        f = mesh.interpolate(source)
+        f *= tau**2
+        f += A1@uh1 + A2@uh0
+
+        uh0[:] = uh1[:]
+        gD = lambda p: pde.dirichlet(p, t + tau)
+
+        dirichlet_bc = DirichletBC(mesh, gD)
+        A0, f = dirichlet_bc.apply(A0, f)
+        uh1[:] = spsolve(A0, f)
+            
+        return uh1, t
+
+
+advance_implicit_theta = partial(advance_implicit, theta=0.25)
+
+box = [0, 1, -2, 2]
+fig, axes = plt.subplots()
+mesh.show_animation(fig, axes, box, advance_implicit_theta, fname='implicit.mp4', frames=nt+1)
+plt.show()
