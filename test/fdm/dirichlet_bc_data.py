3255ef75a32e7c11d519395fc415fde0f3e57593
zhr-edwin <2533537314@qq.com>
2025-05-20 11:31:18 +0800

    modify dirichlet_bc and test

--- test/fdm/dirichlet_bc_data.py
+++ test/fdm/dirichlet_bc_data.py
@@ -1,5 +1,4 @@
 from fealpy.backend import backend_manager as bm
-from fealpy.mesh import UniformMesh
 from fealpy.model import PDEDataManager
 from fealpy.sparse import CSRTensor 
 from scipy import sparse
@@ -7,7 +6,7 @@ from scipy import sparse
 pde = PDEDataManager('poisson').get_example('coscos')
 domain = pde.domain()  # [-1, 1, -1, 1]
 extent = [0, 2, 0, 2]
-mesh = UniformMesh(domain, extent)
+
 
 def bd_1(node):
     x = node[:, 0]
@@ -20,8 +19,6 @@ def dirichlet_bc(threshold, mesh):
     bd_idx = threshold(bd_node)
     return bd_idx
 
-x = dirichlet_bc(bd_1, mesh)
-
 A_before_scipy = sparse.csr_matrix(bm.array(
     [[ 4., -1.,  0., -1.,  0.,  0.,  0.,  0.,  0.],
      [-1.,  4., -1.,  0., -1.,  0.,  0.,  0.,  0.],
@@ -33,6 +30,7 @@ A_before_scipy = sparse.csr_matrix(bm.array(
      [ 0.,  0.,  0.,  0., -1.,  0., -1.,  4., -1.],
      [ 0.,  0.,  0.,  0.,  0., -1.,  0., -1.,  4.]]
 ))
+
 A_before = CSRTensor(A_before_scipy.indptr, A_before_scipy.indices, A_before_scipy.data, A_before_scipy.shape)
 
 f_before = bm.array([ 19.7392088, -19.7392088,  19.7392088, -19.7392088,  19.7392088, -19.7392088,
@@ -49,6 +47,7 @@ A_after_none_scipy = sparse.csr_matrix(bm.array(
      [0., 0., 0., 0., 0., 0., 0., 1., 0.],
      [0., 0., 0., 0., 0., 0., 0., 0., 1.]]
 ))
+
 A_after_none = CSRTensor(A_after_none_scipy.indptr, A_after_none_scipy.indices, A_after_none_scipy.data, A_after_none_scipy.shape)
 
 f_after_none = bm.array([1., -1., 1., -1., 15.7392088, -1., 1., -1., 1.])
