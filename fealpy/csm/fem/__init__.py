f4ce1456a9675a4902a3ee17f0c6bf41c85de665
Liujiawangmath <15674760037@163.com>
2025-05-30 19:35:20 +0800

    update

--- fealpy/csm/fem/__init__.py
+++ fealpy/csm/fem/__init__.py
@@ -2,7 +2,3 @@
 from .beam_diffusion_integrator import BeamElementStiffnessIntegrator
 from .beam_diffusion_integrator import BeamDiffusionIntegrator
 from .beam_source_integrator import BeamSourceIntegrator
-
-
-### Dirichlet Operator
-from .dirichlet_bc import DirichletBC
