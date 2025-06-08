5e527db6db0b3e7a1c08adb14cd7f810443919ca
Huayi Wei <weihuayi@xtu.edu.cn>
2025-06-08 10:41:02 +0800

    update

---
+++ example/computmodel.py
@@ -0,0 +1,22 @@
+
+
+
+class EquationLFEMModel:
+
+
+    def __init__(self, pde, mesh, p=1):
+        pass
+
+
+    def generate_mesh(self):
+        return mesh
+
+    def run(self, maxit=4):
+        pass
+
+
+    def newton_step(self):
+        pass
+
+    def linear_system(self):
+        pass
