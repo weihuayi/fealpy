9183c39fb157ec4716e49f499402acd1ca4a7b39
Hyacine <zhengyy0621@foxmail.com>
2025-06-20 23:55:21 +0800

    fix(common): arg name 'dim' should be replaced by 'axis' fix(cem): the file name to import is not correct. fix(lafem-eit): a variable is used before assigned.

--- fealpy/common/Tools.py
+++ fealpy/common/Tools.py
@@ -1,12 +1,14 @@
 from ..backend import backend_manager as bm
 from ..backend import TensorLike
 
+
 def ranges(nv: TensorLike, start = 0):
-    shifts = bm.cumsum(nv, dim=0)
+    shifts = bm.cumsum(nv, axis=0)
     id_arr = bm.ones(shifts[-1], dtype=bm.int32)
     id_arr = bm.set_at(id_arr, (shifts[:-1]), -bm.asarray(nv[:-1])+1)
     id_arr = bm.set_at(id_arr, (0), start)
-    return bm.cumsum(id_arr, dim=0)
+    return bm.cumsum(id_arr, axis=0)
+
 
 def hash2map(dec: TensorLike, ha: TensorLike):
     n = ha.shape[1]
@@ -21,8 +23,9 @@ def hash2map(dec: TensorLike, ha: TensorLike):
             mdx = bm.argmin(bm.sum(bm.abs(ha[pos] - b[i]), axis=-1))
             m = bm.set_at(m, (i), ha[pos[mdx]])
             v = bm.set_at(v, (i), pos[mdx]+1)
-            
+
     return m, v
+
 
 def angle(v0: TensorLike, v1: TensorLike):
     a = v0/bm.sqrt(bm.sum(v0**2, axis=-1, keepdims=True))
@@ -34,4 +37,3 @@ def angle(v0: TensorLike, v1: TensorLike):
         c = bm.cross(a, b, axis=-1)
         sin = bm.sqrt(bm.sum(c**2, axis=-1))
     return bm.arctan2(sin, cos)
-
