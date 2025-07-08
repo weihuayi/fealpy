92bd89d89bf99b5fe26ea3b8d669b068c5f8d8d8
Huayi Wei <weihuayi@xtu.edu.cn>
2025-07-08 11:53:17 +0800

    merge from draft/bella
    feat(model)：add the surface poisson model, including mesher and poisson data.
    feat(fem)：add the surface poisson computation model.
    feat(example)：add the surface poisson example.

--- fealpy/model/domain_mesher/sphere_domain_mesher.py
+++ fealpy/model/domain_mesher/sphere_domain_mesher.py
@@ -1,7 +1,9 @@
 from typing import Sequence
-from ...backend import backend_manager as bm
-from ...decorator import variantmethod
-from ...mesh import TetrahedronMesh
+from ..backend import backend_manager as bm
+from ..decorator import variantmethod
+from ..geometry import SphereSurface
+from ..mesh import TetrahedronMesh,QuadrangleMesh,TriangleMesh
+from ..mesh import LagrangeQuadrangleMesh,LagrangeTriangleMesh
 
 class SphereDomainMesher:
     """Sphere domain mesh generator"""
@@ -18,4 +20,30 @@ class SphereDomainMesher:
     @variantmethod('tet')
     def init_mesh(self): 
         mesh = TetrahedronMesh.from_unit_sphere_gmsh(h=self.h)
+        return mesh
+    
+
+class SphereDomainMesher3D:
+    """Sphere domain mesh generator"""
+    def __init__(self, surface=None):
+        if surface is None:
+            self.surface = SphereSurface()
+        else:
+            self.surface = surface
+        
+    def geo_dimension(self) -> int:
+        return 3
+
+    @variantmethod('ltri')
+    def init_mesh(self, p:int=1):
+        """Create a LagrangeTriangleMesh from a triangle mesh."""
+        lmesh = TriangleMesh.from_unit_sphere_surface()
+        mesh = LagrangeTriangleMesh.from_triangle_mesh(lmesh, p=p, surface=self.surface)
+        return mesh
+    
+    @variantmethod('lquad')
+    def init_mesh(self, p:int=1):
+        """Create a LagrangeQuadrangleMesh from a quadrangle mesh."""
+        lmesh = QuadrangleMesh.from_unit_sphere_surface()
+        mesh = LagrangeQuadrangleMesh.from_quadrangle_mesh(lmesh, p=p, surface=self.surface)
         return mesh
