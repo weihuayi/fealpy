0e794990f689cf346215d4227aa31b434717e9dd
Huayi Wei <weihuayi@xtu.edu.cn>
2025-07-15 16:45:55 +0800

    Merge from draft/concha
    feat(polycube): add a directory named polycube with mesh deformation polycube segmentation.
    feat(cem): Add a mesher to create mesh of metalenses, with the example.
    example(cem): add an example of the mesher for metalenses.
    feat(cem): the assembly function for total mesh of metalenses has have been realized;
    feat(cem): add a mesher to create one element of metalenses with periodic boundary conditions(pbc) and perfectly matched layer(pml), but now only can create one unit of it, the assembly function for total mesh has not yet been realized;
    feat(cem): add a script for convertint .msh files to .vtu files;
    feat(cem): and a script to read the .mphtxt files(from comsol), and create corresponding fealpy tet mesh;
    feat(cem): add a draft for creating prism mesh for metalenses;
    tese(cem): add relevant tests.

---
+++ fealpy/polycube/polycube_processor.py
@@ -0,0 +1,120 @@
+from fealpy.backend import backend_manager as bm
+from fealpy.mesh import TriangleMesh, TetrahedronMesh
+
+from .mesh_deformation import MeshNormalSmoothDeformation, MeshNormalAlignmentDeformation
+from .polycube_segmentator import PolyCubeSegmentator
+
+
+class PolyCubeProcessor:
+    """
+
+    """
+    def __init__(self, origin_mesh):
+        """
+        Initialize the PolyCubeProcessor with the original mesh.
+
+        Parameters
+        ----------
+        origin_mesh : TriangleMesh or TetrahedronMesh
+            The original mesh to be processed.
+        """
+
+        self.mesh = origin_mesh
+
+    def mesh_normal_smooth_deformation(self, sigma=0.1, s=7, alpha=0.5, max_epochs=100000, error_threshold=1e-3, weights=None):
+        """
+        Apply normal smooth deformation to the original mesh.
+
+        Parameters
+        ----------
+        sigma : float
+            The standard deviation for the Gaussian kernel.
+        s : int
+            The number of nearest neighbors to consider.
+        alpha : float
+            The weight for the smoothness term.
+        max_epochs : int
+            The maximum number of epochs for optimization.
+        error_threshold : float
+            The threshold for convergence.
+        weights : dict
+            Weights for different components in the deformation.
+
+        Returns
+        -------
+        TriangleMesh or TetrahedronMesh
+            The deformed mesh after normal smoothing.
+        """
+
+        normal_smooth_deformer = MeshNormalSmoothDeformation(self.mesh,
+                                                              sigma=sigma, s=s, alpha=alpha,
+                                                              max_epochs=max_epochs,
+                                                              error_threshold=error_threshold,
+                                                              weights=weights)
+        self.mesh = normal_smooth_deformer.optimize()
+        return self.mesh
+
+    def polycube_segmentator(self,
+                        straighten_max_iter=5,
+                        merge_min_size=5,
+                        laplacian_smooth_alpha=0.3,
+                        laplacian_smooth_max_iter=5):
+        """
+        Segmentation of the polycube mesh.
+
+        Parameters
+            straighten_max_iter : int
+                Maximum iterations for edge straightening.
+            merge_min_size : int
+                Minimum size for merging small charts.
+            laplacian_smooth_alpha : float
+                Alpha value for Laplacian smoothing.
+            laplacian_smooth_max_iter : int
+                Maximum iterations for Laplacian smoothing.
+
+        Returns
+            is_valid : bool
+                Whether the resulting topology is valid.
+        """
+        polycube_processor = PolyCubeSegmentator(self.mesh)
+        polycube_processor.build_candidate_charts()
+        polycube_processor.extract_candidate_edges_vertices()
+        polycube_processor.straighten_edges(max_iter=straighten_max_iter)
+        polycube_processor.merge_small_charts(min_size=merge_min_size)
+        is_valid = polycube_processor.validate_topology()
+        polycube_processor.laplacian_smooth(alpha=laplacian_smooth_alpha, max_iter=laplacian_smooth_max_iter)
+        polycube_processor.edge_projection()
+        polycube_processor.detect_turning_points()
+
+        return is_valid
+
+    def mesh_normal_alignment_deformation(self, gamma=1e3, s=6, alpha=0.5, max_epochs=100000, error_threshold=1e-3, weights=None):
+        """
+        Apply normal alignment deformation to the mesh.
+
+        Parameters
+            gamma : float
+                The weight for the alignment term.
+            s : int
+                The number of nearest neighbors to consider.
+            alpha : float
+                The weight for the smoothness term.
+            max_epochs : int
+                The maximum number of epochs for optimization.
+            error_threshold : float
+                The threshold for convergence.
+            weights : dict
+                Weights for different components in the deformation.
+
+        Returns
+            TetrahedronMesh
+                The deformed mesh after normal alignment.
+        """
+
+        normal_alignment_deformer = MeshNormalAlignmentDeformation(self.mesh,
+                                                                   gamma=gamma, s=s, alpha=alpha,
+                                                                   max_epochs=max_epochs,
+                                                                   error_threshold=error_threshold,
+                                                                   weights=weights)
+        self.mesh = normal_alignment_deformer.optimize()
+        return self.mesh
--- test/geometry/test_geometry_base.py
+++ test/geometry/test_geometry_base.py
@@ -44,24 +44,22 @@ class TestGeometryKernelBase:
         spline1 = gkm.add_spline(ctrl_points1)
         spline2 = gkm.add_spline([p1, p2, p3, p4])
         # 显示
-        gkm.display(p1, p2, p3, line, arc, arc2, spline1, spline2,
-                    color=["blue", "red", "green", "yellow", "purple", "orange"],
-                    transpose=[0.3, 0.6, 0.9, 0.5, 0.7, 0.8],)
-        # gkm.display(spline1)
+        # gkm.display(p1, p2, p3, line, arc, arc2, spline1, spline2)
+        gkm.display(spline1)
 
     @pytest.mark.parametrize("kernel", ['occ'])
     @pytest.mark.parametrize("input_data", geometry_data)
     def test_entity_construct2(self, input_data, kernel):
         gkm.set_adapter(kernel)
 
-        box = gkm.add_box(2, 10, 2, 1, 1, 1)
-        ellipsoid = gkm.add_ellipsoid(-10, 0, 0, 5, 3, 2)
-        cylinder1 = gkm.add_cylinder(5, 5, 0, 1, 4)
-        cylinder2 = gkm.add_cylinder(-5, -5, -2, 1, 4, axis=(1, 0, 0))
+        box = gkm.add_box(2, 2, 2, 1, 1, 1)
+        ellipsoid = gkm.add_ellipsoid(0, 0, 0, 5, 3, 2)
+        cylinder1 = gkm.add_cylinder(0, 0, 0, 1, 4)
+        cylinder2 = gkm.add_cylinder(-2, -2, -2, 1, 4, axis=(1, 0, 0))
+        ring = gkm.add_ring(10, 10, 0, 1, 2)
         torus = gkm.add_torus(10, 10, 0, 10, 2)
-        hollow_cyl = gkm.add_hollow_cylinder(5, -6, 0, 5, 3, 10)
-        gkm.display(box, ellipsoid, cylinder1, cylinder2, torus, hollow_cyl,
-                    color=["blue", "red", "green", "k", "purple", "orange"])
+        hollow_cyl = gkm.add_hollow_cylinder(0, 0, 0, 5, 3, 10)
+
 
         edges1 = [gkm.add_line((0, 0, 0), (5, 0, 0)),
                  gkm.add_line((5, 0, 0), (5, 5, 0)),
@@ -115,13 +113,6 @@ class TestGeometryKernelBase:
     def test_entity_construct3(self, input_data, kernel):
         gkm.set_adapter(kernel)
 
-        rectangle = gkm.add_rectangle(0, 0, 0, 3, 2)
-        disk = gkm.add_disk(5, -2, 0, 2, 3)
-        circle = gkm.add_circle(0, 5, 0, 3)
-        ring = gkm.add_ring(0, -5, 0, 2, 3)
-
-        gkm.display(rectangle, disk, circle, ring,
-                    color=["blue", "red", "green", "yellow"])
 
 
     @pytest.mark.parametrize("kernel", ['occ'])
@@ -196,90 +187,18 @@ class TestGeometryKernelBase:
     @pytest.mark.parametrize("kernel", ['occ'])
     @pytest.mark.parametrize("input_data", geometry_data)
     def test_example_metalenses(self, input_data, kernel):
-        # 设置参数
-        square_size = 45  # 正方形的边长
-        circle_radius = 20  # 圆的半径
-        box_length = 0.24  # 长方体的长
-        box_width = 0.12  # 长方体的宽
-        box_height = 0.6  # 长方体的高
-        circle_radius = 20  # 圆的半径
-        square_size = 0.4  # 内部正方形的边长
-        spacing = 0.4  # 相邻正方形的中心间距
-        # 存储正方形中心坐标
-        square_centers = []
-        # 遍历圆内区域
-        num_squares_x = int(circle_radius / spacing)  # x方向正方形数量
-        num_squares_y = int(circle_radius / spacing)  # y方向正方形数量
-        for i in range(-num_squares_x, num_squares_x + 1):  # x方向
-            for j in range(-num_squares_y, num_squares_y + 1):  # y方向
-                # 计算正方形中心坐标
-                center_x = i * spacing
-                center_y = j * spacing
-                # 检查正方形的四个角点是否都在圆内
-                if (bm.sqrt((center_x - square_size / 2) ** 2 + (center_y - square_size / 2) ** 2) <= circle_radius) and \
-                        (bm.sqrt(
-                            (center_x + square_size / 2) ** 2 + (center_y - square_size / 2) ** 2) <= circle_radius) and \
-                        (bm.sqrt(
-                            (center_x - square_size / 2) ** 2 + (center_y + square_size / 2) ** 2) <= circle_radius) and \
-                        (bm.sqrt(
-                            (center_x + square_size / 2) ** 2 + (center_y + square_size / 2) ** 2) <= circle_radius):
-                    # 存储满足条件的中心坐标
-                    square_centers.append((center_x, center_y, 0))
-        # 将列表转换为二维数组
-        square_centers_array = bm.array(square_centers)
-        # 定义旋转的角度
-        # 参数定义
-        f = 60  # 设计焦距
-        Lambda = 0.98  # 入射光波长
+        gkm.set_adapter(kernel)
 
-        def rotate_angle(p):
-            x = p[..., 0]
-            y = p[..., 1]
-            # 计算旋转角度
-            theta = bm.pi / Lambda * (f - bm.sqrt(x ** 2 + y ** 2 + f ** 2))
-            return theta
+        ori = gkm.add_point(0, 0, 0)
+        box_base = gkm.add_box(-12.5, -12.5, -0.1, 25, 25, 0.1)
 
-        def generate_square(center, length, width):
-            # 计算最小坐标点
-            min_coords = center - bm.array([length / 2, width / 2, 0])
-            return min_coords
+        box1 = gkm.add_box(-0.12, -0.06, 0.0, 0.24, 0.12, 0.6)
+        box2 = gkm.translate(box1, (0.4, 0, 0))
+        box3 = gkm.translate(gkm.rotate(box1, (0, 0, 0), (0, 0, 1), 3.14/4), (0, 0.4, 0))
 
-        # 旋转的角度
-        angle = rotate_angle(square_centers_array)
-        # 旋转正方形的最小点坐标
-        min_coords = generate_square(square_centers_array, box_length, box_width)
-        total_shape = []
-        total_shape.append(gkm.add_box(-22.5, -22.5, -0.1, 45, 45, 0.1))
-        option = bm.concatenate((min_coords, square_centers_array), axis=1)
-        angle = angle.reshape(-1, 1)
-        option1 = bm.concatenate((option, angle), axis=1)
-        for i, j, k, m, n, l, angles in option1:
-            box = gkm.rotate(gkm.add_box(i, j, k, 0.24, 0.12, 0.6),
-                             (m, n, l), (0, 0, 1), angles)
-            total_shape.append(box)
+        total_shape = gkm.boolean_union(box_base, box1, box2, box3)
 
-        gkm.display(*total_shape)
-        from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
-        from OCC.Core.StlAPI import StlAPI_Writer
-        from OCC.Core.TopoDS import TopoDS_Shape
-        from OCC.Core.TopoDS import TopoDS_Compound
-        from OCC.Core.BRep import BRep_Builder
-        # 创建一个复合形状
-        compound = TopoDS_Compound()
-        builder = BRep_Builder()
-        builder.MakeCompound(compound)
-
-        # 将所有形状添加到复合形状中
-        for shape in total_shape:
-            builder.Add(compound, shape)
-
-        # 初始化 STL 写入器
-        stl_writer = StlAPI_Writer()
-        stl_writer.SetASCIIMode(True)  # True 为 ASCII 格式，False 为二进制格式
-
-        # 导出复合形状到 STL 文件
-        stl_writer.Write(compound, "metalenses.stl")
-        print("STL file 'metalenses.stl' has been created successfully.")
+        gkm.display(ori, total_shape)
 
     @pytest.mark.parametrize("kernel", ['occ'])
     @pytest.mark.parametrize("input_data", geometry_data)
@@ -293,137 +212,6 @@ class TestGeometryKernelBase:
         total_shape = gkm.boolean_union(screw, nut, roller)
         gkm.display(total_shape)
 
-
-    @pytest.mark.parametrize("kernel", ['occ'])
-    @pytest.mark.parametrize("input_data", geometry_data)
-    def test_performance(self, input_data, kernel):
-        gkm.set_adapter(kernel)
-
-        tmr = timer()
-        next(tmr)
-        maxiter = 100
-        boxs = []
-
-        for i in range(maxiter):
-            box = gkm.add_box(0, 0, 0, 1+i*1.5, 1, 1)
-            boxs.append(box)
-        tmr.send("生成 box 花费时间")
-
-        total_shape = gkm.boolean_union(*boxs)
-        tmr.send("生成联合体花费时间")
-
-        gkm.display(total_shape)
-        # gkm.display(*boxs)
-        tmr.send("显示花费时间")
-        next(tmr)
-
-    @pytest.mark.parametrize("kernel", ['occ'])
-    @pytest.mark.parametrize("input_data", geometry_data)
-    def test_mult_input(self, input_data, kernel):
-        # bm.set_backend("pytorch")
-        gkm.set_adapter(kernel)
-
-        # box
-        # 单个输入
-        box1 = gkm.add_box(0, 0, 0, 5, 5, 5)
-        gkm.display(box1)
-
-        # 列表
-        data_list = [[0, 0, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1], [2, 2, 2, 3, 3, 3]]
-        boxes1 = gkm.add_box(data_list)
-        gkm.display(boxes1, color="red")
-
-        # 元组
-        data_tuple = ((0, 0, 0, 1, 1, 1), (1, 1, 1, 1, 1, 1), (2, 2, 2, 3, 3, 3))
-        boxes3 = gkm.add_box(data_tuple)
-        gkm.display(boxes3, color="blue")
-
-        # 数组
-        data_array = bm.array([[0, 0, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1], [2, 2, 2, 3, 3, 3]])
-        boxes5 = gkm.add_box(data_array)
-        gkm.display(*boxes5, color=["green", "r", "y"])
-
-        # # add_rectangle
-        # data_array = bm.array([[0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [2, 2, 2, 3, 3]])
-        # rectangles = gkm.add_rectangle(data_array)
-        # gkm.display(rectangles, color="green")
-        #
-        # # add_disk
-        # data_array = bm.array([[0, 0, 0, 1, 2], [1, 1, 1, 2, 3], [2, 2, 2, 3, 4]])
-        # disks = gkm.add_disk(data_array)
-        # gkm.display(disks, color="green")
-        #
-        # # add_circle
-        # data_array = bm.array([[0, 0, 0, 1], [1, 1, 1, 2], [2, 2, 2, 3]])
-        # circles = gkm.add_circle(data_array)
-        # gkm.display(circles, color="green")
-        #
-        # # add_ring
-        # data_array = bm.array([[0, 0, 0, 1, 2], [1, 1, 1, 2, 3], [2, 2, 2, 3, 4]])
-        # rings = gkm.add_ring(data_array)
-        # gkm.display(rings, color="green")
-        #
-        # # add_box
-        # data_array = bm.array([[0, 0, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1], [2, 2, 2, 3, 3, 3]])
-        # boxes5 = gkm.add_box(data_array)
-        # gkm.display(boxes5, color="green")
-        #
-        # # add_ellipsoid
-        # data_array = bm.array([[0, 0, 0, 1, 2, 3], [4, 4, 4, 1.5, 2.5, 3.5], [8, 8, 8, 2, 3, 4]])
-        # ellipsoids = gkm.add_ellipsoid(data_array)
-        # gkm.display(ellipsoids, color="green")
-        #
-        # # add_sphere
-        # data_array = bm.array([[0, 0, 0, 1], [3, 3, 3, 1.5], [6, 6, 6, 2]])
-        # spheres = gkm.add_sphere(data_array)
-        # gkm.display(spheres, color="green")
-        #
-        # # add_cylinder
-        data_array = bm.array([[0, 0, 0, 1, 5],
-                                            [1, 1, 1, 2, 10]])
-        axis = bm.array([[1, 0, 0],
-                                [0, 1, 0]])
-        cylinders = gkm.add_cylinder(data_array, axis=axis)
-        gkm.display(cylinders, color="green")
-        #
-        # # add_torus
-        # data_array = bm.array([[0, 0, 0, 2, 1], [3, 3, 3, 3, 1.5], [8, 8, 8, 4, 2]])
-        # toruses = gkm.add_torus(data_array)
-        # gkm.display(toruses, color="green")
-        #
-        # # add_hollow_cylinder
-        # data_array = bm.array([[0, 0, 0, 2, 1, 3], [4, 4, 4, 2.5, 1.5, 3.5], [8, 8, 8, 3, 2, 4]])
-        # axis = bm.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        # hollow_cylinders = gkm.add_hollow_cylinder(data_array, axis=axis)
-        # gkm.display(hollow_cylinders, color="green")
-
-    @pytest.mark.parametrize("kernel", ['occ'])
-    @pytest.mark.parametrize("input_data", geometry_data)
-    def test_import_export(self, input_data, kernel):
-        gkm.set_adapter(kernel)
-
-        box = gkm.add_box(2, 10, 2, 1, 1, 1)
-        ellipsoid = gkm.add_ellipsoid(-10, 0, 0, 5, 3, 2)
-        cylinder1 = gkm.add_cylinder(5, 5, 0, 1, 4)
-        cylinder2 = gkm.add_cylinder(-5, -5, -2, 1, 4, axis=(1, 0, 0))
-        torus = gkm.add_torus(10, 10, 0, 10, 2)
-        hollow_cyl = gkm.add_hollow_cylinder(5, -6, 0, 5, 3, 10)
-        total_shape = [box, ellipsoid, cylinder1, cylinder2, torus, hollow_cyl]
-
-        gkm.export_step(*total_shape, filename="box.step")
-        gkm.export_stl(*total_shape, filename="box.stl", resolution=0.1)
-        gkm.export_brep(*total_shape, filename="box.brep")
-
-        box1 = gkm.import_step("box.step")
-        box2 = gkm.import_stl("box.stl")
-        box3 = gkm.import_brep("box.brep")
-
-        gkm.display(box1)
-        gkm.display(box2)
-        gkm.display(box3)
-
-        metalenses = gkm.import_stl("metalenses.stl")
-        gkm.display(metalenses)
 
 
 if __name__ == "__main__":
