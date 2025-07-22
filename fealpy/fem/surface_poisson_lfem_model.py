c54da0d42fff6b713f1d8a02d3a25d6ecd6a3732
2655493031@qq.com <2655493031@qq.com>
2025-07-22 08:27:27 +0800

    update

--- fealpy/fem/surface_poisson_lfem_model.py
+++ fealpy/fem/surface_poisson_lfem_model.py
@@ -32,9 +32,9 @@ class SurfacePoissonLFEMModel(ComputationalModel):
        self.set_init_mesh(options['mesh_degree'], options['init_mesh']) 
        self.set_space_degree(options['space_degree']) 
     
-    def set_pde(self, pde: Union[SurfacePDEDataT, str] = "exp0001") -> None:
+    def set_pde(self, pde: Union[SurfacePDEDataT, int] = 1) -> None:
         if isinstance(pde, str):
-            self.pde = PDEModelManager("surface_poisson").get_example(1)
+            self.pde = PDEModelManager("surface_poisson").get_example(pde)
         else:
             self.pde = pde
 
