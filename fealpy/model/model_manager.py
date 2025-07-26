dabcf4850be8eded10591417590a9d9c161dbf89
“cyf9898” <2844529583@qq.com>
2025-07-26 10:27:34 +0800

    feat(model):add df to model_manager.

--- fealpy/model/model_manager.py
+++ fealpy/model/model_manager.py
@@ -134,6 +134,7 @@ class PDEModelManager(ModelManager):
         "parabolic": "fealpy.model.parabolic",
         "wave": "fealpy.model.wave",
         "hyperbolic":"fealpy.model.hyperbolic",
+        "darcyforchheimer":"fealpy.model.darcyforchheimer",
         "interface_poisson":"fealpy.model.interface_poisson",
         "ion_flow":"fealpy.model.ion_flow",
         "nonlinear":"fealpy.model.nonlinear",
