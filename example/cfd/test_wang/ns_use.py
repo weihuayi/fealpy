f42cd8aaf47d63ea0bd0cc5aa34ca8d2312e8d78
wpx <1143615697@qq.com>
2025-08-05 16:03:06 +0800

    WIP

--- example/cfd/test_wang/ns_use.py
+++ example/cfd/test_wang/ns_use.py
@@ -1,28 +1,30 @@
 from fealpy.backend import backend_manager as bm
 from fealpy.cfd.incompressible_navier_stokes_lfem_2d_model import IncompressibleNSLFEM2DModel
-from fealpy.cfd.model.test.incompressible_navier_stokes.incompressible_navier_stokes_2d import FromSympy, NSLFEMChannelPDE
-from fealpy.cfd.model.test.incompressible_navier_stokes.exp0001 import Exp0001
-from fealpy.decorator import cartesian
-from fealpy.fem import DirichletBC
+from fealpy.cfd.model.test.incompressible_navier_stokes.incompressible_navier_stokes_2d import FromSympy
+from fealpy.cfd.model.incompressible_navier_stokes.poiseuille_2d import Poiseuille2D
 
 
 pde = FromSympy()
-pde.select_pde['channel']()
+#pde.select_pde['channel']()
 #pde.select_pde['poly2d']()
-#pde.select_pde['sinsincos']()
+pde.select_pde['sinsincos']()
 #
-#pde = NSLFEMChannelPDE()
+#pde = Poiseuille2D()
 
-mesh = pde.set_mesh(5,5)
-model = IncompressibleNSLFEM2DModel(pde)
+mesh = pde.init_mesh['uniform_tri'](10, 10)
+model = IncompressibleNSLFEM2DModel(pde, mesh=mesh)
 model.equation.set_constitutive(2)
 
 fem = model.fem
 equation = model.equation
 timeline = model.timeline
-timeline.set_timeline(0, 10, 100)
-#model.method['IPCS']()
-model.method['Newton']()
+timeline.set_timeline(0, 10, 300)
+model.method['IPCS']()
+#model.method['Newton']()
 
-#model.apply_bc.set('None')
 u1,p = model.run(maxstep=1)
+mesh = model.mesh
+
+mesh.nodedata['p'] = p
+mesh.nodedata['u1'] = u1.reshape(2,-1).T
+mesh.to_vtk('ns2d.vtu')
