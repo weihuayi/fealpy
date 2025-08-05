28855b7e60a21d0724dd04a77fe02a5099c2cdb3
Huayi Wei <weihuayi@xtu.edu.cn>
2025-08-06 07:09:13 +0800

    WIP(csm): 完成实际案例的耦合线性系统的构建工作

--- fealpy/csm/fem/gear_box_modal_lfem_model.py
+++ fealpy/csm/fem/gear_box_modal_lfem_model.py
@@ -34,6 +34,10 @@ class GearBoxModalLFEMModel(ComputationalModel):
         mesh = self.pde.init_mesh()
         self.set_mesh(mesh)
 
+        # 每种计算模型都要指定自己的整数和浮点数类型
+        self.itype = bm.int32
+        self.ftype = bm.float64
+
     def set_pde(self, pde = 2) -> None:
         if isinstance(pde, int):
             self.pde = CSMModelManager("linear_elasticity").get_example(
@@ -50,51 +54,50 @@ class GearBoxModalLFEMModel(ComputationalModel):
 
     def rbe2_matrix(self): 
         """
-        Set node flags for the gear box model.
 
-        the node flags is the base for the dofs flag
         """
         NN = self.mesh.number_of_nodes()
         redges, rnodes = self.mesh.data.get_rbe2_edge()
 
-        # 耦合节点
+        # 壳体与轴系结构的耦合节点
         isCNode = self.mesh.data.get_node_data('isCNode')
 
         # 参考节点, 注意部分参考节点可能不是耦合节点
         isRNode = bm.zeros(NN, dtype=bm.bool)
         isRNode = bm.set_at(isRNode, rnodes, True)
 
-        # 齿轮箱节点
+        # 齿轮箱节点（非参考节点）
         isGBNode = bm.logical_not(isRNode)
 
-        # 参考节点约束的齿轮箱面节点
+        # 参考节点约束的齿轮箱面节点（依赖点，注意有部分面节点应该是为了计算声功率而设置的）
         isSNode = bm.zeros(NN, dtype=bm.bool)
         isSNode = bm.set_at(isSNode, redges[:, 1], True)
 
-        # 实际耦合节点对应的齿轮箱面节点
+        # RBE2, 实际耦合节点对应的齿轮箱面节点
         isRBE2 = isCNode[redges[:, 0]]
         isCSNode = bm.zeros(NN, dtype=bm.bool)
         isCSNode = bm.set_at(isCSNode, redges[isRBE2, 1], True)
         
-        # 齿轮箱固定节点, 注意这里只是临时处理 
+        # 齿轮箱固定(Fixed)节点, 注意这里只是临时处理 
         name = self.mesh.data['boundary_conditions'][0][0]
         nset = self.mesh.data.get_node_set(name)
         isFNode = bm.zeros(NN, dtype=bm.bool)
         isFNode[nset] = True
 
-        # 记录节点的标志
+        # 添加网格节点数据 
         self.mesh.data.add_node_data('isRNode', isRNode)
         self.mesh.data.add_node_data('isGBNode', isGBNode)
         self.mesh.data.add_node_data('isSNode', isSNode)
+        self.mesh.data.add_node_data('isCSNode', isCSNode)
         self.mesh.data.add_node_data('isFNode', isFNode)
 
-
+        # RBE2 单元
         ridx = bm.where(isCNode)[0]
         sidx = bm.where(isCSNode)[0]
-        rmap = bm.zeros(NN, dtype=bm.int32)
-        smap = bm.zeros(NN, dtype=bm.int32)
-        rmap = bm.set_at(rmap, ridx, bm.arange(ridx.shape[0], dtype=bm.int32))
-        smap = bm.set_at(smap, sidx, bm.arange(sidx.shape[0], dtype=bm.int32))
+        rmap = bm.zeros(NN, dtype=self.itype)
+        smap = bm.zeros(NN, dtype=self.itype)
+        rmap = bm.set_at(rmap, ridx, bm.arange(ridx.shape[0], dtype=self.itype))
+        smap = bm.set_at(smap, sidx, bm.arange(sidx.shape[0], dtype=self.itype))
 
         I = smap[redges[isRBE2, 1]]
         J = rmap[redges[isRBE2, 0]]
@@ -102,35 +105,39 @@ class GearBoxModalLFEMModel(ComputationalModel):
 
         node = self.mesh.entity('node')
         # 依赖点指向参考点 
-        v = node[redges[redges[isRBE2, 0]] - node[redges[isRBE2, 1]]
+        v = node[redges[isRBE2, 0]] - node[redges[isRBE2, 1]]
 
         NS = isCSNode.sum() # number of surface nodes
         NR = isCNode.sum() # number of reference nodes
 
         self.logger.info(f"RBE2 matrix: {NS} surface nodes, {NR} reference nodes")
 
-        G = coo_matrix((3*NS, 6*NR), dtype=bm.float64)
-        ones = bm.ones(NS, dtype=bm.float64)
+        #G = coo_matrix((3*NS, 6*NR))
 
-        G += coo_matrix((    ones, (3*I+0, 6*J+0)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix((-v[:, 2], (3*I+0, 6*J+4)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix(( v[:, 1], (3*I+0, 6*J+5)), shape=G.shape, dtype=bm.float64)
+        kwargs = {'shape':(3*NS, 6*NR), 'itype': self.itype, 'dtype': self.ftype}
+        ones = bm.ones(NS, dtype=self.ftype)
 
-        G += coo_matrix((    ones, (3*I+1, 6*J+1)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix(( v[:, 2], (3*I+1, 6*J+3)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix((-v[:, 0], (3*I+1, 6*J+5)), shape=G.shape, dtype=bm.float64)
+        G  = coo_matrix((    ones, (3*I+0, 6*J+0)), **kwargs)
+        G += coo_matrix((-v[:, 2], (3*I+0, 6*J+4)), **kwargs)
+        G += coo_matrix(( v[:, 1], (3*I+0, 6*J+5)), **kwargs)
 
-        G += coo_matrix((    ones, (3*I+2, 6*J+2)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix((-v[:, 1], (3*I+2, 6*J+3)), shape=G.shape, dtype=bm.float64)
-        G += coo_matrix(( v[:, 0], (3*I+2, 6*J+4)), shape=G.shape, dtype=bm.float64)
+        G += coo_matrix((    ones, (3*I+1, 6*J+1)), **kwargs)
+        G += coo_matrix(( v[:, 2], (3*I+1, 6*J+3)), **kwargs)
+        G += coo_matrix((-v[:, 0], (3*I+1, 6*J+5)), **kwargs)
 
-        return G.tocsr().to_scipy()
+        G += coo_matrix((    ones, (3*I+2, 6*J+2)), **kwargs)
+        G += coo_matrix((-v[:, 1], (3*I+2, 6*J+3)), **kwargs)
+        G += coo_matrix(( v[:, 0], (3*I+2, 6*J+4)), **kwargs)
+
+        self.logger.info(f"RBE2 matrix shape: {G.shape}, nnz: {G.nnz}")
+
+        self.G = G.tocsr().to_scipy()
 
 
     def box_linear_system(self):
         """
         """
-        # Implementation of the linear system construction goes here
+
         GD = self.mesh.geo_dimension()
         self.space = functionspace(self.mesh, ('Lagrange', 1), shape=(-1, GD))
 
@@ -153,8 +160,8 @@ class GearBoxModalLFEMModel(ComputationalModel):
         isFNode = self.mesh.data.get_node_data('isFNode')
         isCSNode = self.mesh.data.get_node_data('isCSNode')
         isFreeNode = ~(isRNode | isFNode | isCSNode) 
-        isFreeDof = bm.repeat(isFreeNode, 3)
 
+        isFreeDof = bm.repeat(isFreeNode, 3)
         isCSDof = bm.repeat(isCSNode, 3)
 
         S0 = S[isFreeDof, :]
@@ -166,6 +173,7 @@ class GearBoxModalLFEMModel(ComputationalModel):
 
         M0 = M[isFreeDof, :]
         M1 = M[isCSDof, :]
+
         M00 = M0[:, isFreeDof]
         M01 = M0[:, isCSDof] @ self.G
         M11 = self.G.T @ M1[:, isCSDof] @ self.G
@@ -174,14 +182,8 @@ class GearBoxModalLFEMModel(ComputationalModel):
 
     def shaft_linear_system(self):
         """
-        轴系的线性系统对应的自由度，可以分为耦合与非耦合两部分。
-        耦合部分是指轴系的耦合节点，这些节点的自由度需要与齿轮箱的自由度进行耦合。
-        这里的问题是，轴系的耦合节点的排序与齿轮箱对应节点排序不一致。
-        因此需要对轴系的线性系统进行处理，将耦合节点的自由度与齿轮箱的自由度进行匹配。
-
-        1. 轴系的矩阵进行重排序
-        2. 非耦合节点的排序不变
-        3. 耦合节点的排序要与齿轮箱的节点排序一致
+        Notes:
+            1. 这里假设轴系耦合节点的排序与整体轴系节点的排序一致。
         """
         from scipy.io import loadmat
         from scipy.sparse import csr_matrix
@@ -204,51 +206,61 @@ class GearBoxModalLFEMModel(ComputationalModel):
             [4, 25,  253.6, -1.1, 350244],
             [4,  4,  70, -1.1, 350241],
         ]
-        d0 = bm.array(d0, dtype=bm.float64)
+        d0 = bm.array(d0, dtype=self.ftype)
+
         d1 = [[1, 1.1, 1.2, 2, 2.1, 2.2, 3, 4, 5], [17, 5, 3, 7, 3, 3, 18, 26, 8]]
         d1 = bm.array(d1).T
-        NN = int(d1[:, 1].sum())
-        offset = [0] + [int(a) for a in bm.cumsum(d1[:, 1])] 
+
+        NN = int(d1[:, 1].sum()) # 轴系节点总数
+        # 轴系节点排序偏移数组
+        offset = [0] + [int(a) for a in bm.cumsum(d1[:, 1])]  
         self.logger.info(
                 f"Number of nodes in the shaft system: {NN}"
                 f"Offset for the nodes: {offset}")
 
         nidmap = self.mesh.data['nidmap']
+        # 轴原始编号到自然数编号的映射
         imap = {str(a[0]) : a[1] for a in zip(d1[:, 0], range(d1.shape[0]))}
-        # Create a boolean array to mark coupling nodes
-        isCouplingNodes = bm.zeros(NN, dtype=bm.bool)
+        # 轴系与齿轮箱的耦合节点的标记数组
+        isCNode = bm.zeros(NN, dtype=bm.bool)
         for i, j in d0[:, :2]:
             idx = imap[str(i)] + int(j) - 1
-            isCouplingNodes[idx] = True
-        isCouplingDofs = bm.repeat(isCouplingNodes, 6)
-        self.logger.info(f"Number of coupling nodes: {bm.sum(isCouplingNodes)}")
+            isCNode[idx] = True
 
-        S0 = S[bm.logical_not(isCouplingDofs), :]
-        S1 = S[isCouplingDofs, :]
+        # 轴系的耦合节点的自由度标记数组
+        isCDof = bm.repeat(isCNode, 6)
+        self.logger.info(f"Number of coupling nodes: {bm.sum(isCNode)}")
 
-        S00 = S0[:, bm.logical_not(isCouplingDofs)]
-        S01 = S0[:, isCouplingDofs]
-        S11 = S1[:, isCouplingDofs]
+        # 构造轴系的分块矩阵
+        S0 = S[bm.logical_not(isCDof), :]
+        S1 = S[isCDof, :]
 
-        M0 = M[bm.logical_not(isCouplingDofs), :]
-        M1 = M[isCouplingDofs, :]
+        S00 = S0[:, bm.logical_not(isCDof)]
+        S01 = S0[:, isCDof]
+        S11 = S1[:, isCDof]
 
-        M00 = M0[:, bm.logical_not(isCouplingDofs)]
-        M01 = M0[:, isCouplingDofs]
-        M11 = M1[:, isCouplingDofs]
+        M0 = M[bm.logical_not(isCDof), :]
+        M1 = M[isCDof, :]
+
+        M00 = M0[:, bm.logical_not(isCDof)]
+        M01 = M0[:, isCDof]
+        M11 = M1[:, isCDof]
         self.logger.info(
                 f"shaft system stiffness matrix: {S00.shape}, {S01.shape}, {S11.shape}"
                 f"shaft system mass matrix: {M00.shape}, {M01.shape}, {M11.shape}")
 
-        # 重新对耦合节点的自由度进行排序
+        # 变速箱壳体模型增加耦合节点的标记数组 
+        # 为壳体建模提供耦合节点的标记
+        cnode = nidmap[d0[:, -1].astype(self.itype)]
+        isCNode = bm.zeros(self.mesh.number_of_nodes(), dtype=bm.bool)
+        isCNode = bm.set_at(isCNode, cnode, True)
+        self.mesh.data.add_node_data('isCNode', isCNode)
 
-        cnode = nidmap[d0[:, -1].astype(bm.int32)]
-        isCouplingNodes = bm.zeros(self.mesh.number_of_nodes(), dtype=bm.bool)
-        isCouplingNodes = bm.set_at(isCouplingNodes, cnode, True)
-        self.mesh.data.add_node_data('isCouplingNodes', isCouplingNodes)
+        # 对轴系的耦合节点（自由度）进行排序，保证与齿轮箱的耦合节点（自由度）顺序一致
         re = bm.argsort(cnode)
-        idx = bm.arange(6 * cnode.shape[0], dtype=bm.int32).reshape(-1, 6)
+        idx = bm.arange(6 * cnode.shape[0], dtype=self.itype).reshape(-1, 6)
         idx = idx[re, :].flatten()
+
         S01 = csr_matrix(S01[:, idx])
         S11 = csr_matrix(S11[idx, :][:, idx])
         M01 = csr_matrix(M01[:, idx])
@@ -266,9 +278,10 @@ class GearBoxModalLFEMModel(ComputationalModel):
         from petsc4py import PETSc
         from slepc4py import SLEPc
 
-        self.shaft_linear_system()
-        #S, M = self.linear_system()
-        #S, M = self.apply_bc(S, M)
+        # 获取
+        S0, M0 = self.shaft_linear_system()
+        self.rbe2_matrix()
+        S1, M1 = self.box_linear_system()
 
 
         
